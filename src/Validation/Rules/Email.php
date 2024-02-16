1d4c8e6e0e065a6e922ac7ae227e5670dfe396b7
Aidan Casey <aidan@caseyhouse.net>
2024-02-16 11:16:26 -0500

    Adds validator for email addresses.

---
+++ src/Validation/Rules/Email.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tempest\Validation\Rules;
+
+use Attribute;
+use Egulias\EmailValidator\EmailValidator;
+use Egulias\EmailValidator\Validation\EmailValidation;
+use Egulias\EmailValidator\Validation\RFCValidation;
+use Tempest\Interface\Rule;
+
+#[Attribute]
+final class Email implements Rule
+{
+    public function __construct(
+        private readonly EmailValidation $validationMethod = new RFCValidation()
+    ) {}
+
+    public function isValid(mixed $value): bool
+    {
+        $emailValidator = new EmailValidator();
+
+        return $emailValidator->isValid($value, $this->validationMethod);
+    }
+
+    public function message(): string
+    {
+        return "Value should be a valid email address.";
+    }
+}
