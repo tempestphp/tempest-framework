1f6bf887225c490a687aeb99469caee4a2707a9d
levo-777 <levan.mebonia@protonmail.com>
2024-02-18 17:43:17 +0100

    modified:   app/Commands/MyCommand.php

--- app/Commands/MyCommand.php
+++ app/Commands/MyCommand.php
@@ -4,6 +4,36 @@ declare(strict_types=1);
 
 namespace App\Commands;
 
-class MyCommand
+use App\Migrations\CreateAuthorTable;
+use App\Migrations\CreateBookTable;
+use Tempest\Database\Builder\TableBuilder;
+use Tempest\Interface\Migration;
+
+class MyCommand implements Migration
 {
+    public function getName(): string
+    {
+        // Provide a unique name for your migration command
+        return 'MyMigrationCommand';
+    }
+
+    public function up(TableBuilder $builder): TableBuilder
+    {
+        // Instantiate migrations
+        $createAuthorMigration = new CreateAuthorTable();
+        $createBookMigration = new CreateBookTable();
+
+        // Run migrations
+        $createAuthorMigration->up($builder);
+        $createBookMigration->up($builder);
+
+        return $builder;
+    }
+
+    public function down(TableBuilder $builder): TableBuilder
+    {
+        // If you need to implement a down migration,
+        // you can do so here. Otherwise, leave this method empty.
+        return $builder;
+    }
 }
