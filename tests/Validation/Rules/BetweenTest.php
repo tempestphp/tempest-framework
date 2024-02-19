106346bcc27d241a1808de22f7cb1e9796750a0c
Adam Wills <adam@adamwills.com>
2024-02-19 08:15:32 -0500

    added Between validation rule

---
+++ tests/Validation/Rules/BetweenTest.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Tempest\Validation\Rules;
+
+use PHPUnit\Framework\TestCase;
+use Tempest\Validation\Rules\Between;
+
+class BetweenTest extends TestCase
+{
+    /** @test */
+    public function test_between()
+    {
+        $rule = new Between(min: 0, max: 10);
+
+        $this->assertTrue($rule->isValid(0));
+        $this->assertTrue($rule->isValid(10));
+        $this->assertTrue($rule->isValid(5));
+        $this->assertFalse($rule->isValid(11));
+        $this->assertFalse($rule->isValid(-1));
+    }
+}
