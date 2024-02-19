f14cd6382e0b0f4dc088381192c3f62a4203f698
brendt <brent.roose@gmail.com>
2024-02-19 06:20:29 +0100

    Add migrate command

---
+++ src/Console/Commands/MigrateCommand.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tempest\Console\Commands;
+
+use Tempest\Console\ConsoleCommand;
+use Tempest\Database\Migrations\MigrationManager;
+use Tempest\Interface\Console;
+
+final readonly class MigrateCommand
+{
+    public function __construct(
+        private Console $console,
+        private MigrationManager $migrationManager
+    ) {}
+
+    #[ConsoleCommand(
+        name: 'migrate',
+        description: 'Run all new migrations',
+    )]
+    public function __invoke(): void
+    {
+        $this->migrationManager->up();
+
+        $this->console->success("Done");
+    }
+
+    // TODO: add events for writing to the console which migrations have run
+}
