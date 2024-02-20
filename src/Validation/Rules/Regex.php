3cddb25856be446f3b4883c10658f5e5405f1ca9
Aidan Casey <aidan@caseyhouse.net>
2024-02-20 16:15:06 -0500

    Adds regex validation rule

---
+++ src/Validation/Rules/Regex.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tempest\Validation\Rules;
+
+use Attribute;
+use Tempest\Interface\Rule;
+
+#[Attribute]
+final readonly class Regex implements Rule
+{
+    public function __construct(private string $pattern)
+    {
+    }
+
+    public function isValid(mixed $value): bool
+    {
+        return preg_match($this->pattern, $value) === 1;
+    }
+
+    public function message(): string
+    {
+        return "The value must match the regular expression pattern: $this->pattern";
+    }
+}
