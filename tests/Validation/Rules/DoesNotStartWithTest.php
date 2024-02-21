5fe5a1a84538db3aa7c505c16f8558bb5129845a
Sergiu Daniel Vidican <vsergiu93@gmail.com>
2024-02-21 15:14:08 +0200

    feat: add new does not start with validation rule for strings

---
+++ tests/Validation/Rules/DoesNotStartWithTest.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Tempest\Validation\Rules;
+
+use Tempest\Validation\Rules\DoesNotStartWith;
+use Tests\Tempest\TestCase;
+
+class DoesNotStartWithTest extends TestCase
+{
+    /**
+     * @param string $needle
+     * @param string $stringToTest
+     * @param bool $expected
+     *
+     * @dataProvider dataSets
+     *
+     * @return void
+     */
+    public function test_rule(string $needle, string $stringToTest, bool $expected): void
+    {
+        $rule = new DoesNotStartWith($needle);
+
+        $this->assertEquals($expected, $rule->isValid($stringToTest));
+    }
+
+    public function dataSets(): array
+    {
+        return [
+            'should return false if it starts with the given string' => ['test', 'test this is a test', false],
+            'should return true if it does not start with the given string' => ['test', 'this is a test', true],
+            'should return true for an empty string' => ['test', '', true],
+            'should return true for a single non-matching character' => ['test', 'a', true],
+            'should return false for a matching character string' => ['a', 'apple', false],
+            'should return true if it starts with a different string' => ['test', 'best this is a test', true],
+            'should return true if the string is the reverse of the given string' => ['test', 'tset', true],
+            'should return false if the string and given string are the same' => ['test', 'test', false],
+        ];
+    }
+}
