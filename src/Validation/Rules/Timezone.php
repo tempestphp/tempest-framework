5b6cd676ee0b63bb3ce77fbaa942b7814767449b
Dylan LANNUZEL <lannuzeld@gmail.com>
2024-02-21 23:48:28 +0100

    Accept timezoneGroup & countryCode

--- src/Validation/Rules/Timezone.php
+++ src/Validation/Rules/Timezone.php
@@ -5,14 +5,19 @@ declare(strict_types=1);
 namespace Tempest\Validation\Rules;
 
 use Attribute;
+use DateTimeZone;
 use Tempest\Validation\Rule;
 
 #[Attribute]
 final readonly class Timezone implements Rule
 {
+    public function __construct(private int $timezoneGroup = DateTimeZone::ALL, private ?string $countryCode = null)
+    {
+    }
+
     public function isValid(mixed $value): bool
     {
-        $timezones = timezone_identifiers_list();
+        $timezones = timezone_identifiers_list($this->timezoneGroup, $this->countryCode);
 
         return in_array($value, $timezones, true);
     }
--- tests/Validation/Rules/TimezoneTest.php
+++ tests/Validation/Rules/TimezoneTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\Tempest\Validation\Rules;
 
+use DateTimeZone;
 use PHPUnit\Framework\TestCase;
 use Tempest\Validation\Rules\Timezone;
 
@@ -21,5 +22,37 @@ class TimezoneTest extends TestCase
         $this->assertTrue($rule->isValid('Europe/London'));
         $this->assertTrue($rule->isValid('Europe/Paris'));
         $this->assertTrue($rule->isValid('UTC'));
+    }
+
+    public function test_timezone_with_country_code()
+    {
+        $rule = new Timezone(DateTimeZone::PER_COUNTRY, 'AU');
+
+        $this->assertFalse($rule->isValid('America/New_York'));
+        $this->assertTrue($rule->isValid('Australia/Sydney'));
+        $this->assertTrue($rule->isValid('Australia/Melbourne'));
+
+        $rule = new Timezone(DateTimeZone::PER_COUNTRY, 'US');
+
+        $this->assertFalse($rule->isValid('Europe/Paris'));
+        $this->assertTrue($rule->isValid('America/New_York'));
+        $this->assertTrue($rule->isValid('America/Los_Angeles'));
+        $this->assertTrue($rule->isValid('America/Chicago'));
+    }
+
+    public function test_timezone_with_group()
+    {
+        $rule = new Timezone(DateTimeZone::ASIA);
+
+        $this->assertFalse($rule->isValid('Africa/Nairobi'));
+        $this->assertTrue($rule->isValid('Asia/Tokyo'));
+        $this->assertTrue($rule->isValid('Asia/Hong_Kong'));
+        $this->assertTrue($rule->isValid('Asia/Singapore'));
+
+        $rule = new Timezone(DateTimeZone::INDIAN);
+
+        $this->assertFalse($rule->isValid('Europe/Paris'));
+        $this->assertTrue($rule->isValid('Indian/Reunion'));
+        $this->assertTrue($rule->isValid('Indian/Comoro'));
     }
 }
