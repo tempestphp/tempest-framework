c6a1f5215ea9ead28cc9298ba45c99f3f564d924
Sergiu Daniel Vidican <vsergiu93@gmail.com>
2024-02-21 15:34:04 +0200

    style: use composer qa to fix code style issues

--- tests/Validation/Rules/LowercaseTest.php
+++ tests/Validation/Rules/LowercaseTest.php
@@ -1,16 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tests\Tempest\Validation\Rules;
 
-use Tests\Tempest\TestCase;
 use Tempest\Validation\Rules\Lowercase;
+use Tests\Tempest\TestCase;
 
 class LowercaseTest extends TestCase
 {
     public function test_lowercase()
     {
         $rule = new Lowercase();
-        
+
         $this->assertTrue($rule->isValid('abc'));
         $this->assertTrue($rule->isValid('àbç'));
         $this->assertFalse($rule->isValid('ABC'));
