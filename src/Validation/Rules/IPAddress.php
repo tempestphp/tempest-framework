27b0d938ae9d3f544e04a2b5fa0924d8c85dc127
Aidan Casey <aidan@caseyhouse.net>
2024-02-21 19:16:51 -0500

    Expands IP rule.

--- src/Validation/Rules/IPAddress.php
+++ src/Validation/Rules/IPAddress.php
@@ -10,13 +10,67 @@ use Tempest\Validation\Rule;
 #[Attribute]
 final readonly class IPAddress implements Rule
 {
+    private int $options;
+
+    public function __construct(
+        private bool $ipv4 = false,
+        private bool $ipv6 = false,
+        private bool $allowPrivateRange = true,
+        private bool $allowReservedRange = true
+    ) {
+        $options = 0;
+
+        if ($this->ipv4) {
+            $options = $options | FILTER_FLAG_IPV4;
+        }
+
+        if ($this->ipv6) {
+            $options = $options | FILTER_FLAG_IPV6;
+        }
+
+        if (! $this->allowPrivateRange) {
+            $options = $options | FILTER_FLAG_NO_PRIV_RANGE;
+        }
+
+        if (! $this->allowReservedRange) {
+            $options = $options | FILTER_FLAG_NO_RES_RANGE;
+        }
+
+        $this->options = $options;
+    }
+
     public function isValid(mixed $value): bool
     {
-        return boolval(filter_var($value, FILTER_VALIDATE_IP));
+        return boolval(filter_var($value, FILTER_VALIDATE_IP, $this->options));
     }
 
     public function message(): string
     {
-        return 'Value should be a valid IP Address';
+        if ($this->options === 0) {
+            return 'Value should be a valid IP address';
+        }
+
+        $version = 'IP';
+        $additions = [];
+
+        if ($this->options & FILTER_FLAG_IPV4 && !($this->options & FILTER_FLAG_IPV6)) {
+            $version = 'IPv4';
+        }
+
+        if ($this->options & FILTER_FLAG_IPV6 && !($this->options & FILTER_FLAG_IPV4)) {
+            $version = 'IPv6';
+        }
+
+        if ($this->options & FILTER_FLAG_NO_PRIV_RANGE) {
+            $additions[] = 'not in a private range';
+        }
+
+        if ($this->options & FILTER_FLAG_NO_RES_RANGE) {
+            $additions[] = 'not in a reserved range';
+        }
+
+        return "Value should be a valid $version address" . (
+            empty($additions) ? '' : ' that is ' . implode(' and ', $additions)
+        );
     }
 }
--- tests/Validation/Rules/IPAddressTest.php
+++ tests/Validation/Rules/IPAddressTest.php
@@ -13,7 +13,8 @@ class IPAddressTest extends TestCase
     {
         $rule = new IPAddress();
 
-        $this->assertSame('Value should be a valid IP Address', $rule->message());
+        $this->assertSame('Value should be a valid IP address', $rule->message());
+
         $this->assertTrue($rule->isValid('192.168.0.1'));
         $this->assertTrue($rule->isValid('10.0.0.1'));
         $this->assertTrue($rule->isValid('172.16.0.1'));
@@ -25,5 +26,79 @@ class IPAddressTest extends TestCase
         $this->assertFalse($rule->isValid('192.168.0'));
         $this->assertFalse($rule->isValid('192.168.0.1.2'));
         $this->assertFalse($rule->isValid('192.168.0.1/24'));
+    }
+
+    public function test_ipv4_address()
+    {
+        $rule = new IPAddress(ipv4: true);
+
+        $this->assertSame('Value should be a valid IPv4 address', $rule->message());
+
+        $this->assertTrue($rule->isValid('192.168.0.1'));
+        $this->assertTrue($rule->isValid('10.0.0.1'));
+        $this->assertTrue($rule->isValid('172.16.0.1'));
+
+        $this->assertFalse($rule->isValid('2001:0db8:85a3:0000:0000:8a2e:0370:7334'));
+        $this->assertFalse($rule->isValid('2001:db8:85a3::8a2e:370:7334'));
+    }
+
+    public function test_ipv6_address()
+    {
+        $rule = new IPAddress(ipv6: true);
+
+        $this->assertSame('Value should be a valid IPv6 address', $rule->message());
+
+        $this->assertTrue($rule->isValid('2001:0db8:85a3:0000:0000:8a2e:0370:7334'));
+        $this->assertTrue($rule->isValid('2001:db8:85a3::8a2e:370:7334'));
+
+        $this->assertFalse($rule->isValid('192.168.0.1'));
+        $this->assertFalse($rule->isValid('10.0.0.1'));
+        $this->assertFalse($rule->isValid('172.16.0.1'));
+    }
+
+    public function test_ip_address_without_private_range()
+    {
+        $rule = new IPAddress(allowPrivateRange: false);
+
+        $this->assertFalse($rule->isValid('192.168.1.1'));
+        $this->assertTrue($rule->isValid('210.221.151.70'));
+    }
+
+    public function test_ip_address_without_reserved_range()
+    {
+        $rule = new IPAddress(allowReservedRange: false);
+
+        $this->assertFalse($rule->isValid('169.254.0.0'));
+        $this->assertTrue($rule->isValid('172.16.1.1'));
+    }
+
+    public function test_messages()
+    {
+        $rule = new IPAddress(ipv4: true);
+        $this->assertSame('Value should be a valid IPv4 address', $rule->message());
+
+        $rule = new IPAddress(ipv4: true, allowPrivateRange: false);
+        $this->assertSame('Value should be a valid IPv4 address that is not in a private range', $rule->message());
+
+        $rule = new IPAddress(ipv4: true, allowReservedRange: false);
+        $this->assertSame('Value should be a valid IPv4 address that is not in a reserved range', $rule->message());
+
+        $rule = new IPAddress(ipv4: true, allowPrivateRange: false, allowReservedRange: false);
+        $this->assertSame('Value should be a valid IPv4 address that is not in a private range and not in a reserved range', $rule->message());
+
+        $rule = new IPAddress(ipv6: true);
+        $this->assertSame('Value should be a valid IPv6 address', $rule->message());
+
+        $rule = new IPAddress(ipv6: true, allowPrivateRange: false);
+        $this->assertSame('Value should be a valid IPv6 address that is not in a private range', $rule->message());
+
+        $rule = new IPAddress(ipv6: true, allowReservedRange: false);
+        $this->assertSame('Value should be a valid IPv6 address that is not in a reserved range', $rule->message());
+
+        $rule = new IPAddress(ipv6: true, allowPrivateRange: false, allowReservedRange: false);
+        $this->assertSame('Value should be a valid IPv6 address that is not in a private range and not in a reserved range', $rule->message());
+
+        $rule = new IPAddress(ipv4: true, ipv6: true, allowPrivateRange: false, allowReservedRange: false);
+        $this->assertSame('Value should be a valid IP address that is not in a private range and not in a reserved range', $rule->message());
     }
 }
