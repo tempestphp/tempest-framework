788f03f5c04ae205e7d6d4642311f88dd6200d4a
Sergiu Daniel Vidican <vsergiu93@gmail.com>
2024-02-22 22:40:09 +0200

    refactor: remove polyfill

--- src/Validation/Rules/JSON.php
+++ src/Validation/Rules/JSON.php
@@ -7,31 +7,6 @@ namespace Tempest\Validation\Rules;
 use Attribute;
 use Tempest\Validation\Rule;
 
-/**
- * We need to polyfill this, because at the moment the minimum required version of php for tempest is ^8.2,
- * and json_validate was introduced in 8.3
- * @TODO remove this polyfill once the minimum php version is >= than 8.3
- * Source @link https://php.watch/versions/8.3/json_validate#polyfill
- */
-if (! function_exists('json_validate')) {
-    function json_validate(string $json, int $depth = 512, int $flags = 0): bool
-    {
-        if ($flags !== 0 && $flags !== \JSON_INVALID_UTF8_IGNORE) {
-            throw new \ValueError(
-                'json_validate(): Argument #3 ($flags) must be a valid flag (allowed flags: JSON_INVALID_UTF8_IGNORE)'
-            );
-        }
-
-        if ($depth <= 0) {
-            throw new \ValueError('json_validate(): Argument #2 ($depth) must be greater than 0');
-        }
-
-        \json_decode($json, null, $depth, $flags);
-
-        return \json_last_error() === \JSON_ERROR_NONE;
-    }
-}
-
 #[Attribute]
 final readonly class JSON implements Rule
 {
