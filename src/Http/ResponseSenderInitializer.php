6b92c15bbba9ea84b0d91634419ad259b40b8f83
brendt <brent.roose@gmail.com>
2024-02-24 05:53:02 +0100

    Cleanup

--- src/Http/ResponseSenderInitializer.php
+++ src/Http/ResponseSenderInitializer.php
@@ -4,17 +4,11 @@ declare(strict_types=1);
 
 namespace Tempest\Http;
 
-use Tempest\Container\CanInitialize;
 use Tempest\Container\Container;
 use Tempest\Container\Initializer;
 
-final class ResponseSenderInitializer implements Initializer, CanInitialize
+final class ResponseSenderInitializer implements Initializer
 {
-    public function canInitialize(string $className): bool
-    {
-        return $className === ResponseSender::class;
-    }
-
     public function initialize(string $className, Container $container): object
     {
         $responseSender = new GenericResponseSender();
--- tests/Http/ResponseSenderInitializerTest.php
+++ tests/Http/ResponseSenderInitializerTest.php
@@ -15,9 +15,6 @@ class ResponseSenderInitializerTest extends TestCase
     {
         $initializer = new ResponseSenderInitializer();
 
-        $this->assertTrue($initializer->canInitialize(ResponseSender::class));
-        $this->assertFalse($initializer->canInitialize(\DateTime::class));
-
         $this->assertInstanceOf(
             GenericResponseSender::class,
             $initializer->initialize(ResponseSender::class, $this->container)
