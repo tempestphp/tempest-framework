f537b6b7f5b20bc60d94c26078598674bf3e6f82
Joe Theuerkauf <joe@theuerkauf.dev>
2024-02-26 11:22:28 -0500

    #59 Refactor Kernel to prepare for cache implementation
    * Remove Tempest::__construct() $appConfig parameter - available from Container object
    * Add Kernel::__construct() $bootstraps promoted property
    * Rename Kernel::initContainer()
    * Loop $bootstraps to register them in the Container as singletons
    * Execute Bootstraps boot() in Kernel::init()
    * Add Bootstrap classes for Discovery and Config
    * Set Bootstraps as third Kernel constructor argument
    * Fixed some accessors to AppConfig from the $kernel object

---
+++ src/Bootstraps/ConfigBootstrap.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tempest\Bootstraps;
+
+use Tempest\AppConfig;
+use Tempest\Container\Container;
+use function Tempest\path;
+
+final readonly class ConfigBootstrap implements Bootstrap
+{
+    public function __construct(
+        private Container $container
+    ) {
+    }
+
+    #[\Override]
+    public function boot(): void
+    {
+        // Scan for package config files
+        foreach ($this->container->get(AppConfig::class)->discoveryLocations as $package) {
+            $configFiles = glob(path($package->path, 'Config/**.php'));
+
+            foreach ($configFiles as $configFile) {
+                $configFile = require $configFile;
+
+                $this->container->config($configFile);
+            }
+        }
+    }
+}
