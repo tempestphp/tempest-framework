ba5115623e0f29d48096810060f464abcbeff635
Brent Roose <brent.roose@gmail.com>
2024-02-26 11:10:55 +0100

    QA

--- src/Exceptions/HttpExceptionHandler.php
+++ src/Exceptions/HttpExceptionHandler.php
@@ -6,7 +6,6 @@ namespace Tempest\Exceptions;
 
 use Symfony\Component\VarDumper\Cloner\VarCloner;
 use Symfony\Component\VarDumper\Dumper\HtmlDumper;
-use Symfony\Component\VarDumper\VarDumper;
 use Throwable;
 
 final readonly class HttpExceptionHandler implements ExceptionHandler
