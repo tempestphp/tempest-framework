eee9aaa4be54939262e9dac6ef67d60f7fcfbd5a
Brent Roose <brent.roose@gmail.com>
2024-02-26 09:26:30 +0100

    Fix TestResponseSenderInitializer

--- src/Testing/Http/TestResponseSenderInitializer.php
+++ src/Testing/Http/TestResponseSenderInitializer.php
@@ -6,17 +6,18 @@ namespace Tempest\Testing\Http;
 
 use Tempest\Container\CanInitialize;
 use Tempest\Container\Container;
+use Tempest\Container\Initializer;
 use Tempest\Http\Response;
 use Tempest\Http\ResponseSender;
 
-final class TestResponseSenderInitializer implements CanInitialize
+final class TestResponseSenderInitializer implements Initializer, CanInitialize
 {
     public function canInitialize(string $className): bool
     {
         return in_array($className, [Response::class, TestResponse::class]);
     }
 
-    public function initialize(string $className, Container $container): object
+    public function initialize(Container $container): object
     {
         $responseSender = new TestResponseSender();
 
