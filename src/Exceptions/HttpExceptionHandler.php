3dd18e197cc7b0e1e421d7ecb3bcb1c550bd05fa
Brent Roose <brent.roose@gmail.com>
2024-02-26 11:10:34 +0100

    Use VarDumper for HttpExceptionHandler.php

--- src/Exceptions/HttpExceptionHandler.php
+++ src/Exceptions/HttpExceptionHandler.php
@@ -4,16 +4,21 @@ declare(strict_types=1);
 
 namespace Tempest\Exceptions;
 
+use Symfony\Component\VarDumper\Cloner\VarCloner;
+use Symfony\Component\VarDumper\Dumper\HtmlDumper;
+use Symfony\Component\VarDumper\VarDumper;
 use Throwable;
 
 final readonly class HttpExceptionHandler implements ExceptionHandler
 {
     public function handle(Throwable $throwable): void
     {
-        if (! function_exists('dd')) {
-            // TODO
-        }
+        $varCloner = new VarCloner();
+        $dumper = new HtmlDumper();
 
-        dd($throwable);
+        $dumper->dump($varCloner->cloneVar($throwable), extraDisplayOptions: [
+            'maxDepth' => 50,
+            'maxStringLength' => 500,
+        ]);
     }
 }
