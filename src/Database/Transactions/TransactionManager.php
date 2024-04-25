98ff4985ee74e5419966fa2e68df2c740189b807
przemyslaw-przylucki <przemyslaw.m.przylucki@gmail.com>
2024-04-25 18:46:49 +0200

    Update TransactionManager.php

--- src/Database/Transactions/TransactionManager.php
+++ src/Database/Transactions/TransactionManager.php
@@ -4,12 +4,8 @@ declare(strict_types=1);
 
 namespace Tempest\Database\Transactions;
 
-use PDO;
-
 interface TransactionManager
 {
-    public function __construct(PDO $pdo);
-
     public function begin(): void;
 
     public function commit(): void;
