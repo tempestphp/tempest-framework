8afb1a6508a0983c189413e79c51c87b60fec021
przemyslaw-przylucki <przemyslaw.m.przylucki@gmail.com>
2024-04-29 20:24:43 +0200

    oopsie daisy

--- src/Scheduler/Schedule.php
+++ src/Scheduler/Schedule.php
@@ -4,10 +4,8 @@ declare(strict_types=1);
 
 namespace Tempest\Console\Scheduler;
 
-use Attribute;
 use DateInterval;
 
-#[Attribute(Attribute::TARGET_METHOD)]
 final class Schedule
 {
     public function __construct(
