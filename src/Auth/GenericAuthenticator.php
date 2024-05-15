c5a1b3bd371055470ef7c679a6299624ccad9500
Wendell Adriel <wendelladriel.ti@gmail.com>
2024-05-15 11:55:44 +0100

    Initial login/logout/user implementation

--- src/Auth/GenericAuthenticator.php
+++ src/Auth/GenericAuthenticator.php
@@ -8,7 +8,24 @@ use Tempest\Http\Session\Session;
 
 abstract class GenericAuthenticator implements Authenticator
 {
+    public const string SESSION_USER_KEY = 'tempest_session_user';
+
     public function __construct(protected Session $session)
     {
+    }
+
+    public function logout(): void
+    {
+        $this->session->remove(self::SESSION_USER_KEY);
+        $this->session->destroy();
+    }
+
+    protected function createSession(Identifiable $identifiable): void
+    {
+        $this->session->set(self::SESSION_USER_KEY, [
+            'source' => $identifiable->source(),
+            'identifier_field' => $identifiable->identifier(),
+            'identifier_value' => $identifiable->identifierValue(),
+        ]);
     }
 }
