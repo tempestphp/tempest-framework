4a52d7f71d7237ff45953c9564cb9bfee86a74b6
brendt <brent.roose@gmail.com>
2024-05-24 21:02:30 +0200

    Remove log discovery

--- tests/Unit/KernelTest.php
+++ tests/Unit/KernelTest.php
@@ -12,7 +12,6 @@ use Tempest\Discovery\DiscoveryDiscovery;
 use Tempest\Discovery\DiscoveryLocation;
 use Tempest\Discovery\EventBusDiscovery;
 use Tempest\Discovery\InitializerDiscovery;
-use Tempest\Discovery\LogHandlerDiscovery;
 use Tempest\Discovery\MapperDiscovery;
 use Tempest\Kernel;
 
@@ -39,12 +38,11 @@ class KernelTest extends TestCase
 
         $appConfig = $container->get(AppConfig::class);
 
-        $this->assertCount(5, $appConfig->discoveryClasses);
+        $this->assertCount(4, $appConfig->discoveryClasses);
         $this->assertSame(DiscoveryDiscovery::class, $appConfig->discoveryClasses[0]);
         $this->assertSame(EventBusDiscovery::class, $appConfig->discoveryClasses[1]);
         $this->assertSame(MapperDiscovery::class, $appConfig->discoveryClasses[2]);
-        $this->assertSame(LogHandlerDiscovery::class, $appConfig->discoveryClasses[3]);
-        $this->assertSame(InitializerDiscovery::class, $appConfig->discoveryClasses[4]);
+        $this->assertSame(InitializerDiscovery::class, $appConfig->discoveryClasses[3]);
 
         $this->assertCount(2, $appConfig->discoveryLocations);
         $this->assertSame('Tempest\\', $appConfig->discoveryLocations[0]->namespace);
