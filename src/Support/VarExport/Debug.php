ba1c2f37fc09567cf6666bd0456d83dc4d077c9b
brendt <brent.roose@gmail.com>
2024-05-25 07:52:54 +0200

    Improve debugging

--- src/Support/VarExport/Debug.php
+++ src/Support/VarExport/Debug.php
@@ -5,33 +5,43 @@ declare(strict_types=1);
 namespace Tempest\Support\VarExport;
 
 use Stringable;
+use Symfony\Component\VarDumper\Cloner\VarCloner;
+use Symfony\Component\VarDumper\Dumper\CliDumper;
 use Symfony\Component\VarDumper\VarDumper;
 use Symfony\Component\VarExporter\VarExporter;
 use Tempest\Log\LogConfig;
 
 final readonly class Debug
 {
-    public function __construct(private LogConfig $logConfig)
-    {
-    }
+    public function __construct(private LogConfig $logConfig) {}
 
     public function log(mixed ...$input): void
     {
         $handle = fopen($this->logConfig->debugLogPath, 'a');
 
-        foreach ($input as $key => $item) {
-            if ($item instanceof Stringable) {
-                $output = (string) $item;
-            } else {
-                $output = var_export($item, true);
-            }
+        $cloner = new VarCloner();
 
-            fwrite($handle, "[{$key}] {$output}" . PHP_EOL);
+        foreach ($input as $key => $item) {
+            $output = '';
+
+            $dumper = new CliDumper(function ($line, $depth) use (&$output) {
+                if ($depth < 0) {
+                    return;
+                }
+
+                $output .= str_repeat(' ', $depth) . $line . "\n";
+            });
+
+            $dumper->setColors(true);
+
+            $dumper->dump($cloner->cloneVar($item));
+
+            fwrite($handle, "[{$key}]" . PHP_EOL . $output . PHP_EOL);
         }
 
         fclose($handle);
 
-        VarDumper::dump(...$input);
+        VarDumper::dump($input);
 
         $trace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
 
