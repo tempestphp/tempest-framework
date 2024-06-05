e2f8cd50e4a64c2b60be1fc89ec711db74385786
Wendell Adriel <wendelladriel.ti@gmail.com>
2024-06-05 12:04:55 +0100

    Refactor DatabaseIdentifier

--- src/Auth/DatabaseIdentifier.php
+++ src/Auth/DatabaseIdentifier.php
@@ -25,14 +25,11 @@ final readonly class DatabaseIdentifier implements IdentifierResolver
             throw new InvalidLoginException();
         }
 
-        $found = (new Query(
-            "SELECT * FROM :table WHERE :identifier_field = :identifier_value LIMIT 1",
-            [
-                'table' => $this->config->databaseSource['source'],
-                'identifier_field' => $this->config->databaseSource['identifier'],
-                'identifier_value' => $call->identifier,
-            ]
-        ))->fetchFirst();
+        $found = $this->fetchIdentifiable(
+            table: $this->config->databaseSource['source'],
+            identifierField: $this->config->databaseSource['identifier'],
+            identifierValue: $call->identifier,
+        );
 
         if (is_null($found)) {
             throw new InvalidLoginException();
@@ -52,15 +49,24 @@ final readonly class DatabaseIdentifier implements IdentifierResolver
             return null;
         }
 
-        $query = new Query(
-            "SELECT * FROM :table WHERE :identifier_field = :identifier_value LIMIT 1",
-            [
-                'table' => $this->config->databaseSource['source'],
-                'identifier_field' => $sessionInfo['identifier_field'],
-                'identifier_value' => $sessionInfo['identifier_value'],
-            ]
+        $identifiable = $this->fetchIdentifiable(
+            table: $this->config->databaseSource['source'],
+            identifierField: $sessionInfo['identifier_field'],
+            identifierValue: $sessionInfo['identifier_value'],
         );
 
-        return make($this->config->identifiable)->from($query->fetchFirst());
+        return is_null($identifiable) ? null : make($this->config->identifiable)->from($identifiable);
+    }
+
+    private function fetchIdentifiable(string $table, string $identifierField, string $identifierValue): array|null
+    {
+        return (new Query(
+            "SELECT * FROM :table WHERE :identifier_field = :identifier_value LIMIT 1",
+            [
+                'table' => $table,
+                'identifier_field' => $identifierField,
+                'identifier_value' => $identifierValue,
+            ]
+        ))->fetchFirst();
     }
 }
