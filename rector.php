500100861391dc4aab32e2c251c542d9400e613a
Tonko Mulder <tonko@tonkomulder.nl>
2024-08-08 14:55:24 +0200

    [Rector] add config file
    Signed-off-by: Tonko Mulder <tonko@tonkomulder.nl>

---
+++ rector.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Arguments\Rector\ClassMethod\ArgumentAdderRector;
+use Rector\Caching\ValueObject\Storage\FileCacheStorage;
+use Rector\Config\RectorConfig;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUnusedPublicMethodParameterRector;
+use Rector\DeadCode\Rector\PropertyProperty\RemoveNullPropertyInitializationRector;
+use Rector\DeadCode\Rector\Stmt\RemoveUnreachableStatementRector;
+use Rector\Php70\Rector\StaticCall\StaticCallOnNonStaticToInstanceCallRector;
+use Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
+use Rector\Php74\Rector\Property\RestoreDefaultNullToNullableTypePropertyRector;
+use Rector\Php74\Rector\Ternary\ParenthesizeNestedTernaryRector;
+use Rector\Php81\Rector\Array_\FirstClassCallableRector;
+use Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector;
+use Rector\Php81\Rector\Property\ReadOnlyPropertyRector;
+use Rector\Php82\Rector\Class_\ReadOnlyClassRector;
+use Rector\Php82\Rector\Param\AddSensitiveParameterAttributeRector;
+use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
+use Rector\Php84\Rector\Param\ExplicitNullableParamTypeRector;
+use Rector\TypeDeclaration\Rector\ClassMethod\ReturnNeverTypeRector;
+use Rector\TypeDeclaration\Rector\Closure\ClosureReturnTypeRector;
+use Rector\TypeDeclaration\Rector\Empty_\EmptyOnNullableObjectToInstanceOfRector;
+
+return RectorConfig::configure()
+    ->withCache('./.cache/rector', FileCacheStorage::class)
+    ->withPaths([
+        __DIR__ . '/public',
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    ->withConfiguredRule(AddSensitiveParameterAttributeRector::class, [
+        'sensitive_parameters' => [
+            'password',
+            'currentPassword',
+            'confirmPassword',
+            'confirmedPassword',
+            'plainTextPassword',
+            'newPassword',
+            'tincan_password',
+            'tincanPassword',
+            'secret',
+            'two_factor_secret',
+            'token',
+            'appKey',
+        ],
+    ])
+    ->withRules([
+        ParenthesizeNestedTernaryRector::class,
+        ExplicitNullableParamTypeRector::class,
+    ])
+    ->withSkip([
+        AddOverrideAttributeToOverriddenMethodsRector::class,
+        ArgumentAdderRector::class,
+        ClosureToArrowFunctionRector::class,
+        EmptyOnNullableObjectToInstanceOfRector::class,
+        FirstClassCallableRector::class,
+        NullToStrictStringFuncCallArgRector::class,
+        ReadOnlyClassRector::class,
+        ReadOnlyPropertyRector::class,
+        RemoveNullPropertyInitializationRector::class,
+        RemoveUnreachableStatementRector::class,
+        AddSensitiveParameterAttributeRector::class,
+        RemoveUnusedPublicMethodParameterRector::class,
+        RestoreDefaultNullToNullableTypePropertyRector::class,
+        ReturnNeverTypeRector::class,
+        StaticCallOnNonStaticToInstanceCallRector::class,
+        ClosureReturnTypeRector::class,
+    ])
+    // If needed, we can update the parallel settings to make sure Rector doesn't start generating errors on large codebases
+    ->withParallel(300, 10, 10)
+    // here we can define, what prepared sets of rules will be applied
+    // TODO: enable prepared sets one by one
+    ->withPreparedSets(
+        codeQuality: false,
+        codingStyle: true,
+        privatization: true,
+        naming: false,
+        earlyReturn: true,
+    )
+    // TODO: increase one level at a time
+    ->withDeadCodeLevel(40)// max 40
+    ->withMemoryLimit('3G')
+    ->withPhpSets(php83: true)
+    ->withTypeCoverageLevel(37); // max 37
