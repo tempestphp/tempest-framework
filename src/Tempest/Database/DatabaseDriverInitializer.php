50c9848a418f358d1cc5841fdfd419b69dd96608
Tonko Mulder <tonko@tonkomulder.nl>
2024-08-09 13:50:38 +0200

    move singleton attribute to the method
    Signed-off-by: Tonko Mulder <tonko@tonkomulder.nl>

--- src/Tempest/Database/DatabaseDriverInitializer.php
+++ src/Tempest/Database/DatabaseDriverInitializer.php
@@ -8,9 +8,9 @@ use Tempest\Container\Container;
 use Tempest\Container\Initializer;
 use Tempest\Container\Singleton;
 
-#[Singleton]
 final class DatabaseDriverInitializer implements Initializer
 {
+    #[Singleton]
     public function initialize(Container $container): DatabaseDriver
     {
         return $container->get(DatabaseConfig::class)->driver();
