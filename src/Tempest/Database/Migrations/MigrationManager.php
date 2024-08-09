ee8ff72ec2399004f8f383f2f2be7039ceb554a0
Tonko Mulder <tonko@tonkomulder.nl>
2024-08-09 13:40:42 +0200

    handle specific PDO exception
    Signed-off-by: Tonko Mulder <tonko@tonkomulder.nl>

--- src/Tempest/Database/Migrations/MigrationManager.php
+++ src/Tempest/Database/Migrations/MigrationManager.php
@@ -12,6 +12,7 @@ use Tempest\Database\Exceptions\QueryException;
 use Tempest\Database\Migration as MigrationInterface;
 use Tempest\Database\Query;
 use function Tempest\event;
+use UnhandledMatchError;
 
 final readonly class MigrationManager
 {
@@ -53,9 +54,14 @@ final readonly class MigrationManager
     {
         try {
             $existingMigrations = Migration::all();
-        } catch (PDOException) {
-            // @todo should be handled better as PDO exception doesn't necessarily mean that the migrations table doesn't exist
-            event(new MigrationFailed('Migration', MigrationException::noTable()));
+        } catch (PDOException $exception) {
+            /** @throw UnhandledMatchError */
+            match ((string) $exception->getCode()) {
+                $this->databaseConfig->driver()->dialect()->tableNotFoundCode() => event(
+                    event: new MigrationFailed(name: 'Migration', exception: MigrationException::noTable())
+                ),
+                default => throw new UnhandledMatchError($exception->getMessage()),
+            };
 
             return;
         }
