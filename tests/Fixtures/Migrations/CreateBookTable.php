d487c6ae9979f041588c5e833db0554a20a3bbc8
Tonko Mulder <tonko@tonkomulder.nl>
2024-08-10 23:53:22 +0200

    convert CreateBookTable to use the builder
    Signed-off-by: Tonko Mulder <tonko@tonkomulder.nl>

--- tests/Fixtures/Migrations/CreateBookTable.php
+++ tests/Fixtures/Migrations/CreateBookTable.php
@@ -4,11 +4,17 @@ declare(strict_types=1);
 
 namespace Tests\Tempest\Fixtures\Migrations;
 
+use Tempest\Database\DatabaseDriver;
 use Tempest\Database\Migration;
 use Tempest\Database\Query;
 
 final readonly class CreateBookTable implements Migration
 {
+    public function __construct(
+        private DatabaseDriver $driver,
+    ) {
+    }
+
     public function getName(): string
     {
         return '0000-00-00_create_book_table';
@@ -16,15 +22,22 @@ final readonly class CreateBookTable implements Migration
 
     public function up(): Query|null
     {
-        return new Query("CREATE TABLE Book (
-            `id` INTEGER PRIMARY KEY AUTOINCREMENT,
-            `title` TEXT NOT NULL,
-            `author_id` INTEGER
-        )");
+        return $this->driver
+            ->createQueryStatement('Book')
+            ->createTable()
+            ->primary()
+            ->createColumn('title', 'TEXT')
+            ->createColumn('author_id', 'INTEGER', nullable: true)
+            ->createForeignKey('author_id', 'Author')
+            ->toQuery();
     }
 
     public function down(): Query|null
     {
-        return new Query("DROP TABLE Book");
+        return $this->driver
+            ->createQueryStatement('Book')
+            ->dropForeignKeyFor('Author')
+            ->dropTable()
+            ->toQuery();
     }
 }
