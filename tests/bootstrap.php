46c3ce87cf45375527eb6de24b8056aa351e9664
Sergiu Daniel Vidican <vsergiu93@gmail.com>
2024-08-15 16:23:26 +0300

    refactor: remove os specific code as is not really needed

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,16 +4,4 @@ declare(strict_types=1);
 
 require_once  __DIR__ . '/../vendor/autoload.php';
 
-
-$script = 'tempest discovery:clear';
-
-// Adjust the command for the OS
-if (PHP_OS_FAMILY === 'Windows') {
-    // On Windows, use "php" executable and provide the relative path
-    $command = "php {$script}";
-} else {
-    // On Unix-based systems, execute the script directly
-    $command = "./{$script}";
-}
-
-passthru($command);
+passthru("php tempest discovery:clear");
