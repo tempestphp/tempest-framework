eaa2fa97a1569ad8c53b8526f12461f93026a443
Brent Roose <brent.roose@gmail.com>
2024-08-16 09:30:58 +0200

    Query Builder improvements

--- src/Tempest/Database/Migration.php
+++ src/Tempest/Database/Migration.php
@@ -4,14 +4,11 @@ declare(strict_types=1);
 
 namespace Tempest\Database;
 
-use Tempest\Database\QueryStatements\CreateTableStatement;
-use Tempest\Database\QueryStatements\DropTableStatement;
-
 interface Migration
 {
     public function getName(): string;
 
-    public function up(): CreateTableStatement|null;
+    public function up(): QueryStatement|null;
 
-    public function down(): DropTableStatement|null;
+    public function down(): QueryStatement|null;
 }
