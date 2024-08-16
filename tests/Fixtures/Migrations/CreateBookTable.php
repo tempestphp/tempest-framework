55e4cb35156d4990861eea2038ef84b73779bfe3
Brent Roose <brent.roose@gmail.com>
2024-08-16 10:04:31 +0200

    Update test migration

--- tests/Fixtures/Migrations/CreateBookTable.php
+++ tests/Fixtures/Migrations/CreateBookTable.php
@@ -5,12 +5,8 @@ declare(strict_types=1);
 namespace Tests\Tempest\Fixtures\Migrations;
 
 use Tempest\Database\Migration;
-use Tempest\Database\QueryStatements\BelongsToStatement;
 use Tempest\Database\QueryStatements\CreateTableStatement;
 use Tempest\Database\QueryStatements\DropTableStatement;
-use Tempest\Database\QueryStatements\PrimaryKeyStatement;
-use Tempest\Database\QueryStatements\RawStatement;
-use Tempest\Database\QueryStatements\TextStatement;
 
 final readonly class CreateBookTable implements Migration
 {
@@ -21,15 +17,11 @@ final readonly class CreateBookTable implements Migration
 
     public function up(): CreateTableStatement|null
     {
-        return new CreateTableStatement(
-            'Book',
-            [
-                new PrimaryKeyStatement(),
-                new TextStatement('title'),
-                new RawStatement('author_id INTEGER'),
-                new BelongsToStatement('Book.author_id', 'Author.id'),
-            ]
-        );
+        return (new CreateTableStatement('Book'))
+            ->primary()
+            ->text('title')
+            ->integer('author_id', nullable: true)
+            ->belongsTo('Book.author_id', 'Author.id');
     }
 
     public function down(): DropTableStatement|null
