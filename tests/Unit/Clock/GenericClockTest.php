c0d3cd1bc428a830d88087707f6f03c4a5e442de
Aidan Casey <aidan@caseyhouse.net>
2024-09-11 13:48:53 -0500

    Adds styling.

--- tests/Unit/Clock/GenericClockTest.php
+++ tests/Unit/Clock/GenericClockTest.php
@@ -1,14 +1,20 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tests\Tempest\Unit\Clock;
 
 use DateTimeImmutable;
 use PHPUnit\Framework\TestCase;
 use Tempest\Clock\GenericClock;
 
+/**
+ * @internal
+ * @small
+ */
 final class GenericClockTest extends TestCase
 {
-    public function test_that_generic_clock_returns_the_current_date_time()
+    public function test_that_generic_clock_returns_the_current_date_time(): void
     {
         $clock = new GenericClock();
 
@@ -23,7 +29,7 @@ final class GenericClockTest extends TestCase
         $this->assertLessThan($dateTimeAfter, $clockDateTime);
     }
 
-    public function test_that_generic_clock_returns_the_current_time()
+    public function test_that_generic_clock_returns_the_current_time(): void
     {
         $clock = new GenericClock();
 
@@ -38,11 +44,11 @@ final class GenericClockTest extends TestCase
         $this->assertLessThan($timeAfter, $clockTime);
     }
 
-    public function test_that_generic_clock_sleeps()
+    public function test_that_generic_clock_sleeps(): void
     {
         $timeBefore = time();
 
-        (new GenericClock)->sleep(1);
+        (new GenericClock())->sleep(1);
 
         $timeAfter = time();
 
--- tests/Unit/Clock/MockClockTest.php
+++ tests/Unit/Clock/MockClockTest.php
@@ -1,10 +1,15 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tests\Tempest\Unit\Clock;
 
 use PHPUnit\Framework\TestCase;
 
+/**
+ * @internal
+ * @small
+ */
 final class MockClockTest extends TestCase
 {
-
 }
