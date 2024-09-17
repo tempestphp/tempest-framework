e1ddad36a86858874d1ffb7ccae7aa2815ebd8e1
Yassine Beloued <108750248+yassiNebeL@users.noreply.github.com>
2024-09-17 16:13:05 +0100

    Create FileUploadCaster.php

---
+++ src/Tempest/Mapper/src/Casters/FileUploadCaster.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Casters;
+
+use Tempest\Http\Upload;
+use Tempest\Mapper\Caster;
+
+class FileUploadCaster implements Caster
+{
+    public function cast(mixed $input): ?string
+    {
+        if (!$input instanceof Upload) {
+            throw new InvalidArgumentException('Expected an instance of Tempest\Http\Upload');
+        }
+
+        if ($value->getError() === UPLOAD_ERR_OK) {
+            return $input->getClientFilename();
+        }
+
+        return null;
+    }
+}
