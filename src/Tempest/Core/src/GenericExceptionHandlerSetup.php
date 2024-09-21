acf12f1ac7f0cf08eea5454690d265b662fd2a16
brendt <brent.roose@gmail.com>
2024-09-21 09:44:26 +0200

    wip

--- src/Tempest/Core/src/GenericExceptionHandlerSetup.php
+++ src/Tempest/Core/src/GenericExceptionHandlerSetup.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Tempest\Core;
 
 use NunoMaduro\Collision\Provider as Collision;
+use Throwable;
 use Whoops\Handler\PrettyPageHandler;
 use Whoops\Run;
 
@@ -25,8 +26,8 @@ final readonly class GenericExceptionHandlerSetup implements ExceptionHandlerSet
 
         // Production web
         if ($appConfig->environment->isProduction()) {
-            set_exception_handler($this->renderErrorPage(...));
-            /** @phpstan-ignore-next-line  */
+            set_exception_handler($this->renderExceptionPage(...));
+            /** @phpstan-ignore-next-line */
             set_error_handler($this->renderErrorPage(...));
 
             return;
@@ -38,8 +39,38 @@ final readonly class GenericExceptionHandlerSetup implements ExceptionHandlerSet
         $whoops->register();
     }
 
-    public function renderErrorPage(): void
+    public function renderExceptionPage(Throwable $throwable): void
     {
+        ll($throwable);
+
+        ob_start();
+
+        if (! headers_sent()) {
+            http_response_code(500);
+        }
+
+        echo file_get_contents(__DIR__ . '/500.html');
+
+        ob_end_flush();
+
+        exit;
+    }
+
+    public function renderErrorPage(
+        int $errNo,
+        string $errstr,
+        string $errFile,
+        int $errLine,
+    ): void {
+        if (
+            $errNo === E_USER_WARNING
+            || $errNo === E_DEPRECATED
+        ) {
+            ll("{$errFile}:{$errLine} {$errstr} ({$errNo})");
+
+            return;
+        }
+
         ob_start();
 
         if (! headers_sent()) {
