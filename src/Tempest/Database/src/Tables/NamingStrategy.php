b8293f81114a1a0604cf237c61ff2a16c5241ede
Enzo Innocenzi <enzo@innocenzi.dev>
2024-09-21 18:23:18 +0200

    style: use proper phpdoc for `getName` parameters

--- src/Tempest/Database/src/Tables/NamingStrategy.php
+++ src/Tempest/Database/src/Tables/NamingStrategy.php
@@ -6,6 +6,6 @@ namespace Tempest\Database\Tables;
 
 interface NamingStrategy
 {
-    /** @var class-string<\Tempest\Database\DatabaseModel> $model */
+    /** @param class-string<\Tempest\Database\DatabaseModel> $model */
     public function getName(string $model): string;
 }
