4ecf638b2bfc88491349535840d8fd3b24479069
Aidan Casey <aidan@caseyhouse.net>
2024-09-22 15:16:26 -0400

    Adds permission and directory helpers.

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require 'vendor/autoload.php';
-
-$filesystem = new Tempest\Filesystem\LocalFilesystem();
-
-$filesystem->traverseDirectory(__DIR__ . '/src', function (SplFileInfo $file) {
-    echo $file->getPathname();
-}, false);
