e88bee6845ddf5f9362e6b9ba82c36e741ba287f
brendt <brent.roose@gmail.com>
2024-09-24 19:33:45 +0200

    Refactor to interface + trait

--- src/Tempest/Cache/src/Cache.php
+++ src/Tempest/Cache/src/Cache.php
@@ -1,62 +1,21 @@
 <?php
 
-declare(strict_types=1);
-
 namespace Tempest\Cache;
 
 use Closure;
 use DateTimeInterface;
-use Psr\Cache\CacheItemPoolInterface;
 use Symfony\Component\Cache\CacheItem;
 
-final readonly class Cache
+interface Cache
 {
-    public function __construct(
-        private CacheItemPoolInterface $pool,
-    ) {
-    }
+    public function put(string $key, mixed $value, ?DateTimeInterface $expiresAt = null): CacheItem;
 
-    public function put(string $key, mixed $value, ?DateTimeInterface $expiresAt = null): CacheItem
-    {
-        $item = $this->pool
-            ->getItem($key)
-            ->set($value);
-
-        if ($expiresAt !== null) {
-            $item = $item->expiresAt($expiresAt);
-        }
-
-        $this->pool->save($item);
-
-        return $item;
-    }
-
-    public function get(string $key): mixed
-    {
-        return $this->pool->getItem($key)->get();
-    }
+    public function get(string $key): mixed;
 
     /** @param Closure(): mixed $cache */
-    public function resolve(string $key, Closure $cache, DateTimeInterface $expiresAt = null): mixed
-    {
-        $item = $this->pool->getItem($key);
+    public function resolve(string $key, Closure $cache, DateTimeInterface $expiresAt = null): mixed;
 
-        if (! $item->isHit()) {
-            $item = $this->put($key, $cache(), $expiresAt);
-        }
+    public function remove(string $key): void;
 
-        return $item->get();
-    }
-
-    public function remove(string $key): void
-    {
-        $this->pool->deleteItem($key);
-    }
-
-    public function clear(): void
-    {
-        if (! $this->pool->clear()) {
-            throw new CouldNotClearCache();
-        }
-    }
+    public function clear(): void;
 }
