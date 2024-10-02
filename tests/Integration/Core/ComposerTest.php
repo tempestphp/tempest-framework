c7e8a24a4d9f5fffd5de2248ea46a69af2bcd1ec
Enzo Innocenzi <enzo@innocenzi.dev>
2024-10-02 20:54:57 +0200

    test: cover `Composer` util

---
+++ tests/Integration/Core/ComposerTest.php
@@ -0,0 +1,94 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Tempest\Integration\Core;
+
+use PHPUnit\Framework\Attributes\Test;
+use Tempest\Core\Composer;
+use Tests\Tempest\Integration\FrameworkIntegrationTestCase;
+
+/**
+ * @internal
+ */
+final class ComposerTest extends FrameworkIntegrationTestCase
+{
+    private function initializeComposer(array $composer): Composer
+    {
+        file_put_contents(__DIR__ . '/../../Fixtures/Core/Composer/composer.json', json_encode($composer, JSON_PRETTY_PRINT));
+
+        return new Composer(
+            root: realpath(__DIR__ . '/../../Fixtures/Core/Composer')
+        );
+    }
+
+    #[Test]
+    public function takes_first_namespace_as_main(): void
+    {
+        $composer = $this->initializeComposer([
+            'autoload' => [
+                'psr-4' => [
+                    'App\\' => 'app/',
+                ],
+            ],
+        ]);
+
+        $this->assertSame('App\\', $composer->mainNamespace);
+        $this->assertSame('app/', $composer->mainNamespacePath);
+        $this->assertSame(["App\\" => 'app/'], $composer->namespaces);
+    }
+
+    #[Test]
+    public function loads_composer_class_with_multiple_namespaces(): void
+    {
+        $composer = $this->initializeComposer([
+            'autoload' => [
+                'psr-4' => [
+                    'Module\\' => 'src/Module/',
+                    'Module\\Foo\\' => 'src/Module/Foo/',
+                    'Module\\Bar\\' => 'src/Module/Bar/',
+                ],
+            ],
+        ]);
+
+        $this->assertSame('Module\\', $composer->mainNamespace);
+        $this->assertSame('src/Module/', $composer->mainNamespacePath);
+        $this->assertSame([
+            'Module\\' => 'src/Module/',
+            'Module\\Foo\\' => 'src/Module/Foo/',
+            'Module\\Bar\\' => 'src/Module/Bar/',
+        ], $composer->namespaces);
+    }
+
+    #[Test]
+    public function takes_app_namespace_in_priority(): void
+    {
+        $composer = $this->initializeComposer([
+            'autoload' => [
+                'psr-4' => [
+                    'Config\\' => 'config/',
+                    'App\\' => 'app/',
+                ],
+            ],
+        ]);
+
+        $this->assertSame('App\\', $composer->mainNamespace);
+        $this->assertSame('app/', $composer->mainNamespacePath);
+    }
+
+    #[Test]
+    public function takes_src_namespace_in_priority(): void
+    {
+        $composer = $this->initializeComposer([
+            'autoload' => [
+                'psr-4' => [
+                    'Config\\' => 'config/',
+                    'App\\' => 'src/',
+                ],
+            ],
+        ]);
+
+        $this->assertSame('App\\', $composer->mainNamespace);
+        $this->assertSame('src/', $composer->mainNamespacePath);
+    }
+}
