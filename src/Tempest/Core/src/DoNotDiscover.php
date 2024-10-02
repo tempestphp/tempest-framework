09e28ff6bb1ec89241a73b0cf1f72919eeeb68d8
Enzo Innocenzi <enzo@innocenzi.dev>
2024-10-02 22:52:14 +0200

    feat(discovery): allow exceptions on `DoNotDiscover` classes

--- src/Tempest/Core/src/DoNotDiscover.php
+++ src/Tempest/Core/src/DoNotDiscover.php
@@ -9,4 +9,12 @@ use Attribute;
 #[Attribute(Attribute::TARGET_CLASS)]
 final class DoNotDiscover
 {
+    public function __construct(
+        /**
+         * Allows the specified `Discovery` classes to still discover this class.
+         * @var array<class-string<\Tempest\Core\Discovery>>
+         */
+        public readonly array $except = [],
+    ) {
+    }
 }
