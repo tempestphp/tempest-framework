b53ce6a798e17fb55b3370f40f8de477422191ab
Enzo Innocenzi <enzo@innocenzi.dev>
2024-10-04 20:08:14 +0200

    feat(event-bus): support closure-based listeners

--- src/Tempest/EventBus/src/EventBus.php
+++ src/Tempest/EventBus/src/EventBus.php
@@ -4,7 +4,11 @@ declare(strict_types=1);
 
 namespace Tempest\EventBus;
 
+use Closure;
+
 interface EventBus
 {
     public function dispatch(string|object $event): void;
+
+    public function listen(string|object $event, Closure $handler): void;
 }
---
+++ src/Tempest/EventBus/tests/Fixtures/MyService.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tempest\EventBus\Tests\Fixtures;
+
+final class MyService
+{
+    public function __construct(
+        public readonly string $value
+    ) {
+    }
+}
