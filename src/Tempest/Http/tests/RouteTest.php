80b3d4902d29b0f15d205324554347b0fb644cd0
Sergiu Daniel Vidican <vsergiu93@gmail.com>
2024-10-06 00:44:58 +0300

    test: start adding unit test for route as it is easier to write more test cases than the integration test

---
+++ src/Tempest/Http/tests/RouteTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tempest\Http\Tests;
+
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\TestCase;
+use Tempest\Http\Method;
+use Tempest\Http\Route;
+
+/**
+ * @internal
+ */
+
+final class RouteTest extends TestCase
+{
+    #[DataProvider('uri_provider')]
+    public function test_matching_regex(string $uri, string $expected): void
+    {
+        $route = new Route($uri, Method::GET);
+        $this->assertEquals($expected, $route->matchingRegex);
+    }
+
+    public static function uri_provider(): array
+    {
+        return [
+            'static route' => ['/foo', '/foo\/?'],
+            'dynamic route' => ['/foo/{bar}', '/foo/([^/]++)\/?'],
+            'dynamic route custom regex' => ['/foo/{bar:.*}', '/foo/(.*)\/?'],
+            'dynamic route custom regex and nested {}' => ['/foo/{bar:a{3}}', '/foo/(a{3})\/?'],
+            'dynamic route with broken custom regex' => ['/foo/{bar: {bar}}', '/foo/( {bar})\/?'],
+            'dynamic route custom regex and nested group' => ['/foo/{bar:id_([0-9]+)}', '/foo/(id_([0-9]+))\/?'],
+        ];
+    }
+}
