1bcbfa4640a69e5d9d783eb03d8e27d8c2abf10a
Sergiu Daniel Vidican <vsergiu93@gmail.com>
2024-10-07 00:54:48 +0300

    test: add some extra test case for matching regex and some two new test cases

--- src/Tempest/Http/tests/RouteTest.php
+++ src/Tempest/Http/tests/RouteTest.php
@@ -22,6 +22,18 @@ final class RouteTest extends TestCase
         $this->assertEquals($expected, $route->matchingRegex);
     }
 
+    public function test_correctly_identifies_static_route(): void
+    {
+        $route = new Route('/foo', Method::GET);
+        $this->assertFalse($route->isDynamic);
+    }
+
+    public function test_correctly_identifies_dynamic_route(): void
+    {
+        $route = new Route('/{foo}', Method::GET);
+        $this->assertTrue($route->isDynamic);
+    }
+
     public static function uri_provider(): array
     {
         return [
@@ -29,7 +41,7 @@ final class RouteTest extends TestCase
             'dynamic route' => ['/foo/{bar}', '/foo/([^/]++)\/?'],
             'dynamic route custom regex' => ['/foo/{bar:.*}', '/foo/(.*)\/?'],
             'dynamic route custom regex and nested {}' => ['/foo/{bar:a{3}}', '/foo/(a{3})\/?'],
-            'dynamic route with broken custom regex' => ['/foo/{bar: {bar}}', '/foo/( {bar})\/?'],
+            'dynamic route with broken custom regex' => ['/foo/{bar: {bar}}', '/foo/({bar})\/?'],
             'dynamic route custom regex and nested group' => ['/foo/{bar:id_([0-9]+)}', '/foo/(id_([0-9]+))\/?'],
         ];
     }
