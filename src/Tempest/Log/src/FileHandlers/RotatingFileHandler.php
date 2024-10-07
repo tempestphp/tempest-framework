14fea7d1594a3683d0a0318dff2e2113dd7577ed
Caffeinated Coder <Phil-Venter@users.noreply.github.com>
2024-10-07 13:31:37 +0200

    fix(#275): implement weekly log rotation (#548)
    Co-authored-by: Brent Roose <brent.roose@gmail.com>

---
+++ src/Tempest/Log/src/FileHandlers/RotatingFileHandler.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tempest\Log\FileHandlers;
+
+use DateTimeImmutable;
+use InvalidArgumentException;
+use Monolog\Handler\RotatingFileHandler as MonoRotatingFileHandler;
+
+final class RotatingFileHandler extends MonoRotatingFileHandler
+{
+    public const string FILE_PER_WEEK = 'Y-W';
+
+    protected function setDateFormat(string $dateFormat): void
+    {
+        if (0 === preg_match('{^[Yy](([/_.-]?m([/_.-]?d)?)|([/_.-]?W))?$}', $dateFormat)) {
+            throw new InvalidArgumentException(
+                'Invalid date format - format must be one of '.
+                'RotatingFileHandler::FILE_PER_DAY ("Y-m-d"), RotatingFileHandler::FILE_PER_WEEK ("Y-W"), '.
+                'RotatingFileHandler::FILE_PER_MONTH ("Y-m") or RotatingFileHandler::FILE_PER_YEAR ("Y"), '.
+                'or you can set one of the date formats using slashes, underscores and/or dots instead of dashes.'
+            );
+        }
+
+        $this->dateFormat = $dateFormat;
+    }
+
+    protected function getNextRotation(): DateTimeImmutable
+    {
+        return match (str_replace(['/','_','.'], '-', $this->dateFormat)) {
+            self::FILE_PER_WEEK => (new DateTimeImmutable('first day of next week'))->setTime(0, 0, 0),
+            self::FILE_PER_MONTH => (new DateTimeImmutable('first day of next month'))->setTime(0, 0, 0),
+            self::FILE_PER_YEAR => (new DateTimeImmutable('first day of January next year'))->setTime(0, 0, 0),
+            default => (new DateTimeImmutable('tomorrow'))->setTime(0, 0, 0),
+        };
+    }
+}
