5d4d1b83e0d2b9f7768c021f8e572eddba088c15
Hanie Asemi <hanieasemi76@gmail.com>
2024-10-08 18:10:10 +0330

    feat: add boolean data type (#547)

---
+++ src/Tempest/Database/src/QueryStatements/BooleanStatement.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tempest\Database\QueryStatements;
+
+use Tempest\Database\DatabaseDialect;
+use Tempest\Database\QueryStatement;
+
+final readonly class BooleanStatement implements QueryStatement
+{
+    public function __construct(
+        private string $name,
+        private bool $nullable = false,
+        private ?bool $default = null,
+    ) {
+    }
+
+    public function compile(DatabaseDialect $dialect): string
+    {
+        return sprintf(
+            '`%s` BOOLEAN %s %s',
+            $this->name,
+            $this->default ? "DEFAULT {$this->default}" : '',
+            $this->nullable ? '' : 'NOT NULL',
+        );
+    }
+}
