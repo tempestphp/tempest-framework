01c3e5bb9bd161a84aff628ddcb8da231518a4d0
NeoIsRecursive <neo.leijondahl@gmail.com>
2024-10-08 21:17:35 +0200

    cleanup middleware

--- src/Tempest/Http/src/ResolveRequestMiddleware.php
+++ src/Tempest/Http/src/ResolveRequestMiddleware.php
@@ -19,36 +19,48 @@ final readonly class ResolveRequestMiddleware implements HttpMiddleware
 
     public function __invoke(Request $request, callable $next): Response
     {
-        $requestClass = $request::class;
-
         // Let's find out if our input request data matches what the route's action needs
         try {
-            // We'll loop over all the handler's parameters
-            foreach ($this->container->get(MatchedRoute::class)->route->handler->getParameters() as $parameter) {
+            $wantedRequestClass = $this->getRequestClassFromRoute($this->container->get(MatchedRoute::class));
 
-                // If the parameter's type is an instance of Request…
-                if ($parameter->getType()->matches(Request::class)) {
-                    // We'll use that specific request class
-                    $requestClass = $parameter->getType()->getName();
-
-                    break;
-                }
+            if ($wantedRequestClass === null) {
+                return $next($request);
             }
 
             // If the request class is different from the one we have, we'll map it to the new one
-            if ($request::class !== $requestClass) {
+            if ($request::class !== $wantedRequestClass) {
                 /** @var Request */
-                $request = map($this->container->get(PsrRequest::class))->to($requestClass);
+                $request = map($this->container->get(PsrRequest::class))->to($wantedRequestClass);
             }
 
+            // We'll bind the request to the container
             $this->container->singleton(Request::class, fn () => $request);
             $this->container->singleton($request::class, fn () => $request);
 
+            // Then we'll validate the request
             $request->validate();
 
             return $next($request);
         } catch (ValidationException $validationException) {
             return new Invalid($request, $validationException->failingRules);
         }
+    }
+
+    /**
+     * @return class-string<Request>|null
+     */
+    private function getRequestClassFromRoute(MatchedRoute $matchedRoute): ?string
+    {
+        // We'll loop over all the handler's parameters
+        foreach ($matchedRoute->route->handler->getParameters() as $parameter) {
+
+            // If the parameter's type is an instance of Request…
+            if ($parameter->getType()->matches(Request::class)) {
+                // We'll use that specific request class
+                return $parameter->getType()->getName();
+            }
+        }
+
+        return null;
     }
 }
