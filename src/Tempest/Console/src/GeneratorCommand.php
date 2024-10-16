66b6d0e28111f20cabd42b9c79ca0e4404e22d0c
Guillaume Turpin <guillaume.turpin45@gmail.com>
2024-10-16 12:07:28 +0200

    :sparkles: Handle specific handlers for generator commands

--- src/Tempest/Console/src/GeneratorCommand.php
+++ src/Tempest/Console/src/GeneratorCommand.php
@@ -4,8 +4,42 @@ declare(strict_types=1);
 
 namespace Tempest\Console;
 
+use Tempest\Reflection\MethodReflector;
+
+use function Tempest\get;
+
 /**
  * Defines a console command that is specifically for generating files.
  */
 #[\Attribute(\Attribute::TARGET_METHOD)]
-final class GeneratorCommand extends ConsoleCommand {}
+final class GeneratorCommand extends ConsoleCommand {
+    /**
+     * The command handler method that's binded to the attribute.
+     * Used to be passed to the handlerCallback method.
+     */
+    protected MethodReflector $commandHandler;
+    
+    public function setHandler(MethodReflector $handler): self
+    {
+        $this->commandHandler = $handler;
+        $this->handler        = MethodReflector::fromParts($this, 'handlerCallback');
+
+        return $this;
+    }
+
+    /**
+     * The handler callback that will proxy the command to run multiple handlers itself.
+     *
+     * @param array<mixed> $params The parameters passed to the command.
+     */
+    public function handlerCallback(array $params): void
+    {
+        $commandInstance   = get($this->commandHandler->getDeclaringClass()->getName());
+        $stubFileGenerator = $this->commandHandler->invokeArgs(
+            $commandInstance,
+            $params
+        );
+
+        $stubFileGenerator->generate();
+    }
+}
--- src/Tempest/Generation/src/StubFileGenerator.php
+++ src/Tempest/Generation/src/StubFileGenerator.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace Tempest\Generation;
 
+use Tempest\Console\Console;
+
+use function Tempest\get;
+
 /**
  * This class can generate a file from a stub file with additional useful methods.
  * It only works with PHP class files.
@@ -11,19 +15,25 @@ namespace Tempest\Generation;
 final class StubFileGenerator
 {
     /**
-     * @param string $className The final class name for the generated file.
-     * @param class-string $stubFile The stub file to use for the generation.
      * @param string $targetPath The path where the generated file will be saved including the filename and extension.
+     * @param class-string $stubFile The stub file to use for the generation.
      * @param array<string, string> $replacements An array of key-value pairs to replace in the stub file.
      *     The keys are the placeholders in the stub file (e.g. 'DummyNamespace')
      *     The values are the replacements for the placeholders (e.g. 'App\Models')
      * @param boolean $shouldOverride Whether the generator should override the file if it already exists.
      */
     public function __construct(
-        protected readonly string $className,
-        protected readonly string $stubFile,
         protected readonly string $targetPath,
+        protected readonly string $stubFile,
         protected readonly array $replacements = [],
         protected readonly bool $shouldOverride = false,
     ) {}
+
+    public function generate(): void {
+        $console = get(Console::class);
+
+        $console->success('File generated successfully!');
+
+        dd('Generating file...' );
+    }
 }
