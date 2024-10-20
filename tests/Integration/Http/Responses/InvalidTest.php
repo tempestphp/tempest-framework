540dbe1d4b3a6dca0a34027b79d1f4cbe6318983
NeoIsRecursive <neo.leijondahl@gmail.com>
2024-10-20 19:50:57 +0200

    Add test

--- tests/Integration/Http/Responses/InvalidTest.php
+++ tests/Integration/Http/Responses/InvalidTest.php
@@ -20,11 +20,39 @@ use Tests\Tempest\Integration\FrameworkIntegrationTestCase;
  */
 final class InvalidTest extends FrameworkIntegrationTestCase
 {
-    public function test_invalid(): void
+    public function test_invalid_with_psr_request(): void
     {
         /** @var PsrRequest $request */
         $request = map(new GenericRequest(Method::GET, '/original', ['foo' => 'bar']))->with(RequestToPsrRequestMapper::class);
         $request = $request->withHeader('Referer', '/original');
+
+        $response = new Invalid(
+            $request,
+            [
+                'foo' => [
+                    new NotEmpty(),
+                ],
+            ]
+        );
+
+        $this->assertSame(Status::FOUND, $response->getStatus());
+        $this->assertSame('/original', $response->getHeader('Location')->values[0]);
+
+        $session = $this->container->get(Session::class);
+
+        $this->assertArrayHasKey('foo', $session->get(Session::VALIDATION_ERRORS));
+        $this->assertArrayHasKey('foo', $session->get(Session::ORIGINAL_VALUES));
+    }
+
+    public function test_invalid_with_request()
+    {
+        $request = new GenericRequest(
+            method: Method::GET,
+            uri: '/original',
+            body: ['foo' => 'bar'],
+            headers: ['referer' => '/original']
+        );
+
 
         $response = new Invalid(
             $request,
