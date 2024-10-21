e6d78b8bb4fa8d35e0a43f9e0748dbdaa173e7e3
Guillaume Turpin <guillaume.turpin45@gmail.com>
2024-10-21 19:42:30 +0200

    :recycle: Refactor factory to GeneratorCommand specific method

--- src/Tempest/Console/src/GeneratorCommand.php
+++ src/Tempest/Console/src/GeneratorCommand.php
@@ -4,8 +4,33 @@ declare(strict_types=1);
 
 namespace Tempest\Console;
 
+use Tempest\Generation\StubFileGenerator;
+
+use function Tempest\get;
+use function Tempest\Support\arr;
+
 /**
  * Defines a console command that is specifically for generating files.
  */
 #[\Attribute(\Attribute::TARGET_METHOD)]
-final class GeneratorCommand extends ConsoleCommand {}
+final class GeneratorCommand extends ConsoleCommand {
+    /**
+     * Make the handler for the given command.
+     * This allow the console to run this handler without altering the command structure.
+     *
+     * @return \Closure(array<mixed> $params) The command handler.
+     */
+    public function makeHandler(): \Closure {
+        return function (array $params) {
+            // Resolve all generators and run them.
+            arr(
+                $this->handler->invokeArgs(
+                    get($this->handler->getDeclaringClass()->getName()),
+                    $params
+                )
+            )
+                ->filter(fn($generator) => $generator instanceof StubFileGenerator)
+                ->each(fn(StubFileGenerator $generator) => $generator->generate());
+        };
+    }
+}
