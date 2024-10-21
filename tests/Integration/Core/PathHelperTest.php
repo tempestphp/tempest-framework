5a91c7ee4eb9733e0a1962b0bc3bd81508991492
NeoIsRecursive <neo.leijondahl@gmail.com>
2024-10-21 23:18:15 +0200

    Fix tests for winblows (i hope)

--- tests/Integration/Core/PathHelperTest.php
+++ tests/Integration/Core/PathHelperTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Tests\Tempest\Integration\Core;
 
 use function Tempest\base_path;
+use function Tempest\path;
 use Tests\Tempest\Integration\FrameworkIntegrationTestCase;
 
 /**
@@ -14,7 +15,7 @@ final class PathHelperTest extends FrameworkIntegrationTestCase
 {
     public function test_can_get_base_path(): void
     {
-        $this->assertSame(realpath($this->root), base_path());
-        $this->assertSame(realpath($this->root . '/tests/Fixtures'), base_path('/tests/Fixtures'));
+        $this->assertSame(path(realpath($this->root)), base_path());
+        $this->assertSame(path(realpath($this->root . '/tests/Fixtures')), base_path('/tests/Fixtures'));
     }
 }
