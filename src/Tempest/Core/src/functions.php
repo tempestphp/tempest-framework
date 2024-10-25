0ec8ae909aff2717dc1862fcdc77ed21c98c15fc
Aidan Casey <aidan@caseyhouse.net>
2024-10-24 21:51:36 -0400

    chore: remove test dependencies

--- src/Tempest/Core/src/functions.php
+++ src/Tempest/Core/src/functions.php
@@ -3,11 +3,6 @@
 declare(strict_types=1);
 
 namespace Tempest {
-    function testFunction(): string
-    {
-        return 'a-test';
-    }
-
     function path(string ...$parts): string
     {
         $path = implode('/', $parts);
