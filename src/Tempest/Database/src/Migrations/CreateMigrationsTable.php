3c94f124a0165b003e98ba0819f700a5c5bd1aaf
Guillaume Turpin <guillaume.turpin45@gmail.com>
2024-10-28 17:32:25 +0100

    :bug: Fix issue

--- src/Tempest/Database/src/Migrations/CreateMigrationsTable.php
+++ src/Tempest/Database/src/Migrations/CreateMigrationsTable.php
@@ -4,17 +4,14 @@ declare(strict_types=1);
 
 namespace Tempest\Database\Migrations;
 
-use Tempest\Core\CanBePublished;
 use Tempest\Core\DoNotDiscover;
-use Tempest\Core\PublishDiscovery;
 use Tempest\Database\Migration;
 use Tempest\Database\Migrations\Migration as Model;
 use Tempest\Database\QueryStatement;
 use Tempest\Database\QueryStatements\CreateTableStatement;
 use Tempest\Database\QueryStatements\DropTableStatement;
 
-#[CanBePublished]
-#[DoNotDiscover(except: [PublishDiscovery::class])]
+#[DoNotDiscover]
 final readonly class CreateMigrationsTable implements Migration
 {
     public function getName(): string
