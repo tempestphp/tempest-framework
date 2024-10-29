082695e79b8121023b32379e6c658cc0c7d42958
Brent Roose <brent.roose@gmail.com>
2024-10-29 13:13:51 +0100

    Add failing test

---
+++ tests/Fixtures/Console/CommandWithArgumentName.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Tempest\Fixtures\Console;
+
+use Tempest\Console\ConsoleArgument;
+use Tempest\Console\ConsoleCommand;
+use Tempest\Console\HasConsole;
+
+final readonly class CommandWithArgumentName
+{
+    use HasConsole;
+
+    #[ConsoleCommand(name: 'command-with-argument-name')]
+    public function __invoke(
+        #[ConsoleArgument(name: 'new-name')]
+        string $input,
+        #[ConsoleArgument(name: 'new-flag')]
+        bool $flag = false,
+    ): void
+    {
+        $this->writeln($input);
+        $this->writeln($flag ? 'true' : 'false');
+    }
+}
