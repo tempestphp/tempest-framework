a31d41df7c26a9b8e975bdd5a261c717490fec01
Guillaume Turpin <guillaume.turpin45@gmail.com>
2024-11-04 15:29:54 +0100

    :sparkles: Refactor and add manipulations on the fly

--- src/Tempest/Generation/src/StubFileGenerator.php
+++ src/Tempest/Generation/src/StubFileGenerator.php
@@ -4,11 +4,12 @@ declare(strict_types=1);
 
 namespace Tempest\Generation;
 
-use Tempest\Console\Console;
-use Tempest\Generation\Exceptions\StubFileGenerationFailedException;
-use Tempest\Support\PathHelper;
-use Tempest\Support\StringHelper;
 use Throwable;
+use Tempest\Support\StringHelper;
+use Tempest\Support\PathHelper;
+use Tempest\Generation\Exceptions\FileGenerationFailedException;
+use Tempest\Console\Console;
+use Closure;
 
 /**
  * This class can generate a file from a stub file with additional useful methods.
@@ -27,17 +28,19 @@ final class StubFileGenerator
      * @param array<string, string> $replacements An array of key-value pairs to replace in the stub file.
      *     The keys are the placeholders in the stub file (e.g. 'DummyNamespace')
      *     The values are the replacements for the placeholders (e.g. 'App\Models')
+     * @param array<Closure> $manipulations An array of manipulations to apply to the generated class.
      * @param bool $shouldOverride Whether the generator should override the file if it already exists.
      */
     public function generate(
         string $targetPath,
         string $stubFile,
         array $replacements = [],
+        array $manipulations = [],
         bool $shouldOverride = false,
     ): void {
         try {
             $this->prepareFilesystem($targetPath, $shouldOverride);
-            $file_path = $this->writeFile($targetPath, $stubFile, $replacements);
+            $file_path = $this->writeFile($targetPath, $stubFile, $replacements, $manipulations);
 
             $this->console->success(sprintf('File successfully created at "%s".', $file_path));
         } catch (Throwable $throwable) {
@@ -51,6 +54,8 @@ final class StubFileGenerator
      *
      * @param string $targetPath The path where the generated file will be saved including the filename and extension.
      * @param bool $shouldOverride Whether the generator should override the file if it already exists.
+     * 
+     * @throws FileGenerationFailedException If the operation has been aborted.
      */
     private function prepareFilesystem(
         string $targetPath,
@@ -59,15 +64,16 @@ final class StubFileGenerator
         // Delete the file if it exists and we force the override
         if (file_exists($targetPath)) {
             if (! $shouldOverride) {
-                return;
+                throw new FileGenerationFailedException('The operation has been aborted.');
             }
 
             @unlink($targetPath);
         }
 
         // Recursively create directories before writing the file
-        if (! file_exists(dirname($targetPath))) {
-            mkdir(dirname($targetPath), recursive: true);
+        $directory = dirname($targetPath);
+        if (! is_dir($directory)) {
+            mkdir($directory, recursive: true);
         }
     }
 
@@ -79,8 +85,9 @@ final class StubFileGenerator
      * @param array<string, string> $replacements An array of key-value pairs to replace in the stub file.
      *     The keys are the placeholders in the stub file (e.g. 'DummyNamespace')
      *     The values are the replacements for the placeholders (e.g. 'App\Models')
-     *
-     * @throws StubFileGenerationFailedException If the file could not be written.
+     * @param array<Closure> $manipulations An array of manipulations to apply to the generated class.
+     * 
+     * @throws FileGenerationFailedException If the file could not be written.
      *
      * @return string The path where the file was written.
      */
@@ -88,6 +95,7 @@ final class StubFileGenerator
         string $targetPath,
         string $stubFile,
         array $replacements = [],
+        array $manipulations = [],
     ): string {
         // Transform stub to class
         $namespace = PathHelper::toRegisteredNamespace($targetPath);
@@ -104,15 +112,11 @@ final class StubFileGenerator
             $classManipulator->manipulate(fn (StringHelper $code) => $code->replace($placeholder, $replacement));
         }
 
-        // Write the file
-        $is_success = (bool) file_put_contents(
-            $targetPath,
-            $classManipulator->print()
-        );
-
-        if (! $is_success) {
-            throw new StubFileGenerationFailedException('The file could not be written.');
+        foreach ($manipulations as $manipulation) {
+            $classManipulator->manipulate($manipulation);
         }
+
+        $classManipulator->save($targetPath);
 
         return $targetPath;
     }
