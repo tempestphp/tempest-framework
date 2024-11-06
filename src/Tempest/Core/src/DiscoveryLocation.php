4320fc5c43742ef43309b805e546efd51619d291
Brent Roose <brent.roose@gmail.com>
2024-11-06 14:09:48 +0100

    wip

--- src/Tempest/Core/src/DiscoveryLocation.php
+++ src/Tempest/Core/src/DiscoveryLocation.php
@@ -14,6 +14,6 @@ final readonly class DiscoveryLocation
 
     public function isVendor(): bool
     {
-        return str_contains($this->path, '/vendor/');
+        return str_contains(str_replace('\\', '/', $this->path), '/vendor/');
     }
 }
