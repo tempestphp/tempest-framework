68d7e5482ff2fc69bdb648272e3dc0686797ceb8
samir <sutikkukun@gmail.com>
2024-11-12 13:19:29 +0100

    feat(log): allow usage of multiple same log channels (#718)

--- src/Tempest/Log/tests/GenericLoggerTest.php
+++ src/Tempest/Log/tests/GenericLoggerTest.php
@@ -35,6 +35,17 @@ final class GenericLoggerTest extends TestCase
         $this->assertStringContainsString('test', file_get_contents($filePath));
     }
 
+    protected function tearDown(): void
+    {
+        $files = glob(__DIR__ . '/logs/*.log');
+
+        foreach ($files as $file) {
+            if (is_file($file)) {
+                unlink($file);
+            }
+        }
+    }
+
     public function test_daily_log_channel_works(): void
     {
         $filePath = __DIR__ . '/logs/tempest-' . date('Y-m-d') . '.log';
@@ -71,5 +82,27 @@ final class GenericLoggerTest extends TestCase
         $this->assertFileExists($filePath);
 
         $this->assertStringContainsString('test', file_get_contents($filePath));
+    }
+
+    public function test_multiple_same_log_channels_works(): void
+    {
+        $filePath = __DIR__ . '/logs/multiple-tempest1.log';
+        $secondFilePath = __DIR__ . '/logs/multiple-tempest2.log';
+
+        $config = new LogConfig(
+            channels: [
+                new AppendLogChannel($filePath),
+                new AppendLogChannel($secondFilePath),
+            ],
+        );
+
+        $logger = new GenericLogger($config);
+        $logger->info('test');
+
+        $this->assertFileExists($filePath);
+        $this->assertStringContainsString('test', file_get_contents($filePath));
+
+        $this->assertFileExists($secondFilePath);
+        $this->assertStringContainsString('test', file_get_contents($secondFilePath));
     }
 }
