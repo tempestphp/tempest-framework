779973e5f2af6cdae75c919561c0da9139eb63fa
Enzo Innocenzi <enzo@innocenzi.dev>
2024-11-14 01:47:01 -0800

    fix(console): handle nested `style` tags (#726)

--- src/Tempest/Console/src/Highlight/DynamicTokenType.php
+++ src/Tempest/Console/src/Highlight/DynamicTokenType.php
@@ -15,7 +15,7 @@ final readonly class DynamicTokenType implements TokenType
     ) {
     }
 
-    public function getStyle(): TerminalStyle
+    public function getBeforeStyle(): TerminalStyle
     {
         $normalizedStyle = str($this->style)
             ->lower()
@@ -32,6 +32,56 @@ final readonly class DynamicTokenType implements TokenType
         }
 
         return TerminalStyle::RESET;
+    }
+
+    public function getAfterStyle(): TerminalStyle
+    {
+        return match ($this->getBeforeStyle()) {
+            // Mods
+            TerminalStyle::BOLD => TerminalStyle::RESET_INTENSITY,
+            TerminalStyle::DIM => TerminalStyle::RESET_INTENSITY,
+            TerminalStyle::ITALIC => TerminalStyle::RESET_ITALIC,
+            TerminalStyle::HIDDEN => TerminalStyle::VISIBLE,
+            TerminalStyle::UNDERLINE => TerminalStyle::RESET_UNDERLINE,
+            TerminalStyle::OVERLINE => TerminalStyle::RESET_OVERLINE,
+            TerminalStyle::STRIKETHROUGH => TerminalStyle::RESET_STRIKETHROUGH,
+            TerminalStyle::REVERSE_TEXT => TerminalStyle::RESET_REVERSE_TEXT,
+            // Foregrounds
+            TerminalStyle::FG_BLACK,
+            TerminalStyle::FG_DARK_RED,
+            TerminalStyle::FG_DARK_GREEN,
+            TerminalStyle::FG_DARK_YELLOW,
+            TerminalStyle::FG_DARK_BLUE,
+            TerminalStyle::FG_DARK_MAGENTA,
+            TerminalStyle::FG_DARK_CYAN,
+            TerminalStyle::FG_LIGHT_GRAY,
+            TerminalStyle::FG_GRAY,
+            TerminalStyle::FG_RED,
+            TerminalStyle::FG_GREEN,
+            TerminalStyle::FG_YELLOW,
+            TerminalStyle::FG_BLUE,
+            TerminalStyle::FG_MAGENTA,
+            TerminalStyle::FG_CYAN,
+            TerminalStyle::FG_WHITE => TerminalStyle::RESET_FOREGROUND,
+            // Backgrounds
+            TerminalStyle::BG_BLACK,
+            TerminalStyle::BG_DARK_RED,
+            TerminalStyle::BG_DARK_GREEN,
+            TerminalStyle::BG_DARK_YELLOW,
+            TerminalStyle::BG_DARK_BLUE,
+            TerminalStyle::BG_DARK_MAGENTA,
+            TerminalStyle::BG_DARK_CYAN,
+            TerminalStyle::BG_LIGHT_GRAY,
+            TerminalStyle::BG_GRAY,
+            TerminalStyle::BG_RED,
+            TerminalStyle::BG_GREEN,
+            TerminalStyle::BG_YELLOW,
+            TerminalStyle::BG_BLUE,
+            TerminalStyle::BG_MAGENTA,
+            TerminalStyle::BG_CYAN,
+            TerminalStyle::BG_WHITE => TerminalStyle::RESET_BACKGROUND,
+            default => TerminalStyle::RESET,
+        };
     }
 
     public function getValue(): string
--- src/Tempest/Console/tests/TempestConsoleLanguage/Injections/DynamicInjectionTest.php
+++ src/Tempest/Console/tests/TempestConsoleLanguage/Injections/DynamicInjectionTest.php
@@ -16,15 +16,18 @@ use Tempest\Highlight\Highlighter;
  */
 final class DynamicInjectionTest extends TestCase
 {
-    #[TestWith(['<style="fg-cyan">foo</style>', "\e[96mfoo\e[0m"])]
-    #[TestWith(['<style="bg-red">foo</style>', "\e[101mfoo\e[0m"])]
-    #[TestWith(['<style="bold">foo</style>', "\e[1mfoo\e[0m"])]
-    #[TestWith(['<style="underline">foo</style>', "\e[4mfoo\e[0m"])]
+    #[TestWith(['<style="fg-cyan">foo</style>', "\e[96mfoo\e[39m"])]
+    #[TestWith(['<style="bg-red">foo</style>', "\e[101mfoo\e[49m"])]
+    #[TestWith(['<style="bold">foo</style>', "\e[1mfoo\e[22m"])]
+    #[TestWith(['<style="underline">foo</style>', "\e[4mfoo\e[24m"])]
     #[TestWith(['<style="reset">foo</style>', "\e[0mfoo\e[0m"])]
-    #[TestWith(['<style="reverse-text">foo</style>', "\e[7mfoo\e[0m"])]
-    #[TestWith(['<style="bg-darkcyan fg-cyan underline">Tempest</style>', "\e[46m\e[96m\e[4mTempest\e[0m\e[0m\e[0m"])]
-    #[TestWith(['<style="bg-dark-cyan fg-cyan underline">Tempest</style>', "\e[46m\e[96m\e[4mTempest\e[0m\e[0m\e[0m"])]
-    #[TestWith(['<style="fg-cyan"><style="bg-dark-red">foo</style></style>', "\e[96m\e[41mfoo\e[0m\e[0m"])]
+    #[TestWith(['<style="reverse-text">foo</style>', "\e[7mfoo\e[27m"])]
+    #[TestWith(['<style="bg-darkcyan fg-cyan underline">Tempest</style>', "\e[46m\e[96m\e[4mTempest\e[49m\e[39m\e[24m"])]
+    #[TestWith(['<style="bg-dark-cyan fg-cyan underline">Tempest</style>', "\e[46m\e[96m\e[4mTempest\e[49m\e[39m\e[24m"])]
+    #[TestWith(['<style="fg-cyan"><style="bg-dark-red">foo</style></style>', "\e[96m\e[41mfoo\e[49m\e[39m"])]
+    #[TestWith(['<style="dim"><style="bg-dark-red fg-white">foo</style></style>', "\e[2m\e[41m\e[97mfoo\e[49m\e[39m\e[22m"])]
+    #[TestWith(['<style="fg-cyan">cyan</style>unstyled<style="bg-dark-red">dark red</style>', "\e[96mcyan\e[39munstyled\e[41mdark red\e[49m"])]
+    #[TestWith(['<style="dim"><style="fg-gray">dim-gray</style> just-gray</style>', "\e[2m\e[90mdim-gray\e[39m just-gray\e[22m"])]
     #[Test]
     public function language(string $content, string $expected): void
     {
