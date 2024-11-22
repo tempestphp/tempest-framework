233bbc4d86c828f757c11fc973ef7626423e0e72
Brent Roose <brent.roose@gmail.com>
2024-11-22 15:08:46 +0100

    wip

--- tests/Integration/View/ViewComponentDiscoveryTest.php
+++ tests/Integration/View/ViewComponentDiscoveryTest.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Tests\Tempest\Integration\View;
 
+use Tempest\Core\DiscoveryItems;
+use Tempest\Core\DiscoveryLocation;
 use Tempest\View\Components\Input;
 use Tempest\View\Exceptions\DuplicateViewComponent;
 use Tempest\View\ViewComponentDiscovery;
@@ -17,9 +19,11 @@ final class ViewComponentDiscoveryTest extends FrameworkIntegrationTestCase
     public function test_duplicates(): void
     {
         $discovery = $this->container->get(ViewComponentDiscovery::class);
+        $discovery->setItems(new DiscoveryItems([]));
 
         try {
-            $discovery->discoverPath(__DIR__ . '/duplicateComponent.view.php');
+            $discovery->discoverPath(new DiscoveryLocation('', ''), __DIR__ . '/duplicateComponent.view.php');
+            $discovery->apply();
         } catch (DuplicateViewComponent $duplicateViewComponent) {
             $this->assertStringContainsString(__DIR__ . '/duplicateComponent.view.php', $duplicateViewComponent->getMessage());
             $this->assertStringContainsString(Input::class, $duplicateViewComponent->getMessage());
