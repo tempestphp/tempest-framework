3bddb1253ac45ab9193623e600b59391e477beb7
Yassine Beloued <108750248+yassiNebeL@users.noreply.github.com>
2024-12-02 17:03:43 +0100

    Update CreateBookTable.php

--- tests/Fixtures/Migrations/CreateBookTable.php
+++ tests/Fixtures/Migrations/CreateBookTable.php
@@ -22,6 +22,7 @@ final readonly class CreateBookTable implements Migration
         return CreateTableStatement::forModel(Book::class)
             ->primary()
             ->text('title')
+            ->text('cover')
             ->belongsTo('books.author_id', 'authors.id', nullable: true);
     }
 
