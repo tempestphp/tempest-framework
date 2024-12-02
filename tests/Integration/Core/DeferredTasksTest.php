42262faea3acdd3685e8f924f4aace5a78f08362
Enzo Innocenzi <enzo@innocenzi.dev>
2024-12-02 15:14:45 -0800

    feat(core): allow `defer` callbacks to receive parameters from container (#798)

--- tests/Integration/Core/DeferredTasksTest.php
+++ tests/Integration/Core/DeferredTasksTest.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Tests\Tempest\Integration\Core;
 
+use Tempest\Container\Container;
 use Tempest\Core\Kernel\FinishDeferredTasks;
+use function Tempest\defer;
 use function Tempest\uri;
 use Tests\Tempest\Fixtures\Controllers\DeferController;
 use Tests\Tempest\Integration\FrameworkIntegrationTestCase;
@@ -22,8 +24,23 @@ final class DeferredTasksTest extends FrameworkIntegrationTestCase
             ->get(uri(DeferController::class))
             ->assertOk();
 
-        $this->container->get(FinishDeferredTasks::class)();
+        $this->container->invoke(FinishDeferredTasks::class);
 
         $this->assertTrue(DeferController::$executed);
+    }
+
+    public function test_deferred_tasks_are_executed_with_container_parameters(): void
+    {
+        $executed = false;
+
+        defer(function (Container $container) use (&$executed): void {
+            $container->invoke(function () use (&$executed): void {
+                $executed = true;
+            });
+        });
+
+        $this->container->invoke(FinishDeferredTasks::class);
+
+        $this->assertTrue($executed);
     }
 }
