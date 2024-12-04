3e445f58a4f4bdab2b3365cb2fcba5ac5beefc79
Guillaume Turpin <guillaume.turpin45@gmail.com>
2024-12-04 15:13:56 +0100

    :sparkles: Make the command

---
+++ src/Tempest/Console/src/Enums/MiddlewareType.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tempest\Console\Enums;
+
+use Tempest\Http\HttpMiddleware;
+use Tempest\EventBus\EventBusMiddleware;
+use Tempest\Console\ConsoleMiddleware;
+use Tempest\CommandBus\CommandBusMiddleware;
+
+/**
+ * Represents available middleware types in Tempest.
+ */
+enum MiddlewareType: string {
+    case CONSOLE = 'console';
+    case HTTP = 'http';
+    case EVENT_BUS = 'event-bus';
+    case COMMAND_BUS = 'command-bus';
+
+    /**
+     * Get the related interface for the middleware type.
+     *
+     * @return class-string
+     */
+    public function relatedInterface(): string {
+        return match ($this) {
+            self::CONSOLE => ConsoleMiddleware::class,
+            self::HTTP => HttpMiddleware::class,
+            self::EVENT_BUS => EventBusMiddleware::class,
+            self::COMMAND_BUS => CommandBusMiddleware::class,
+        };
+    }
+}
---
+++ src/Tempest/Console/src/Stubs/HttpMiddlewareStub.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tempest\Console\Stubs;
+
+use Tempest\Http\HttpMiddleware;
+use Tempest\Http\Request;
+use Tempest\Http\HttpMiddlewareCallable;
+use Tempest\Http\Response;
+
+final class HttpMiddlewareStub implements HttpMiddleware {
+    public function __invoke(Request $request, HttpMiddlewareCallable $next): Response {
+        $response = $next($request);
+        
+        return $response;
+    }
+}
