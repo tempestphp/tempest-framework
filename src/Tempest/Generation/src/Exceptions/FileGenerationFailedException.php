be20015a3b2c419a1f1b2227864363d4c4aebf41
Vincent Hagen <blackshadev@users.noreply.github.com>
2024-12-07 19:49:25 +0100

    chore: upgrade phpstan to v2 (#821)

--- src/Tempest/Generation/src/Exceptions/FileGenerationFailedException.php
+++ src/Tempest/Generation/src/Exceptions/FileGenerationFailedException.php
@@ -6,7 +6,6 @@ namespace Tempest\Generation\Exceptions;
 
 use Exception;
 
-/** @phpstan-ignore-next-line because this exception is extended */
 class FileGenerationFailedException extends Exception
 {
 }
