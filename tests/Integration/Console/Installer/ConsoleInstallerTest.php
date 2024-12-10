2b17d79bd549177aa9f5167be0812265f2fa647b
Brent Roose <brent.roose@gmail.com>
2024-12-10 12:46:16 +0100

    wip

---
+++ tests/Integration/Console/Installer/ConsoleInstallerTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Integration\Console\Installer;
+
+use Tempest\Core\ComposerNamespace;
+use Tests\Tempest\Integration\FrameworkIntegrationTestCase;
+
+final class ConsoleInstallerTest extends FrameworkIntegrationTestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->installer
+            ->configure(
+                __DIR__ . '/install',
+                new ComposerNamespace('App\\', __DIR__ . '/install/App'),
+            )
+            ->setRoot(__DIR__ . '/install');
+    }
+
+    protected function tearDown(): void
+    {
+        $this->installer->clean();
+
+        parent::tearDown();
+    }
+
+    public function test_console_installer(): void
+    {
+        $this->console
+            ->call('install console -f');
+
+        $this->installer
+            ->assertFileExists('tempest')
+            ->assertFileExists('.env.example')
+            ->assertFileExists('.env');
+    }
+}
