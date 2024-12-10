90b632109ad4a43e3b93dc84d6327dc58ca4a2c4
Brent Roose <brent.roose@gmail.com>
2024-12-10 12:55:49 +0100

    feat(console): add installer (#837)

---
+++ tests/Integration/Console/Installer/ConsoleInstallerTest.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Integration\Console\Installer;
+
+use Tempest\Core\ComposerNamespace;
+use Tests\Tempest\Integration\FrameworkIntegrationTestCase;
+
+/**
+ * @internal
+ */
+final class ConsoleInstallerTest extends FrameworkIntegrationTestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->installer
+            ->configure(
+                __DIR__ . '/install',
+                new ComposerNamespace('App\\', __DIR__ . '/install/App'),
+            )
+            ->setRoot(__DIR__ . '/install');
+    }
+
+    protected function tearDown(): void
+    {
+        $this->installer->clean();
+
+        parent::tearDown();
+    }
+
+    public function test_console_installer(): void
+    {
+        $this->console
+            ->call('install console -f');
+
+        $this->installer
+            ->assertFileExists('tempest');
+    }
+}
