d90aa40d966aa2f79e8352a3b2bfdcfc2f48927c
Brent Roose <brent.roose@gmail.com>
2024-12-10 13:43:42 +0100

    wip

--- src/Tempest/Core/src/ShellExecutors/GenericShellExecutor.php
+++ src/Tempest/Core/src/ShellExecutors/GenericShellExecutor.php
@@ -10,7 +10,6 @@ final class GenericShellExecutor implements ShellExecutor
 {
     public function execute(string $command): void
     {
-        /** @phpstan-ignore-next-line */
         shell_exec($command);
     }
 }
