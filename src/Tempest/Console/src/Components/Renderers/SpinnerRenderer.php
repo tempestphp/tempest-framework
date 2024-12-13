5755ab7466c9e8e220fb26c99f6770eebb1c2ed0
Enzo Innocenzi <enzo@innocenzi.dev>
2024-12-13 16:31:53 +0100

    chore: apply fixes from qa

--- src/Tempest/Console/src/Components/Renderers/SpinnerRenderer.php
+++ src/Tempest/Console/src/Components/Renderers/SpinnerRenderer.php
@@ -6,20 +6,14 @@ namespace Tempest\Console\Components\Renderers;
 
 use Tempest\Console\Components\ComponentState;
 use Tempest\Console\Terminal\Terminal;
-use function Tempest\Support\str;
 
 final class SpinnerRenderer
 {
-    private const array FRAMES = [ '⠁', '⠉', '⠙', '⠚', '⠒', '⠂', '⠂', '⠒', '⠲', '⠴', '⠤', '⠄', '⠄', '⠤', '⠴', '⠲', '⠒', '⠂', '⠂', '⠒', '⠚', '⠙', '⠉', '⠁'];
+    private const array FRAMES = ['⠁', '⠁', '⠉', '⠙', '⠚', '⠒', '⠂', '⠂', '⠒', '⠲', '⠴', '⠤', '⠄', '⠄', '⠤', '⠠', '⠠', '⠤', '⠦', '⠖', '⠒', '⠐', '⠐', '⠒', '⠓', '⠋', '⠉', '⠈', '⠈'];
 
     private int $index = 0;
 
     private(set) int $speed = 80_000;
-
-    public function __construct(
-        private readonly string $label,
-    ) {
-    }
 
     public function render(Terminal $terminal, ComponentState $state): string
     {
@@ -33,14 +27,5 @@ final class SpinnerRenderer
         $this->index = ($this->index + 1) % count(self::FRAMES);
 
         return self::FRAMES[$previous];
-
-        return str()
-            ->append("\n")
-            ->append($margin)
-            ->append('<style="fg-cyan">', self::FRAMES[$previous], '</style>')
-            ->append(' ')
-            ->append(str($this->label)->truncate(min(150, $terminal->width - strlen($margin) * 2)))
-            ->append("\n")
-            ->toString();
     }
 }
