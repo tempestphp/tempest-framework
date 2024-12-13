e332bbd23240460ede83ce1ee39c37b825e5b3ee
Brent Roose <brent.roose@gmail.com>
2024-12-13 13:58:23 +0100

    feat(database): chunked results (#855)

--- tests/Integration/Database/Builder/ModelQueryBuilderTest.php
+++ tests/Integration/Database/Builder/ModelQueryBuilderTest.php
@@ -71,6 +71,52 @@ final class ModelQueryBuilderTest extends FrameworkIntegrationTestCase
         $this->assertSame('B', $books[1]->title);
     }
 
+    public function test_offset(): void
+    {
+        $this->migrate(
+            CreateMigrationsTable::class,
+            CreateAuthorTable::class,
+            CreateBookTable::class,
+        );
+
+        (Book::new(title: 'A'))->save();
+        (Book::new(title: 'B'))->save();
+        (Book::new(title: 'C'))->save();
+        (Book::new(title: 'D'))->save();
+
+        $books = Book::query()->limit(2)->offset(2)->all();
+
+        $this->assertCount(2, $books);
+        $this->assertSame('C', $books[0]->title);
+        $this->assertSame('D', $books[1]->title);
+    }
+
+    public function test_chunk(): void
+    {
+        $this->migrate(
+            CreateMigrationsTable::class,
+            CreateAuthorTable::class,
+            CreateBookTable::class,
+        );
+
+        (Book::new(title: 'A'))->save();
+        (Book::new(title: 'B'))->save();
+        (Book::new(title: 'C'))->save();
+        (Book::new(title: 'D'))->save();
+
+        $results = [];
+        Book::query()->chunk(function (array $chunk) use (&$results) {
+            $results = [...$results, ...$chunk];
+        }, 2);
+        $this->assertCount(4, $results);
+
+        $results = [];
+        Book::query()->where('title <> "A"')->chunk(function (array $chunk) use (&$results) {
+            $results = [...$results, ...$chunk];
+        }, 2);
+        $this->assertCount(3, $results);
+    }
+
     public function test_raw(): void
     {
         $this->migrate(
