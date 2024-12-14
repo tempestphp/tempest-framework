49f019c8e742e99212b2d2507206d5339bf480b3
Brent Roose <brent.roose@gmail.com>
2024-12-14 14:24:42 +0100

    feat(database): bindings in query methods (#859)

--- src/Tempest/Database/src/Query.php
+++ src/Tempest/Database/src/Query.php
@@ -14,25 +14,29 @@ final class Query
     ) {
     }
 
-    public function execute(): Id
+    public function execute(mixed ...$bindings): Id
     {
+        $this->bindings = [...$this->bindings, ...$bindings];
+
         $database = $this->getDatabase();
 
-        $database->execute($this);
+        $query = $this->withBindings($bindings);
 
-        return isset($this->bindings['id'])
-            ? new Id($this->bindings['id'])
+        $database->execute($query);
+
+        return isset($query->bindings['id'])
+            ? new Id($query->bindings['id'])
             : $database->getLastInsertId();
     }
 
-    public function fetch(): array
+    public function fetch(mixed ...$bindings): array
     {
-        return $this->getDatabase()->fetch($this);
+        return $this->getDatabase()->fetch($this->withBindings($bindings));
     }
 
-    public function fetchFirst(): ?array
+    public function fetchFirst(mixed ...$bindings): ?array
     {
-        return $this->getDatabase()->fetchFirst($this);
+        return $this->getDatabase()->fetchFirst($this->withBindings($bindings));
     }
 
     public function getSql(): string
@@ -45,6 +49,15 @@ final class Query
         $this->sql .= PHP_EOL . $append;
 
         return $this;
+    }
+
+    public function withBindings(array $bindings): self
+    {
+        $clone = clone $this;
+
+        $clone->bindings = [...$clone->bindings, ...$bindings];
+
+        return $clone;
     }
 
     private function getDatabase(): Database
