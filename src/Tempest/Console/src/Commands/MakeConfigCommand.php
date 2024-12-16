fa27c7ab5d5d49d3b7dcf52dd27433d708421edc
Guillaume Turpin <guillaume.turpin45@gmail.com>
2024-12-16 12:53:20 +0100

    :sparkles: Make the command inlcuding only database config

---
+++ src/Tempest/Console/src/Commands/MakeConfigCommand.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tempest\Console\Commands;
+
+use InvalidArgumentException;
+use Tempest\Console\ConsoleArgument;
+use Tempest\Console\ConsoleCommand;
+use Tempest\Console\Enums\ConfigType;
+use Tempest\Console\Enums\MiddlewareType;
+use Tempest\Console\Stubs\CommandBusMiddlewareStub;
+use Tempest\Console\Stubs\ConsoleMiddlewareStub;
+use Tempest\Console\Stubs\EventBusMiddlewareStub;
+use Tempest\Console\Stubs\HttpMiddlewareStub;
+use Tempest\Core\PublishesFiles;
+use Tempest\Generation\DataObjects\StubFile;
+use Tempest\Generation\Exceptions\FileGenerationAbortedException;
+use Tempest\Generation\Exceptions\FileGenerationFailedException;
+
+use function Tempest\root_path;
+use function Tempest\src_path;
+use function Tempest\Support\str;
+
+final class MakeConfigCommand
+{
+    use PublishesFiles;
+
+    #[ConsoleCommand(
+        name: 'make:config',
+        description: 'Creates a new config class',
+        aliases: ['config:make', 'config:create', 'create:config'],
+    )]
+    public function __invoke(
+        #[ConsoleArgument(
+            name: 'type',
+            help: 'The type of the config to create',
+        )]
+        ConfigType $configType,
+    ): void {
+        try {
+            $stubFile = $this->getStubFileFromConfigType($configType);
+            $suggestedPath = str( $this->getSuggestedPath( 'Dummy' ) )
+                ->replace( 'Dummy', $configType->value . '.config' )
+                ->toString();
+            $targetPath = $this->promptTargetPath($suggestedPath);
+            $shouldOverride = $this->askForOverride($targetPath);
+
+            $this->stubFileGenerator->generateRawFile(
+                stubFile: $stubFile,
+                targetPath: $targetPath,
+                shouldOverride: $shouldOverride,
+            );
+
+            $this->success(sprintf('Middleware successfully created at "%s".', $targetPath));
+        } catch (FileGenerationAbortedException|FileGenerationFailedException|InvalidArgumentException $e) {
+            $this->error($e->getMessage());
+        }
+    }
+
+    private function getStubFileFromConfigType(ConfigType $configType): StubFile
+    {
+        $stubPath = dirname( __DIR__ ) . '/Stubs';
+        
+        return match ($configType) {
+            ConfigType::DATABASE => StubFile::from( $stubPath . '/database.config.stub.php'), // @phpstan-ignore match.alwaysTrue (Because this is a guardrail for the future implementations)
+            default => throw new InvalidArgumentException(sprintf('The "%s" config type has no supported stub file.', $configType->value)),
+        };
+    }
+}
---
+++ src/Tempest/Console/src/Enums/ConfigType.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tempest\Console\Enums;
+
+/**
+ * Represents available config types in Tempest.
+ */
+enum ConfigType: string
+{
+    case DATABASE = 'database';
+}
