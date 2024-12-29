bae7a1b7db37e982b0dcf2b859ed83fe44fb5b10
Vincent Hagen <vinnie@script4web.nl>
2024-12-29 14:43:43 +0100

    feat(database): Add explicit relation attributes

--- src/Tempest/Database/src/Builder/Relations/HasManyRelation.php
+++ src/Tempest/Database/src/Builder/Relations/HasManyRelation.php
@@ -6,24 +6,27 @@ namespace Tempest\Database\Builder\Relations;
 
 use Tempest\Database\Builder\FieldName;
 use Tempest\Database\Builder\TableName;
+use Tempest\Database\Exceptions\InvalidRelation;
+use Tempest\Database\HasMany;
 use Tempest\Reflection\ClassReflector;
 use Tempest\Reflection\PropertyReflector;
 
 final readonly class HasManyRelation implements Relation
 {
-    private ClassReflector $relationModelClass;
+    private function __construct(
+        private ClassReflector $relationModelClass,
+        private FieldName $localField,
+        private FieldName $joinField,
+    ) {
+    }
 
-    private FieldName $localField;
-
-    private FieldName $joinField;
-
-    public function __construct(PropertyReflector $property, string $alias)
+    public static function fromInference(PropertyReflector $property, string $alias): self
     {
-        $this->relationModelClass = $property->getIterableType()->asClass();
+        $relationModelClass = self::getRelationModelClass($property);
 
         $inverseProperty = null;
 
-        foreach ($this->relationModelClass->getPublicProperties() as $potentialInverseProperty) {
+        foreach ($relationModelClass->getPublicProperties() as $potentialInverseProperty) {
             if ($potentialInverseProperty->getType()->equals($property->getClass()->getType())) {
                 $inverseProperty = $potentialInverseProperty;
 
@@ -31,11 +34,45 @@ final readonly class HasManyRelation implements Relation
             }
         }
 
-        $localTable = TableName::for($property->getClass(), $alias);
-        $this->localField = new FieldName($localTable, 'id');
+        if ($inverseProperty === null) {
+            throw InvalidRelation::inversePropertyNotFound(
+                $property->getClass()->getName(),
+                $property->getName(),
+                $relationModelClass->getName(),
+            );
+        }
 
-        $joinTable = TableName::for($this->relationModelClass, "{$alias}.{$property->getName()}[]");
-        $this->joinField = new FieldName($joinTable, $inverseProperty->getName() . '_id');
+        $localTable = TableName::for($property->getClass(), $alias);
+        $localField = new FieldName($localTable, 'id');
+
+        $joinTable = TableName::for($relationModelClass, "{$alias}.{$property->getName()}[]");
+        $joinField = new FieldName($joinTable, $inverseProperty->getName() . '_id');
+
+        return new self($relationModelClass, $localField, $joinField);
+    }
+
+    public static function getRelationModelClass(
+        PropertyReflector $property,
+        HasMany|null $relation = null,
+    ): ClassReflector {
+        if ($relation !== null && $relation->inverseClassName !== null) {
+            return new ClassReflector($relation->inverseClassName);
+        }
+
+        return $property->getIterableType()->asClass();
+    }
+
+    public static function fromAttribute(HasMany $relation, PropertyReflector $property, string $alias): self
+    {
+        $relationModelClass = self::getRelationModelClass($property, $relation);
+
+        $localTable = TableName::for($property->getClass(), $alias);
+        $localField = new FieldName($localTable, $relation->localPropertyName);
+
+        $joinTable = TableName::for($relationModelClass, "{$alias}.{$property->getName()}[]");
+        $joinField = new FieldName($joinTable, $relation->inversePropertyName);
+
+        return new self($relationModelClass, $localField, $joinField);
     }
 
     public function getStatement(): string
