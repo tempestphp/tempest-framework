715bcb9a28c19b8e2a1aea6e7a12c143c82980c8
Guillaume Turpin <guillaume.turpin45@gmail.com>
2025-01-02 14:10:41 +0100

    :white_check_mark: Add tests

---
+++ src/Tempest/Generation/tests/Fixtures/ClassWithDummyStringToBeReplacedByFqcn.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tempest\Generation\Tests\Fixtures;
+
+final class ClassWithDummyStringToBeReplacedByFqcn
+{
+    public function dummy(): string {
+        return 'fqcn-to-be-replaced';
+    }
+}
