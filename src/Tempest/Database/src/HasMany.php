5e4df2421fdd165d9967e7793d9962735006e86f
Vincent Hagen <blackshadev@users.noreply.github.com>
2025-01-09 11:01:28 +0100

    feat(database): add explicit relation attributes (#874)

---
+++ src/Tempest/Database/src/HasMany.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tempest\Database;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_PROPERTY)]
+final readonly class HasMany
+{
+    /** @param null|class-string $inverseClassName */
+    public function __construct(
+        public string $inversePropertyName,
+        public ?string $inverseClassName = null,
+        public string $localPropertyName = 'id',
+    ) {
+    }
+}
