7f1ba3984aa1aaa20088a78db5245babd48a018c
Vincent Hagen <vinnie@script4web.nl>
2025-01-09 19:47:47 +0100

    test(database): add seperate initializer for FrameworkIntegrationTestCase

--- src/Tempest/Database/src/ConnectionInitializer.php
+++ src/Tempest/Database/src/ConnectionInitializer.php
@@ -7,26 +7,16 @@ namespace Tempest\Database;
 use Tempest\Container\Container;
 use Tempest\Container\Initializer;
 use Tempest\Container\Singleton;
-use Tempest\Core\AppConfig;
 
 final class ConnectionInitializer implements Initializer
 {
-    private static Connection|null $instance = null;
-
     #[Singleton]
     public function initialize(Container $container): Connection
     {
-        // Reuse same connection instance in unit tests
-        if (self::$instance !== null && $container->get(AppConfig::class)->environment->isTesting()) {
-            return self::$instance;
-        }
-
         $databaseConfig = $container->get(DatabaseConfig::class);
 
         $connection = new PDOConnection($databaseConfig->connection());
         $connection->connect();
-
-        self::$instance = $connection;
 
         return $connection;
     }
