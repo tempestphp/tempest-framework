091ee5c12e730659793e26429e2d7e76360de570
Enzo Innocenzi <enzo@innocenzi.dev>
2025-01-10 12:38:54 +0100

    refactor(terminal): use property hook to support disabling tty

---
+++ tests/Integration/Console/TerminalTest.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Tempest\Integration\Console;
+
+use Tempest\Console\Console;
+use Tempest\Console\Terminal\Terminal;
+use Tests\Tempest\Integration\FrameworkIntegrationTestCase;
+
+/**
+ * @internal
+ */
+final class TerminalTest extends FrameworkIntegrationTestCase
+{
+    public function test_supports_tty(): void
+    {
+        $this->console->withoutPrompting()->call(function (Console $console) {
+            $terminal = new Terminal($console);
+
+            $this->assertTrue($terminal->supportsTty);
+
+            $terminal->disableTty();
+
+            $this->assertFalse($terminal->supportsTty);
+        });
+    }
+}
