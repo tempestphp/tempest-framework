16d3729daa7994363c24791a91eaf0669bd51d8c
Mark <14284867+xHeaven@users.noreply.github.com>
2025-01-11 12:17:53 +0100

    refactor: narrow down return types

--- src/Tempest/Database/src/QueryStatements/CanExecuteStatement.php
+++ src/Tempest/Database/src/QueryStatements/CanExecuteStatement.php
@@ -5,11 +5,12 @@ declare(strict_types=1);
 namespace Tempest\Database\QueryStatements;
 
 use Tempest\Database\DatabaseDialect;
+use Tempest\Database\Id;
 use Tempest\Database\Query;
 
 trait CanExecuteStatement
 {
-    public function execute(DatabaseDialect $dialect): mixed
+    public function execute(DatabaseDialect $dialect): Id
     {
         return (new Query($this->compile($dialect)))->execute();
     }
