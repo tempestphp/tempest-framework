363ec0c6abdd0457d8678efc1d6fe1f84333c851
Guillaume Turpin <guillaume.turpin45@gmail.com>
2025-02-26 15:57:42 +0100

    :bug: Fix tests fixtures imports

--- src/Tempest/Support/tests/Fixtures/Enums/SampleIntegerBackedEnum.php
+++ src/Tempest/Support/tests/Fixtures/Enums/SampleIntegerBackedEnum.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Tempest\Support\Tests\Fixtures\Enums;
 
-use Tempest\Support\Enums\IsEnumHelper;
+use Tempest\Support\IsEnumHelper;
 
 enum SampleIntegerBackedEnum: int
 {
