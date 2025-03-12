c2547b2cd905058940aa86d5452c3e8d00bfca26
Enzo Innocenzi <enzo@innocenzi.dev>
2025-03-12 19:28:16 +0100

    chore(vite): show proper run command depending on package manager

--- src/Tempest/Support/src/JavaScript/PackageManager.php
+++ src/Tempest/Support/src/JavaScript/PackageManager.php
@@ -35,6 +35,20 @@ enum PackageManager: string
         };
     }
 
+    public function getRunCommand(string $script): string
+    {
+        return (
+            $this->getBinaryName() .
+            ' ' .
+            match ($this) {
+                self::BUN => $script,
+                self::NPM => "run {$script}",
+                self::YARN => $script,
+                self::PNPM => $script,
+            }
+        );
+    }
+
     public function getInstallCommand(): string
     {
         return match ($this) {
