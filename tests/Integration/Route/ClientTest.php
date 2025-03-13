4845ce82a78226370c1bbeee19dce126bf38f5db
brendt <brent.roose@gmail.com>
2025-03-13 12:54:11 +0100

    wip

--- tests/Integration/Route/ClientTest.php
+++ tests/Integration/Route/ClientTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\Tempest\Integration\Route;
 
+use GuzzleHttp\Exception\ConnectException;
 use Laminas\Diactoros\RequestFactory;
 use Laminas\Diactoros\StreamFactory;
 use Laminas\Diactoros\Uri;
@@ -27,9 +28,7 @@ final class ClientTest extends FrameworkIntegrationTestCase
         parent::setUp();
 
         $this->server = new Process([root_path('tempest'), 'serve', 'localhost', '8088']);
-        $this->server->start(function (...$args) {
-            lw(...$args);
-        });
+        $this->server->start();
 
         // Server needs to start
         usleep(100000);
@@ -58,14 +57,17 @@ final class ClientTest extends FrameworkIntegrationTestCase
             ->withHeader('Referer', 'http://localhost:8088/request-test/form')
             ->withHeader('Accept', 'application/json')
             ->withHeader('Content-Type', 'application/x-www-form-urlencoded')
-            ->withBody(new StreamFactory()->createStream('name=a a&b.name=b'))
-        ;
+            ->withBody(new StreamFactory()->createStream('name=a a&b.name=b'));
 
-        $response = $this->driver->sendRequest($request);
+        try {
+            $response = $this->driver->sendRequest($request);
 
-        $this->assertSame(200, $response->getStatusCode());
-        $this->assertSame('a a', $response->getHeader('name')[0]);
-        $this->assertSame('b', $response->getHeader('b.name')[0]);
+            $this->assertSame(200, $response->getStatusCode());
+            $this->assertSame('a a', $response->getHeader('name')[0]);
+            $this->assertSame('b', $response->getHeader('b.name')[0]);
+        } catch (ConnectException) {
+            $this->markTestSkipped('Could not connect to server.');
+        }
     }
 
     public function test_json_post_request(): void
@@ -74,13 +76,16 @@ final class ClientTest extends FrameworkIntegrationTestCase
             ->createRequest('POST', new Uri('http://localhost:8088/request-test/form'))
             ->withHeader('Accept', 'application/json')
             ->withHeader('Content-Type', 'application/json')
-            ->withBody(new StreamFactory()->createStream('{"name": "a a", "b": {"name": "b"}}'))
-        ;
+            ->withBody(new StreamFactory()->createStream('{"name": "a a", "b": {"name": "b"}}'));
 
-        $response = $this->driver->sendRequest($request);
+        try {
+            $response = $this->driver->sendRequest($request);
 
-        $this->assertSame(200, $response->getStatusCode());
-        $this->assertSame('a a', $response->getHeader('name')[0]);
-        $this->assertSame('b', $response->getHeader('b.name')[0]);
+            $this->assertSame(200, $response->getStatusCode());
+            $this->assertSame('a a', $response->getHeader('name')[0]);
+            $this->assertSame('b', $response->getHeader('b.name')[0]);
+        } catch (ConnectException) {
+            $this->markTestSkipped('Could not connect to server.');
+        }
     }
 }
