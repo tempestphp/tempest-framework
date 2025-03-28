3a4a56cbc4575a9c3775779c328d120e9384e0d0
Enzo Innocenzi <enzo@innocenzi.dev>
2025-03-28 14:24:49 +0100

    refactor(core): remove unused error handling code and improve error registration

--- src/Tempest/Router/src/ErrorResponses/error.view.php
+++ src/Tempest/Router/src/ErrorResponses/error.view.php
@@ -7,7 +7,7 @@
   <div class="flex flex-col justify-center items-center bg-[#061324] min-w-screen min-h-screen text-[#a8caf7] antialiased">
     <div class="px-8 container">
       <h1 class="font-thin text-8xl">{{ $status }}</h1>
-      <p class="text-xl uppercase">{{ \Tempest\Support\Str\ensure_ends_with($message, '.') }}</p>
+      <p :if="$message" class="text-xl uppercase">{{ \Tempest\Support\Str\ensure_ends_with($message, '.') }}</p>
     </div>
   </div>
 </body>
