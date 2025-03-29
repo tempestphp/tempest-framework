04e41822bf12fd4e6337f5a0e2fe1377a2fba892
Enzo Innocenzi <enzo@innocenzi.dev>
2025-03-29 23:35:23 +0100

    refactor(console): improve migration commands console output

--- src/Tempest/Framework/Commands/MigrateUpCommand.php
+++ src/Tempest/Framework/Commands/MigrateUpCommand.php
@@ -34,6 +34,8 @@ final class MigrateUpCommand
     public function __invoke(
         #[ConsoleArgument(description: 'Validates the integrity of existing migration files by checking if they have been tampered with.')]
         bool $validate = true,
+        #[ConsoleArgument(description: 'Drops all tables and rerun migrations from scratch.')]
+        bool $fresh = false,
     ): ExitCode {
         if ($validate) {
             $validationSuccess = $this->console->call(MigrateValidateCommand::class);
@@ -43,10 +45,16 @@ final class MigrateUpCommand
             }
         }
 
+        if ($fresh) {
+            return $this->console->call(MigrateFreshCommand::class, ['validate' => false]);
+        }
+
+        $this->console->header('Migrating');
         $this->migrationManager->up();
 
-        $this->console
-            ->success(sprintf('Migrated %s migrations', $this->count));
+        if ($this->count === 0) {
+            $this->console->info('There is no new migration to run.');
+        }
 
         return ExitCode::SUCCESS;
     }
@@ -54,7 +62,10 @@ final class MigrateUpCommand
     #[EventHandler]
     public function onMigrationMigrated(MigrationMigrated $event): void
     {
-        $this->console->writeln("- {$event->name}");
         $this->count += 1;
+        $this->console->keyValue(
+            key: "<style='fg-gray'>{$event->name}</style>",
+            value: "<style='fg-green'>MIGRATED</style>",
+        );
     }
 }
