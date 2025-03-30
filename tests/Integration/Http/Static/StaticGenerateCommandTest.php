f63b8d290257ed403587d6b06e9c008392fd6bbc
Enzo Innocenzi <enzo@innocenzi.dev>
2025-03-30 14:12:11 +0200

    refactor(router): improve static page generation console command output (#1100)

--- tests/Integration/Http/Static/StaticGenerateCommandTest.php
+++ tests/Integration/Http/Static/StaticGenerateCommandTest.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace Tests\Tempest\Integration\Http\Static;
 
+use Tempest\Console\ExitCode;
 use Tempest\Core\AppConfig;
 use Tempest\Router\Static\StaticGenerateCommand;
 use Tests\Tempest\Integration\FrameworkIntegrationTestCase;
+use Tests\Tempest\Integration\Http\Static\Fixtures\StaticPageController;
 
 use function Tempest\root_path;
 
@@ -15,6 +17,14 @@ use function Tempest\root_path;
  */
 final class StaticGenerateCommandTest extends FrameworkIntegrationTestCase
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->registerRoute(StaticPageController::class);
+        $this->registerStaticPage(StaticPageController::class);
+    }
+
     public function test_static_site_generate_command(): void
     {
         $appConfig = new AppConfig(baseUri: 'https://test.com');
@@ -39,5 +49,47 @@ final class StaticGenerateCommandTest extends FrameworkIntegrationTestCase
 
         $this->assertStringContainsString('c', $d);
         $this->assertStringContainsString('d', $d);
+    }
+
+    public function test_failure_status_code(): void
+    {
+        $this->registerRoute([StaticPageController::class, 'http500']);
+        $this->registerStaticPage([StaticPageController::class, 'http500']);
+
+        $appConfig = new AppConfig(baseUri: 'https://test.com');
+        $this->container->config($appConfig);
+
+        $this->console
+            ->call(StaticGenerateCommand::class)
+            ->assertSee('HTTP 500')
+            ->assertExitCode(ExitCode::ERROR);
+    }
+
+    public function test_failure_no_textual_content(): void
+    {
+        $this->registerRoute([StaticPageController::class, 'noTextualContent']);
+        $this->registerStaticPage([StaticPageController::class, 'noTextualContent']);
+
+        $appConfig = new AppConfig(baseUri: 'https://test.com');
+        $this->container->config($appConfig);
+
+        $this->console
+            ->call(StaticGenerateCommand::class)
+            ->assertSee('NO CONTENT')
+            ->assertExitCode(ExitCode::ERROR);
+    }
+
+    public function test_failure_no_build(): void
+    {
+        $this->registerRoute([StaticPageController::class, 'vite']);
+        $this->registerStaticPage([StaticPageController::class, 'vite']);
+
+        $appConfig = new AppConfig(baseUri: 'https://test.com');
+        $this->container->config($appConfig);
+
+        $this->console
+            ->call(StaticGenerateCommand::class)
+            ->assertSee('A Vite build is needed for [/static/vite/a/b]')
+            ->assertExitCode(ExitCode::ERROR);
     }
 }
