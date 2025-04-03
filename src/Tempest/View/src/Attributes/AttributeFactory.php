35f85e9495139cdf7bb8b533e7a1c5de41d796dd
Brent Roose <brent.roose@gmail.com>
2025-04-03 10:28:53 +0200

    feat(view): improve boolean attributes (#1111)

--- src/Tempest/View/src/Attributes/AttributeFactory.php
+++ src/Tempest/View/src/Attributes/AttributeFactory.php
@@ -5,11 +5,10 @@ declare(strict_types=1);
 namespace Tempest\View\Attributes;
 
 use Tempest\View\Attribute;
-use Tempest\View\Element;
 
 final readonly class AttributeFactory
 {
-    public function make(Element $element, string $attributeName): Attribute
+    public function make(string $attributeName): Attribute
     {
         return match (true) {
             $attributeName === ':if' => new IfAttribute(),
@@ -17,7 +16,6 @@ final readonly class AttributeFactory
             $attributeName === ':else' => new ElseAttribute(),
             $attributeName === ':foreach' => new ForeachAttribute(),
             $attributeName === ':forelse' => new ForelseAttribute(),
-            BooleanAttribute::matches($element, $attributeName) => new BooleanAttribute($attributeName),
             str_starts_with($attributeName, ':') => new ExpressionAttribute($attributeName),
             default => new DataAttribute($attributeName),
         };
