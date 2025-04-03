25f982270f7c80f7d45ec5dd61f925e12b6882ae
Enzo Innocenzi <enzo@innocenzi.dev>
2025-04-03 19:53:27 +0200

    chore: fix typos

--- src/Tempest/Container/src/CurrentTag.php
+++ src/Tempest/Container/src/CurrentTag.php
@@ -5,7 +5,7 @@ namespace Tempest\Container;
 use Attribute;
 
 /**
- * Injects the current tag to the target property.
+ * Injects to the property the tag with which the current class has been resolved.
  */
 #[Attribute(Attribute::TARGET_PROPERTY)]
 final class CurrentTag
--- src/Tempest/Container/src/ForwardTag.php
+++ src/Tempest/Container/src/ForwardTag.php
@@ -5,7 +5,7 @@ namespace Tempest\Container;
 use Attribute;
 
 /**
- * Resolves this dependency using the tag with which the curreent class has been resolved.
+ * Resolves this dependency using the tag with which the current class has been resolved.
  */
 #[Attribute(Attribute::TARGET_PARAMETER)]
 final class ForwardTag
