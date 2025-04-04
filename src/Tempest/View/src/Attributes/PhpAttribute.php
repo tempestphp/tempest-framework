d86d32742597352897b1e6fdcaf94e2568c40efc
brendt <brent.roose@gmail.com>
2025-04-04 20:33:56 +0200

    wip

--- src/Tempest/View/src/Attributes/PhpAttribute.php
+++ src/Tempest/View/src/Attributes/PhpAttribute.php
@@ -9,7 +9,6 @@ use Tempest\View\Element;
  * This class is used whenever PHP code occurs within element tags, it will make sure this code is left untouched
  * <div <?= 'hi' ?> class="foo">
  * The most common use case for this happening is when conditional attributes are rendered within a view component
- * (which is recompiled, I should look into whether that's actually necessary)
  */
 final readonly class PhpAttribute implements Attribute
 {
