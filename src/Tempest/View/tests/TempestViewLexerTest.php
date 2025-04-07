33085b04b09bcaaf7758c7859ccc89233b96b806
Brent Roose <brent.roose@gmail.com>
2025-04-07 10:11:50 +0200

    fix(view): lexing multiline attributes in windows (#1121)

--- src/Tempest/View/tests/TempestViewLexerTest.php
+++ src/Tempest/View/tests/TempestViewLexerTest.php
@@ -92,12 +92,6 @@ final class TempestViewLexerTest extends TestCase
 
     public function test_multiline_attributes(): void
     {
-        if (PHP_OS_FAMILY === 'Windows') {
-            $this->markTestSkipped('This test should be debugged on Windows.');
-            /* @phpstan-ignore-next-line */
-            return;
-        }
-
         $html = <<<'HTML'
         <div
             class="abc"
