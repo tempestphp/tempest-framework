6ecc9ceeb65bba1ad33bbe1c5c99f902d7e91b91
brendt <brent.roose@gmail.com>
2025-04-28 13:03:53 +0200

    wip

--- src/Tempest/Container/tests/Fixtures/ClassWithLazySlowPropertyDependency.php
+++ src/Tempest/Container/tests/Fixtures/ClassWithLazySlowPropertyDependency.php
@@ -3,13 +3,13 @@ declare(strict_types=1);
 
 namespace Tempest\Container\Tests\Fixtures;
 
+use Tempest\Container\Proxy;
 use Tempest\Container\Inject;
-use Tempest\Container\Lazy;
 
 final class ClassWithLazySlowPropertyDependency
 {
     #[Inject]
-    #[Lazy]
+    #[Proxy]
     private(set) SlowDependency $dependency;
 
     public function __construct() {}
