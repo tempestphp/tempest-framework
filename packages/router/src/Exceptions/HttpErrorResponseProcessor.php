2260441244fb5ae8e8c66d7a402f902b3eaad5ed
Enzo Innocenzi <enzo@innocenzi.dev>
2025-05-05 19:17:32 +0200

    refactor: don't throw `HttpException` outside of production

--- packages/router/src/Exceptions/HttpErrorResponseProcessor.php
+++ packages/router/src/Exceptions/HttpErrorResponseProcessor.php
@@ -2,14 +2,27 @@
 
 namespace Tempest\Router\Exceptions;
 
+use Tempest\Core\AppConfig;
 use Tempest\Http\HttpException;
 use Tempest\Http\Response;
 use Tempest\Router\ResponseProcessor;
 
-final class HttpErrorResponseProcessor implements ResponseProcessor
+final readonly class HttpErrorResponseProcessor implements ResponseProcessor
 {
+    public function __construct(
+        private AppConfig $appConfig,
+    ) {}
+
     public function process(Response $response): Response
     {
+        // Throwing an HttpException during tests would make testing more
+        // complex, and is not strictly needed. During development, we
+        // don't need exceptions either, since the exception handler
+        // is different. For this reason, we skip processing here.
+        if (! $this->appConfig->environment->isProduction()) {
+            return $response;
+        }
+
         // Don't handle responses that already have a body. This is to avoid
         // interferring with error responses voluntarily thrown in userland.
         if ($response->body) {
