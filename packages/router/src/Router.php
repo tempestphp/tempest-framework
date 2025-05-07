909350158b7244afb3540492e5da73ec9f29a122
wryk <wryk@wryk.moe>
2025-05-07 17:08:12 +0200

    add `$action` docblock

--- packages/router/src/Router.php
+++ packages/router/src/Router.php
@@ -14,11 +14,25 @@ interface Router
 
     /**
      * Creates a valid URI to the given `$action`.
+     *
+     * `$action` is one of :
+     * - Controller FQCN and its method as a tuple
+     * - Invokable controller FQCN
+     * - URI string starting with `/`
+     *
+     * @param array{class-string, string}|class-string|string $action
      */
     public function toUri(array|string $action, ...$params): string;
 
     /**
      * Checks if the URI to the given `$action` would match the current route.
+     *
+     * `$action` is one of :
+     * - Controller FQCN and its method as a tuple
+     * - Invokable controller FQCN
+     * - URI string starting with `/`
+     *
+     * @param array{class-string, string}|class-string|string $action
      */
     public function isCurrentUri(array|string $action, ...$params): bool;
 }
