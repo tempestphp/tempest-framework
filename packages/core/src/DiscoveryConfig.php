7563faf1b59679756d984d4344dde6e07df062d4
brendt <brent.roose@gmail.com>
2025-05-09 11:18:39 +0200

    wip

--- packages/core/src/DiscoveryConfig.php
+++ packages/core/src/DiscoveryConfig.php
@@ -8,9 +8,7 @@ final class DiscoveryConfig
 
     public function shouldSkip(string $input): bool
     {
-        return $this->skipDiscovery[$input]
-            ?? $this->skipDiscovery[str_replace(['\\', '/'], DIRECTORY_SEPARATOR, $input)]
-            ?? false;
+        return $this->skipDiscovery[$input] ?? $this->skipDiscovery[str_replace(['\\', '/'], DIRECTORY_SEPARATOR, $input)] ?? false;
     }
 
     public function skipClasses(string ...$classNames): self
