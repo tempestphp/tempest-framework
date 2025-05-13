763936b4ba653a51800b8341c328a36bb13005b0
brendt <brent.roose@gmail.com>
2025-05-13 09:17:16 +0200

    wip

--- packages/core/src/DiscoveryConfig.php
+++ packages/core/src/DiscoveryConfig.php
@@ -8,6 +8,7 @@ final class DiscoveryConfig
 
     public function shouldSkip(string $input): bool
     {
+        // TODO: This should be optimized
         return $this->skipDiscovery[$input]
             ?? $this->skipDiscovery[str_replace(['\\', '/'], DIRECTORY_SEPARATOR, $input)]
             ?? $this->skipDiscovery[realpath($input)]
