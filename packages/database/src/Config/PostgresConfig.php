485d473220114b2d769d5cea7e6eed8159434403
brendt <brent.roose@gmail.com>
2025-05-21 09:59:06 +0200

    wip

--- packages/database/src/Config/PostgresConfig.php
+++ packages/database/src/Config/PostgresConfig.php
@@ -12,14 +12,7 @@ use UnitEnum;
 final class PostgresConfig implements DatabaseConfig
 {
     public string $dsn {
-        get => sprintf(
-            'pgsql:host=%s;port=%s;dbname=%s;user=%s;password=%s',
-            $this->host,
-            $this->port,
-            $this->database,
-            $this->username,
-            $this->password,
-        );
+        get => $this->buildDsn();
     }
 
     public DatabaseDialect $dialect {
@@ -34,10 +27,28 @@ final class PostgresConfig implements DatabaseConfig
         #[SensitiveParameter]
         public string $username = 'postgres',
         #[SensitiveParameter]
-        public string $password = '',
+        public ?string $password = null,
         #[SensitiveParameter]
         public string $database = 'app',
         public NamingStrategy $namingStrategy = new PluralizedSnakeCaseStrategy(),
         public null|string|UnitEnum $tag = null,
     ) {}
+
+    private function buildDsn(): string
+    {
+        $dsn = sprintf(
+            'pgsql:host=%s;port=%s;dbname=%s;user=%s',
+            $this->host,
+            $this->port,
+            $this->database,
+            $this->username,
+            $this->password,
+        );
+
+        if ($this->password !== null) {
+            $dsn .= ';password=%s' . $this->password;
+        }
+
+        return $dsn;
+    }
 }
