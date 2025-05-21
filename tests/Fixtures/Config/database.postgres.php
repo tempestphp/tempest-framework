36b2dcca58ddb92cadebbfa536ff15af5722a658
brendt <brent.roose@gmail.com>
2025-05-21 13:12:07 +0200

    wip

--- tests/Fixtures/Config/database.postgres.php
+++ tests/Fixtures/Config/database.postgres.php
@@ -3,5 +3,10 @@
 declare(strict_types=1);
 
 use Tempest\Database\Config\PostgresConfig;
+use function Tempest\env;
 
-return new PostgresConfig(password: 'password');
+
+return new PostgresConfig(
+    username: env('POSTGRES_USER', 'postgres'),
+    password: env('POSTGRES_PASSWORD', ''),
+);
