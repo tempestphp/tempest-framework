dba57d8e5df1bfb66c3c4d8a8138cb2dd0508832
Enzo Innocenzi <enzo@innocenzi.dev>
2025-06-27 14:12:03 +0200

    feat(router): allow specifying port using `host:port` syntax (#1321)

--- packages/router/src/Commands/ServeCommand.php
+++ packages/router/src/Commands/ServeCommand.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Tempest\Router\Commands;
 
 use Tempest\Console\ConsoleCommand;
+use Tempest\Intl\Number;
+use Tempest\Support\Str;
 
 final readonly class ServeCommand
 {
@@ -15,6 +17,12 @@ final readonly class ServeCommand
     public function __invoke(string $host = 'localhost', int $port = 8000, string $publicDir = 'public/'): void
     {
         $routerFile = __DIR__ . '/router.php';
+
+        if (Str\contains($host, ':')) {
+            [$host, $overridenPort] = explode(':', $host, limit: 2);
+            $port = Number\parse($overridenPort, default: $port);
+        }
+
         passthru("php -S {$host}:{$port} -t {$publicDir} {$routerFile}");
     }
 }
