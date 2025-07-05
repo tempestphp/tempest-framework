3a5a751911330aae1c610aa5396ca8616d19ee0f
brendt <brent.roose@gmail.com>
2025-07-05 08:07:46 +0200

    chore: release v1.1.0

--- packages/core/src/Kernel.php
+++ packages/core/src/Kernel.php
@@ -8,7 +8,7 @@ use Tempest\Container\Container;
 
 interface Kernel
 {
-    public const string VERSION = '1.0.3';
+    public const string VERSION = '1.1.0';
 
     public string $root {
         get;
