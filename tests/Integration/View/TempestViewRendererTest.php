f2c03df89977fdf0008b40c1777f31cde91108ca
Márk Magyar <magyarheaven@gmail.com>
2025-07-10 07:46:35 +0200

    fix(view): remove multiline comments before AST parsing (#1395)

--- tests/Integration/View/TempestViewRendererTest.php
+++ tests/Integration/View/TempestViewRendererTest.php
@@ -727,4 +727,24 @@ final class TempestViewRendererTest extends FrameworkIntegrationTestCase
 
         $this->assertSnippetsMatch('<p>this is rendered text</p>', $html);
     }
+
+    public function test_multiline_view_comments(): void
+    {
+        $html = $this->render(<<<'HTML'
+        {{-- this is a comment
+                <div>
+                    <!-- Start -->
+                    <x-label>{{ Tempest\Intl\translate('test_2') }}</x-label>
+                <x-input
+                    name="test"
+                    type="text"
+                    class="py-2.5 sm:py-3 px-4 block w-full border-gray-500 border-1 rounded-lg sm:text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" placeholder="This is placeholder" />
+                <!-- end -->
+            </div>
+            --}}
+        <p>This should be rendered</p>
+        HTML);
+
+        $this->assertSnippetsMatch('<p>This should be rendered</p>', $html);
+    }
 }
