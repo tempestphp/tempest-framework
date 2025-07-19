361c2fbf6a39e9a0b17b99fff7e2991a62a85daa
Brent Roose <brent.roose@gmail.com>
2025-07-19 08:34:32 +0200

    fix(http): prevent CSRF token variable name collision (#1413)

--- packages/http/src/Session/x-csrf-token.view.php
+++ packages/http/src/Session/x-csrf-token.view.php
@@ -4,8 +4,6 @@ use Tempest\Http\Session\Session;
 
 use function Tempest\get;
 
-$name = Session::CSRF_TOKEN_KEY;
-$token = get(Session::class)->token;
 ?>
 
-<input type="hidden" name="{{ $name }}" value="{{ $token }}" />
+<input type="hidden" name="{{ Session::CSRF_TOKEN_KEY }}" value="{{ get(Session::class)->token }}" />
