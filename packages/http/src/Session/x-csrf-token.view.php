fd77e5365a5e5d07c454a4579a674517dc9a9499
Enzo Innocenzi <enzo@innocenzi.dev>
2025-07-20 13:13:43 +0200

    feat(http): add `csrf_token` function

--- packages/http/src/Session/x-csrf-token.view.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Tempest\Http\Session\Session;
-
-use function Tempest\get;
-
-?>
-
-<input type="hidden" name="{{ Session::CSRF_TOKEN_KEY }}" value="{{ get(Session::class)->token }}" />
