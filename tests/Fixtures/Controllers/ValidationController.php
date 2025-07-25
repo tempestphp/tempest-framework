97b232fb1bb912abebe0bf42a270dfda090cd13a
Tonko Mulder <tonko@tonkomulder.nl>
2025-07-25 16:16:52 +0200

    organized the other Request class

--- tests/Fixtures/Controllers/ValidationController.php
+++ tests/Fixtures/Controllers/ValidationController.php
@@ -12,6 +12,7 @@ use Tempest\Router\Get;
 use Tempest\Router\Post;
 use Tests\Tempest\Fixtures\Modules\Books\Models\Book;
 use Tests\Tempest\Fixtures\Requests\BookRequest;
+use Tests\Tempest\Fixtures\Requests\ValidationRequest;
 
 use function Tempest\uri;
 
@@ -24,7 +25,7 @@ final readonly class ValidationController
     }
 
     #[Post('/test-validation-responses')]
-    public function store(RequestForValidationController $request): Response // @mago-expect best-practices/no-unused-parameter
+    public function store(ValidationRequest $request): Response // @mago-expect best-practices/no-unused-parameter
     {
         return new Redirect(uri([self::class, 'get']));
     }
