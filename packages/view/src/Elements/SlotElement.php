47a9ad36719f95feabb2704efccfb7a1d68c1819
Brent Roose <brent.roose@gmail.com>
2025-07-25 09:49:42 +0200

    refactor(view): attach tokens to relevant elements for raw slot content support (#1428)

--- packages/view/src/Elements/SlotElement.php
+++ packages/view/src/Elements/SlotElement.php
@@ -5,12 +5,15 @@ declare(strict_types=1);
 namespace Tempest\View\Elements;
 
 use Tempest\View\Element;
+use Tempest\View\Parser\Token;
+use Tempest\View\WithToken;
 
-final class SlotElement implements Element
+final class SlotElement implements Element, WithToken
 {
     use IsElement;
 
     public function __construct(
+        public readonly Token $token,
         public readonly string $name,
         array $attributes = [],
     ) {
--- packages/view/src/Elements/TemplateElement.php
+++ packages/view/src/Elements/TemplateElement.php
@@ -5,13 +5,17 @@ declare(strict_types=1);
 namespace Tempest\View\Elements;
 
 use Tempest\View\Element;
+use Tempest\View\Parser\Token;
+use Tempest\View\WithToken;
 
-final class TemplateElement implements Element
+final class TemplateElement implements Element, WithToken
 {
     use IsElement;
 
-    public function __construct(array $attributes = [])
-    {
+    public function __construct(
+        public readonly Token $token,
+        array $attributes = [],
+    ) {
         $this->attributes = $attributes;
     }
 
