acace86ee39afefd81a4199f6c50831601d71624
Maarten Dekker <65236304+Maarten-Dekker@users.noreply.github.com>
2025-07-29 17:57:28 +0200

    feat(vite): support passing configuration to containerized environments (#1426)
    Co-authored-by: Enzo Innocenzi <enzo@innocenzi.dev>

--- packages/vite-plugin-tempest/src/config.ts
+++ packages/vite-plugin-tempest/src/config.ts
@@ -1,3 +1,5 @@
+import { readFileSync } from 'node:fs'
+import { resolve } from 'node:path'
 import type { TempestViteConfiguration } from './types'
 import { exec, php } from './utils'
 
@@ -5,23 +7,61 @@ const TEMPEST_BIN = 'tempest'
 const VITE_CONFIG_COMMAND = 'vite:config'
 
 export async function loadTempestConfiguration(): Promise<TempestViteConfiguration> {
-	try {
-		const override = process.env.TEMPEST_PLUGIN_CONFIGURATION_OVERRIDE
-		if (override) {
-			return JSON.parse(override)
-		}
+	return await loadConfigurationFromEnvironmentOverride()
+		?? await loadConfigurationFromEnvironmentPath()
+		?? await loadConfigurationFromTempestConsole()
+}
 
+async function loadConfigurationFromEnvironmentOverride(): Promise<TempestViteConfiguration | undefined> {
+	const override = process.env.TEMPEST_PLUGIN_CONFIGURATION_OVERRIDE
+
+	if (!override) {
+		return undefined
+	}
+
+	try {
+		return JSON.parse(override)
+	} catch (error) {
+		console.error(
+			`[vite-plugin-tempest] Could not parse configuration override from TEMPEST_PLUGIN_CONFIGURATION_OVERRIDE.`,
+		)
+		throw error
+	}
+}
+
+async function loadConfigurationFromEnvironmentPath(): Promise<TempestViteConfiguration | undefined> {
+	const path = process.env.TEMPEST_PLUGIN_CONFIGURATION_PATH
+
+	if (!path) {
+		return undefined
+	}
+
+	try {
+		const filePath = resolve(process.cwd(), path)
+		const fileContent = readFileSync(filePath, 'utf-8')
+
+		return JSON.parse(fileContent)
+	} catch (error) {
+		console.error(`[vite-plugin-tempest] Failed to read or parse the file at [${path}].`)
+		throw error
+	}
+}
+
+async function loadConfigurationFromTempestConsole(): Promise<TempestViteConfiguration> {
+	try {
 		const { stdout } = await exec(`${php.value} ${TEMPEST_BIN} ${VITE_CONFIG_COMMAND}`)
 		const json = stdout.match(/\{.*\}/s)
 
 		return JSON.parse(json?.[0] as string)
-	} catch (e) {
-		console.error(`Could not load configuration from [${php.value} ${TEMPEST_BIN} ${VITE_CONFIG_COMMAND}].`)
+	} catch (error) {
+		console.error(
+			`[vite-plugin-tempest] Could not load configuration from [${php.value} ${TEMPEST_BIN} ${VITE_CONFIG_COMMAND}].`,
+		)
 
-		if ((e as any).stdout) {
-			console.error((e as any).stdout)
+		if ((error as any).stdout) {
+			console.error((error as any).stdout)
 		}
 
-		throw e
+		throw error
 	}
 }
