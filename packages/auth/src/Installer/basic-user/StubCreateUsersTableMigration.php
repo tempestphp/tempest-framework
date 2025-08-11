948abb508ffd1d9311d7012125e1626ce382160e
Enzo Innocenzi <enzo@innocenzi.dev>
2025-08-11 02:59:55 +0200

    fix(auth): add missing `SkipDiscovery` attribute

--- packages/auth/src/Installer/basic-user/StubCreateUsersTableMigration.php
+++ packages/auth/src/Installer/basic-user/StubCreateUsersTableMigration.php
@@ -5,7 +5,9 @@ namespace Tempest\Auth\Installer;
 use Tempest\Database\MigratesUp;
 use Tempest\Database\QueryStatement;
 use Tempest\Database\QueryStatements\CreateTableStatement;
+use Tempest\Discovery\SkipDiscovery;
 
+#[SkipDiscovery]
 final class StubCreateUsersTableMigration implements MigratesUp
 {
     public string $name = '0000-00-00_create_users_table';
