19c881105297bfddbf366741167dccad03a13a4f
Guillaume Turpin <guillaume.turpin45@gmail.com>
2025-08-14 12:14:09 +0200

    :fire: Remove useless code

--- packages/auth/src/OAuth/IsOAuth2Provider.php
+++ packages/auth/src/OAuth/IsOAuth2Provider.php
@@ -4,14 +4,8 @@ declare(strict_types=1);
 
 namespace Tempest\Auth\OAuth;
 
-use Tempest\Container\Inject;
-use Tempest\HttpClient\HttpClient;
-
 trait IsOAuth2Provider
 {
-    #[Inject]
-    private readonly HttpClient $httpClient;
-
     public private(set) string $scopeSeparator = ' ';
 
     public function __construct(
