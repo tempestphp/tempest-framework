fad991b89a60f3947e4b4424750bde7635c53e1d
Enzo Innocenzi <enzo@innocenzi.dev>
2025-08-15 21:29:23 +0200

    feat: improve `make:migration`

--- packages/database/src/Stubs/UpMigrationStub.php
+++ packages/database/src/Stubs/UpMigrationStub.php
@@ -7,21 +7,17 @@ namespace Tempest\Database\Stubs;
 use Tempest\Database\MigratesUp;
 use Tempest\Database\QueryStatement;
 use Tempest\Database\QueryStatements\CreateTableStatement;
-use Tempest\Database\QueryStatements\DropTableStatement;
 use Tempest\Discovery\SkipDiscovery;
 
 #[SkipDiscovery]
-final class MigrationStub implements MigratesUp
+final class UpMigrationStub implements MigratesUp
 {
     public string $name = 'dummy-date_dummy-table-name';
 
     public function up(): QueryStatement
     {
-        return new CreateTableStatement(
-            tableName: 'dummy-table-name',
-        )
+        return new CreateTableStatement('dummy-table-name')
             ->primary()
-            ->text('name')
             ->datetime('created_at')
             ->datetime('updated_at');
     }
