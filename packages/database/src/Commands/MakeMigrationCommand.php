6efad06080c99e25862ad7632065e3972c8d6a8d
Enzo Innocenzi <enzo@innocenzi.dev>
2025-08-15 21:29:23 +0200

    feat: improve `make:migration`

--- packages/database/src/Commands/MakeMigrationCommand.php
+++ packages/database/src/Commands/MakeMigrationCommand.php
@@ -9,12 +9,14 @@ use Tempest\Console\ConsoleArgument;
 use Tempest\Console\ConsoleCommand;
 use Tempest\Core\PublishesFiles;
 use Tempest\Database\Enums\MigrationType;
-use Tempest\Database\Stubs\MigrationStub;
+use Tempest\Database\Stubs\ObjectMigrationStub;
+use Tempest\Database\Stubs\UpMigrationStub;
 use Tempest\Discovery\SkipDiscovery;
 use Tempest\Generation\ClassManipulator;
 use Tempest\Generation\DataObjects\StubFile;
 use Tempest\Generation\Exceptions\FileGenerationFailedException;
 use Tempest\Generation\Exceptions\FileGenerationWasAborted;
+use Tempest\Support\Str;
 use Tempest\Validation\Rules\EndsWith;
 use Tempest\Validation\Rules\IsNotEmptyString;
 
@@ -30,21 +32,21 @@ final class MakeMigrationCommand
         aliases: ['migration:make', 'migration:create', 'create:migration'],
     )]
     public function __invoke(
-        #[ConsoleArgument(
-            description: 'The file name of the migration',
-        )]
+        #[ConsoleArgument(description: 'The file name of the migration')]
         string $fileName,
-        #[ConsoleArgument(
-            name: 'type',
-            description: 'The type of the migration to create',
-        )]
+        #[ConsoleArgument(name: 'type', description: 'The type of the migration to create')]
         MigrationType $migrationType = MigrationType::OBJECT,
     ): void {
         try {
-            $stubFile = $this->getStubFileFromMigrationType($migrationType);
+            $stub = match ($migrationType) {
+                MigrationType::RAW => StubFile::from(dirname(__DIR__) . '/Stubs/migration.stub.sql'),
+                MigrationType::OBJECT => StubFile::from(ObjectMigrationStub::class),
+                MigrationType::UP => StubFile::from(UpMigrationStub::class),
+            };
+
             $targetPath = match ($migrationType) {
-                MigrationType::RAW => $this->generateRawFile($fileName, $stubFile),
-                default => $this->generateClassFile($fileName, $stubFile),
+                MigrationType::RAW => $this->generateRawFile($fileName, $stub),
+                default => $this->generateClassFile($fileName, $stub),
             };
 
             $this->success(sprintf('Migration file successfully created at "%s".', $targetPath));
@@ -53,36 +55,36 @@ final class MakeMigrationCommand
         }
     }
 
-    /**
-     * Generates a raw migration file.
-     * @param string $fileName The name of the file.
-     * @param StubFile $stubFile The stub file to use.
-     *
-     * @return string The path to the generated file.
-     */
-    private function generateRawFile(
-        string $fileName,
-        StubFile $stubFile,
-    ): string {
-        $now = date('Y-m-d');
-        $tableName = str($fileName)->snake()->toString();
-        $suggestedPath = str($this->getSuggestedPath('Dummy'))
-            ->replace(
-                ['Dummy', '.php'],
-                [$now . '_' . $tableName, '.sql'],
-            )
+    private function generateRawFile(string $filename, StubFile $stubFile): string
+    {
+        $tableName = str($filename)
+            ->snake()
+            ->stripStart('create')
+            ->stripEnd('table')
+            ->stripStart('_')
+            ->stripEnd('_')
             ->toString();
+
+        $filename = str($filename)
+            ->start('create_')
+            ->finish('_table')
+            ->toString();
+
+        $suggestedPath = Str\replace(
+            string: $this->getSuggestedPath('Dummy'),
+            search: ['Dummy', '.php'],
+            replace: [date('Y-m-d') . '_' . $filename, '.sql'],
+        );
 
         $targetPath = $this->promptTargetPath($suggestedPath, rules: [
             new IsNotEmptyString(),
             new EndsWith('.sql'),
         ]);
-        $shouldOverride = $this->askForOverride($targetPath);
 
         $this->stubFileGenerator->generateRawFile(
             stubFile: $stubFile,
             targetPath: $targetPath,
-            shouldOverride: $shouldOverride,
+            shouldOverride: $this->askForOverride($targetPath),
             replacements: [
                 'DummyTableName' => $tableName,
             ],
@@ -91,48 +93,41 @@ final class MakeMigrationCommand
         return $targetPath;
     }
 
-    /**
-     * Generates a class migration file.
-     *
-     * @param string $fileName The name of the file.
-     * @param StubFile $stubFile The stub file to use.
-     *
-     * @return string The path to the generated file.
-     */
-    private function generateClassFile(
-        string $fileName,
-        StubFile $stubFile,
-    ): string {
-        $suggestedPath = $this->getSuggestedPath($fileName);
-        $targetPath = $this->promptTargetPath($suggestedPath);
-        $shouldOverride = $this->askForOverride($targetPath);
+    private function generateClassFile(string $filename, StubFile $stubFile): string
+    {
+        $tableName = str($filename)
+            ->snake()
+            ->stripStart('create')
+            ->stripEnd('table')
+            ->stripStart('_')
+            ->stripEnd('_')
+            ->toString();
+
+        $filename = str($filename)
+            ->afterLast(['\\', '/'])
+            ->start('Create')
+            ->finish('Table')
+            ->when(
+                condition: Str\contains($filename, ['\\', '/']),
+                callback: fn ($path) => $path->prepend(Str\before_last($filename, ['\\', '/']), '/'),
+            )
+            ->toString();
+
+        $targetPath = $this->promptTargetPath($this->getSuggestedPath($filename));
 
         $this->stubFileGenerator->generateClassFile(
             stubFile: $stubFile,
             targetPath: $targetPath,
-            shouldOverride: $shouldOverride,
+            shouldOverride: $this->askForOverride($targetPath),
             replacements: [
                 'dummy-date' => date('Y-m-d'),
-                'dummy-table-name' => str($fileName)->snake()->toString(),
+                'dummy-table-name' => $tableName,
             ],
             manipulations: [
-                fn (ClassManipulator $class) => $class->removeClassAttribute(SkipDiscovery::class),
+                static fn (ClassManipulator $class) => $class->removeClassAttribute(SkipDiscovery::class),
             ],
         );
 
         return $targetPath;
-    }
-
-    private function getStubFileFromMigrationType(MigrationType $migrationType): StubFile
-    {
-        try {
-            return match ($migrationType) {
-                MigrationType::RAW => StubFile::from(dirname(__DIR__) . '/Stubs/migration.stub.sql'),
-                MigrationType::OBJECT => StubFile::from(MigrationStub::class), // @phpstan-ignore match.alwaysTrue (Because this is a guardrail for the future implementations)
-                default => throw new InvalidArgumentException(sprintf('The "%s" migration type has no supported stub file.', $migrationType->value)),
-            };
-        } catch (InvalidArgumentException $invalidArgumentException) {
-            throw new FileGenerationFailedException(sprintf('Cannot retrieve stub file: %s', $invalidArgumentException->getMessage()));
-        }
     }
 }
