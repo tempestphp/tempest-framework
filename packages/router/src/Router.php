351bd427f23de61b8f66368ce2ad13466b76843a
Enzo Innocenzi <enzo@innocenzi.dev>
2025-08-16 07:20:51 +0200

    feat(router)!: support signed URIs

--- packages/router/src/Router.php
+++ packages/router/src/Router.php
@@ -5,34 +5,12 @@ declare(strict_types=1);
 namespace Tempest\Router;
 
 use Psr\Http\Message\ServerRequestInterface as PsrRequest;
+use Tempest\DateTime\DateTime;
+use Tempest\DateTime\Duration;
 use Tempest\Http\Request;
 use Tempest\Http\Response;
 
 interface Router
 {
     public function dispatch(Request|PsrRequest $request): Response;
-
-    /**
-     * Creates a valid URI to the given `$action`.
-     *
-     * `$action` is one of :
-     * - Controller FQCN and its method as a tuple
-     * - Invokable controller FQCN
-     * - URI string starting with `/`
-     *
-     * @param array{class-string, string}|class-string|string $action
-     */
-    public function toUri(array|string $action, ...$params): string;
-
-    /**
-     * Checks if the URI to the given `$action` would match the current route.
-     *
-     * `$action` is one of :
-     * - Controller FQCN and its method as a tuple
-     * - Invokable controller FQCN
-     * - URI string starting with `/`
-     *
-     * @param array{class-string, string}|class-string|string $action
-     */
-    public function isCurrentUri(array|string $action, ...$params): bool;
 }
