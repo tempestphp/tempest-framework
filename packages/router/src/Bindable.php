c85920c8fc58771f32ce7853cf4f5d3923c87bd1
brendt <brent.roose@gmail.com>
2025-08-16 07:26:10 +0200

    wip

--- packages/router/src/Bindable.php
+++ packages/router/src/Bindable.php
@@ -4,6 +4,10 @@ namespace Tempest\Router;
 
 interface Bindable
 {
+    /**
+     * @var int|string
+     * The value being used to generate URIs when passed into the `\Tempest\Router\uri` function
+     */
     public int|string $bindingValue {
         get;
     }
