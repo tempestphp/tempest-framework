692ea392ae03ebc3a81f52e474bc822fc055cc3b
brendt <brent.roose@gmail.com>
2025-08-20 12:06:58 +0200

    wip

--- packages/upgrade/src/tempest2.php
+++ packages/upgrade/src/tempest2.php
@@ -2,13 +2,24 @@
 
 use Rector\Config\RectorConfig;
 use Rector\Renaming\Rector\Name\RenameClassRector;
+use Rector\Renaming\Rector\PropertyFetch\RenamePropertyRector;
+use Rector\Renaming\ValueObject\RenameProperty;
 
 return static function (RectorConfig $config) : void {
     $config->removeUnusedImports();
     $config->importNames();
     $config->importShortClasses();
+
     $config->ruleWithConfiguration(RenameClassRector::class, [
         'Tempest\\Database\\Id' => 'Tempest\\Database\\PrimaryKey',
         'Tempest\\CommandBus\\AsyncCommand' => 'Tempest\\CommandBus\\Async',
+    ]);
+
+    $config->ruleWithConfiguration(RenamePropertyRector::class, [
+        new RenameProperty(
+            type: 'Tempest\\Database\\PrimaryKey',
+            oldProperty: 'id',
+            newProperty: 'value'
+        )
     ]);
 };
