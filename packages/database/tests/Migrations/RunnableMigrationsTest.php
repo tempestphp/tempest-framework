666cf67a5fbb14061676d44fb48c2da138c4d150
Enzo Innocenzi <enzo@innocenzi.dev>
2025-08-29 20:44:39 +0200

    refactor: minor clean up

--- packages/database/tests/Migrations/RunnableMigrationsTest.php
+++ packages/database/tests/Migrations/RunnableMigrationsTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Tempest\Database\Tests\Migrations;
 
 use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
 use Tempest\Database\MigratesUp;
 use Tempest\Database\Migrations\RunnableMigrations;
@@ -14,15 +15,17 @@ use Tempest\Database\QueryStatements\RawStatement;
 final class RunnableMigrationsTest extends TestCase
 {
     #[DataProvider('provide_migrations')]
-    public function test_migration_ordering(array $migrationNames, array $expectedOrder): void
+    #[Test]
+    public function migration_ordering(array $migrationNames, array $expectedOrder): void
     {
         $migrations = array_map(fn (string $name) => $this->createDatabaseMigration($name), $migrationNames);
 
-        $runnableMigrations = new RunnableMigrations($migrations);
-
         $this->assertSame(
-            $expectedOrder,
-            $this->extractMigrationNames($runnableMigrations),
+            expected: $expectedOrder,
+            actual: array_map(
+                callback: static fn (MigratesUp $migration) => $migration->name,
+                array: iterator_to_array(new RunnableMigrations($migrations)),
+            ),
         );
     }
 
@@ -33,15 +36,19 @@ final class RunnableMigrationsTest extends TestCase
                 'migrationNames' => [
                     'migration1_1',
                     'migration1_10',
+                    '2025-10-12_create_idbn_table',
                     'migration1_2',
+                    '2025-01-12_create_author_table',
                     '2025-08-10_create_user_table',
                     '2025-08-01_create_book_table',
                     '2025-08-12_create_chapter_table',
                 ],
                 'expectedOrder' => [
+                    '2025-01-12_create_author_table',
                     '2025-08-01_create_book_table',
                     '2025-08-10_create_user_table',
                     '2025-08-12_create_chapter_table',
+                    '2025-10-12_create_idbn_table',
                     'migration1_1',
                     'migration1_2',
                     'migration1_10',
@@ -62,13 +69,5 @@ final class RunnableMigrationsTest extends TestCase
                 return new RawStatement('SELECT 1');
             }
         };
-    }
-
-    private function extractMigrationNames(RunnableMigrations $migrations): array
-    {
-        return array_map(
-            fn (MigratesUp $migration) => $migration->name,
-            iterator_to_array($migrations),
-        );
     }
 }
