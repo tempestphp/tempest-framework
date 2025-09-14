5d84af30830c344061607ac404634731aa540baa
Enzo Innocenzi <enzo@innocenzi.dev>
2025-09-14 19:45:44 +0200

    feat: suggest running migrations in installer

--- packages/auth/src/Installer/AuthenticationInstaller.php
+++ packages/auth/src/Installer/AuthenticationInstaller.php
@@ -4,10 +4,16 @@ declare(strict_types=1);
 
 namespace Tempest\Auth\Installer;
 
+use Tempest\Console\Console;
+use Tempest\Console\Input\ConsoleArgumentBag;
+use Tempest\Container\Container;
 use Tempest\Core\Installer;
 use Tempest\Core\PublishesFiles;
+use Tempest\Database\Migrations\MigrationManager;
 
+use function Tempest\root_path;
 use function Tempest\src_path;
+use function Tempest\Support\Namespace\to_fqcn;
 
 final class AuthenticationInstaller implements Installer
 {
@@ -15,17 +21,34 @@ final class AuthenticationInstaller implements Installer
 
     private(set) string $name = 'auth';
 
+    public function __construct(
+        private readonly MigrationManager $migrationManager,
+        private readonly Container $container,
+        private readonly Console $console,
+        private readonly ConsoleArgumentBag $consoleArgumentBag,
+    ) {}
+
     public function install(): void
     {
-        $publishFiles = [
-            __DIR__ . '/basic-user/StubUserModel.php' => src_path('Authentication/User.php'),
-            __DIR__ . '/basic-user/StubCreateUsersTableMigration.php' => src_path('Authentication/CreateUsersTable.php'),
-        ];
+        $migration = $this->publish(__DIR__ . '/basic-user/StubCreateUsersTableMigration.php', src_path('Authentication/CreateUsersTable.php'));
+        $this->publish(__DIR__ . '/basic-user/StubUserModel.php', src_path('Authentication/User.php'));
+        $this->publishImports();
 
-        foreach ($publishFiles as $source => $destination) {
-            $this->publish($source, $destination);
+        if ($migration && $this->shouldMigrate()) {
+            $this->migrationManager->executeUp(
+                migration: $this->container->get(to_fqcn($migration, root: root_path())),
+            );
+        }
+    }
+
+    private function shouldMigrate(): bool
+    {
+        $argument = $this->consoleArgumentBag->get('migrate');
+
+        if ($argument === null || ! is_bool($argument->value)) {
+            return $this->console->confirm('Do you want to execute migrations?', default: false);
         }
 
-        $this->publishImports();
+        return (bool) $argument->value;
     }
 }
