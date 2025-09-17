40f5eada34425f98b0b469ff13927f2b0a191980
Guillaume Turpin <guillaume.turpin45@gmail.com>
2025-09-17 16:46:58 +0200

    :sparkles: Create GithubProvider

---
+++ packages/auth/src/OAuth/League/GithubProvider.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tempest\Auth\OAuth\League;
+
+use Tempest\Auth\OAuth\DataObjects\OAuthUserData;
+
+final class GithubProvider
+{
+    use IsOauthProvider;
+
+    public function configure(
+        string $clientId,
+        string $clientSecret,
+        string $redirectUri,
+        ?array $defaultScopes = null,
+        string $stateSessionSlug = 'oauth-state'
+    ): self
+    {
+        $this->defaultScopes = $defaultScopes ??= ['user:email'];
+        $this->authorizeEndpoint = 'https://github.com/login/oauth/authorize';
+        $this->accessTokenEndpoint = 'https://github.com/login/oauth/access_token';
+        $this->userDataEndpoint = 'https://api.github.com/user';
+
+        return $this->configureInternalProvider(
+            clientId: $clientId,
+            clientSecret: $clientSecret,
+            defaultScopes: $defaultScopes,
+            redirectUri: $redirectUri,
+            authorizeEndpoint: $this->authorizeEndpoint,
+            accessTokenEndpoint: $this->accessTokenEndpoint,
+            userDataEndpoint: $this->userDataEndpoint,
+            stateSessionSlug: $stateSessionSlug
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function createUserDataFromResponse(array $userData): OAuthUserData
+    {
+        return new OAuthUserData(
+            id: $userData['id'] ?? null,
+            nickname: $userData['login'] ?? null,
+            name: $userData['name'] ?? null,
+            email: $userData['email'] ?? null,
+            avatar: $userData['avatar_url'] ?? null,
+            rawData: $userData
+        );
+    }
+}
