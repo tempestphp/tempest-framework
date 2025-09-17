fefba90fdc63a12aaa3ecf54b773d936479ed38a
Brent Roose <brendt@stitcher.io>
2025-09-17 07:05:39 +0200

    Update packages/upgrade/src/Set/TempestSetList.php

--- packages/upgrade/src/Set/TempestSetList.php
+++ packages/upgrade/src/Set/TempestSetList.php
@@ -6,5 +6,5 @@ namespace Tempest\Upgrade\Set;
 
 final class TempestSetList
 {
-    public const TEMPEST_20 = __DIR__ . '/../../config/sets/tempest20.php';
+public string const TEMPEST_20 = __DIR__ . '/../../config/sets/tempest20.php';
 }
