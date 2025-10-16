68946186ec1871835c070c028c5469c187f68396
aazsamir <sutikkukun@gmail.com>
2025-10-16 19:10:21 +0200

    fix(reflection): null return type if not defined

--- packages/reflection/tests/MethodReflectorTest.php
+++ packages/reflection/tests/MethodReflectorTest.php
@@ -5,6 +5,7 @@ namespace Tempest\Reflection\Tests;
 use PHPUnit\Framework\TestCase;
 use Tempest\Reflection\ClassReflector;
 use Tempest\Reflection\ParameterReflector;
+use Tempest\Reflection\Tests\Fixtures\NoReturnType;
 use Tempest\Reflection\Tests\Fixtures\TestClassA;
 
 final class MethodReflectorTest extends TestCase
@@ -22,6 +23,15 @@ final class MethodReflectorTest extends TestCase
             new ClassReflector(TestClassA::class)
                 ->getMethod('method')
                 ->getParameter('unknown'),
+        );
+    }
+
+    public function test_no_return_type_returns_null(): void
+    {
+        $this->assertNull(
+            new ClassReflector(NoReturnType::class)
+                ->getMethod('noReturnType')
+                ->getReturnType(),
         );
     }
 }
