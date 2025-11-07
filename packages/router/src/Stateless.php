fcf55d182fe30093e82e08570fc276cc86e76ae2
brendt <brent.roose@gmail.com>
2025-11-07 13:34:28 +0100

    wip

--- packages/router/src/Stateless.php
+++ packages/router/src/Stateless.php
@@ -4,6 +4,9 @@ namespace Tempest\Router;
 
 use Attribute;
 
+/**
+ * Mark a route handler as stateless, causing all cookie- and session-related middleware to be skipped.
+ */
 #[Attribute(Attribute::TARGET_METHOD)]
 final class Stateless
 {
