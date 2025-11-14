14a8da80880dda8eb6eb0b1222d75adf578908d5
Layla Tichy <contact@laylatichy.com>
2025-11-14 07:20:12 +0000

    feat(testing): inject app config in Integration test setup (#1710)

--- src/Tempest/Framework/Testing/IntegrationTest.php
+++ src/Tempest/Framework/Testing/IntegrationTest.php
@@ -122,6 +122,8 @@ abstract class IntegrationTest extends TestCase
         $container = $this->kernel->container;
         $this->container = $container;
 
+        $this->appConfig = $this->container->get(className: AppConfig::class);
+
         return $this;
     }
 
