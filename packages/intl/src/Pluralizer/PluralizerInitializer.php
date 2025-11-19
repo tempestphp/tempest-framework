e991ca2124f95e8988154e75ad42146a5cef75a6
Brent Roose <brent.roose@gmail.com>
2025-11-19 10:23:12 +0100

    fix(intl): make pluralizer singleton (#1726)

--- packages/intl/src/Pluralizer/PluralizerInitializer.php
+++ packages/intl/src/Pluralizer/PluralizerInitializer.php
@@ -6,9 +6,11 @@ namespace Tempest\Intl\Pluralizer;
 
 use Tempest\Container\Container;
 use Tempest\Container\Initializer;
+use Tempest\Container\Singleton;
 
 final readonly class PluralizerInitializer implements Initializer
 {
+    #[Singleton]
     public function initialize(Container $container): Pluralizer
     {
         return new InflectorPluralizer();
