47e53c2d5dc06fe0b4beaa764b7360c296952d4a
Mark <magyarheaven@gmail.com>
2025-12-03 21:57:03 +0100

    fix(vite-plugin): resolve hostname from BASE_URI not key path

--- packages/vite-plugin-tempest/src/plugin.ts
+++ packages/vite-plugin-tempest/src/plugin.ts
@@ -325,10 +325,6 @@ function resolveEnvironmentServerConfig(env: Record<string, string>): {
  * Resolve the host name from the environment.
  */
 function resolveHostFromEnv(env: Record<string, string>): string | undefined {
-	if (env.VITE_DEV_SERVER_KEY) {
-		return env.VITE_DEV_SERVER_KEY
-	}
-
 	try {
 		return new URL(env.BASE_URI).host
 	} catch {
