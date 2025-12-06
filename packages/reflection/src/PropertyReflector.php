f6b44232597ac1ab661fabc45424b1227c33afdc
Mark <magyarheaven@gmail.com>
2025-12-06 12:27:41 +0100

    feat(reflection): resolve short class names in @var docblock annotations

--- packages/reflection/src/PropertyReflector.php
+++ packages/reflection/src/PropertyReflector.php
@@ -5,11 +5,19 @@ declare(strict_types=1);
 namespace Tempest\Reflection;
 
 use Error;
+use PhpToken;
+use ReflectionClass;
 use ReflectionProperty as PHPReflectionProperty;
 
 final class PropertyReflector implements Reflector
 {
     use HasAttributes;
+
+    /** @var array<string, array<string, string>> */
+    private static array $useStatementCache = [];
+
+    /** @var array<string, string> */
+    private static array $resolvedTypeCache = [];
 
     public function __construct(
         private readonly PHPReflectionProperty $reflectionProperty,
@@ -94,17 +102,197 @@ final class PropertyReflector implements Reflector
     {
         $doc = $this->reflectionProperty->getDocComment();
 
-        if (! $doc) {
+        if (! $doc || ! preg_match('/@var\s+(?:(?:array|list)<([\\\\\w]+)>|([\\\\\w]+)\[\])/', $doc, $match)) {
             return null;
         }
 
-        preg_match('/@var ([\\\\\w]+)\[]/', $doc, $match);
+        $rawType = $match[1] !== '' ? $match[1] : $match[2];
+        $typeName = ltrim($rawType, '\\');
 
-        if (! isset($match[1])) {
+        return str_contains($rawType, '\\')
+            ? new TypeReflector($typeName)
+            : new TypeReflector($this->resolveShortClassName($typeName));
+    }
+
+    private function resolveShortClassName(string $shortName): string
+    {
+        $declaringClass = $this->reflectionProperty->getDeclaringClass();
+        $cacheKey = $declaringClass->getName() . '::' . $shortName;
+
+        return self::$resolvedTypeCache[$cacheKey] ??= $this->doResolveShortClassName($shortName, $declaringClass);
+    }
+
+    private function doResolveShortClassName(string $shortName, ReflectionClass $declaringClass): string
+    {
+        $fileName = $declaringClass->getFileName();
+
+        if ($fileName) {
+            self::$useStatementCache[$fileName] ??= $this->parseUseStatements($fileName);
+
+            if (isset(self::$useStatementCache[$fileName][$shortName])) {
+                return self::$useStatementCache[$fileName][$shortName];
+            }
+        }
+
+        $namespace = $declaringClass->getNamespaceName();
+
+        if ($namespace !== '') {
+            $fqcn = $namespace . '\\' . $shortName;
+
+            if (class_exists($fqcn)) {
+                return $fqcn;
+            }
+        }
+
+        return $shortName;
+    }
+
+    /** @return array<string, string> */
+    private function parseUseStatements(string $fileName): array
+    {
+        $content = file_get_contents($fileName);
+
+        if ($content === false) {
+            return [];
+        }
+
+        $tokens = PhpToken::tokenize($content);
+        $useStatements = [];
+        $count = count($tokens);
+
+        for ($i = 0; $i < $count; $i++) {
+            $token = $tokens[$i];
+
+            if ($token->is([T_CLASS, T_INTERFACE, T_TRAIT, T_ENUM])) {
+                break;
+            }
+
+            if (! $token->is(T_USE)) {
+                continue;
+            }
+
+            $i++;
+            $this->skipWhitespaceTokens($tokens, $i, $count);
+
+            if ($tokens[$i]->is([T_FUNCTION, T_CONST])) {
+                continue;
+            }
+
+            $fqcn = $this->parseNamespacedName($tokens, $i, $count);
+            $this->skipWhitespaceTokens($tokens, $i, $count);
+
+            if ($tokens[$i]->text === '{') {
+                $this->parseGroupUse($tokens, $i, $count, $fqcn, $useStatements);
+                continue;
+            }
+
+            $alias = $this->parseAlias($tokens, $i, $count) ?? $this->getShortName($fqcn);
+            $useStatements[$alias] = $fqcn;
+
+            while ($i < $count && $tokens[$i]->text !== ';') {
+                $i++;
+            }
+        }
+
+        return $useStatements;
+    }
+
+    /** @param PhpToken[] $tokens */
+    private function skipWhitespaceTokens(array $tokens, int &$i, int $count): void
+    {
+        while ($i < $count && $tokens[$i]->is([T_WHITESPACE, T_COMMENT])) {
+            $i++;
+        }
+    }
+
+    /** @param PhpToken[] $tokens */
+    private function parseNamespacedName(array $tokens, int &$i, int $count): string
+    {
+        $name = '';
+
+        while ($i < $count) {
+            $token = $tokens[$i];
+
+            if ($token->is([T_STRING, T_NAME_QUALIFIED, T_NAME_FULLY_QUALIFIED, T_NS_SEPARATOR])) {
+                $name .= $token->text;
+                $i++;
+            } elseif ($token->is(T_WHITESPACE)) {
+                $i++;
+            } else {
+                break;
+            }
+        }
+
+        return ltrim($name, '\\');
+    }
+
+    /** @param PhpToken[] $tokens */
+    private function parseAlias(array $tokens, int &$i, int $count): ?string
+    {
+        if (! $tokens[$i]->is(T_AS)) {
             return null;
         }
 
-        return new TypeReflector(ltrim($match[1], '\\'));
+        $i++;
+        $this->skipWhitespaceTokens($tokens, $i, $count);
+
+        if ($tokens[$i]->is(T_STRING)) {
+            return $tokens[$i++]->text;
+        }
+
+        return null;
+    }
+
+    /**
+     * @param PhpToken[] $tokens
+     * @param array<string, string> $useStatements
+     */
+    private function parseGroupUse(array $tokens, int &$i, int $count, string $prefix, array &$useStatements): void
+    {
+        $i++;
+
+        while ($i < $count && $tokens[$i]->text !== '}') {
+            $this->skipWhitespaceTokens($tokens, $i, $count);
+
+            if ($tokens[$i]->is([T_FUNCTION, T_CONST])) {
+                while ($i < $count && $tokens[$i]->text !== ',' && $tokens[$i]->text !== '}') {
+                    $i++;
+                }
+
+                if ($tokens[$i]->text === ',') {
+                    $i++;
+                }
+
+                continue;
+            }
+
+            $name = $this->parseNamespacedName($tokens, $i, $count);
+
+            if ($name === '') {
+                $i++;
+                continue;
+            }
+
+            $this->skipWhitespaceTokens($tokens, $i, $count);
+
+            $alias = $this->parseAlias($tokens, $i, $count) ?? $this->getShortName($name);
+            $useStatements[$alias] = $prefix . '\\' . $name;
+
+            $this->skipWhitespaceTokens($tokens, $i, $count);
+
+            if ($tokens[$i]->text === ',') {
+                $i++;
+            }
+        }
+
+        $i++;
+    }
+
+    private function getShortName(string $fqcn): string
+    {
+        $pos = strrpos($fqcn, '\\');
+
+        return $pos === false ? $fqcn : substr($fqcn, $pos + 1);
     }
 
     public function isUninitialized(object $object): bool
