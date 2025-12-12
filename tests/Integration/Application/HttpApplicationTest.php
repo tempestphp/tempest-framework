50b7f9782cd7ff9a77d4a88be585e7582a1950a3
brendt <brent.roose@gmail.com>
2025-12-12 10:22:49 +0100

    wip

--- tests/Integration/Application/HttpApplicationTest.php
+++ tests/Integration/Application/HttpApplicationTest.php
@@ -4,7 +4,15 @@ declare(strict_types=1);
 
 namespace Tests\Tempest\Integration\Application;
 
+use PHPUnit\Framework\Attributes\Test;
+use Symfony\Component\Process\Process;
+use Tempest\Http\Session\Session;
+use Tempest\HttpClient\HttpClient;
+use Tests\Tempest\Fixtures\Controllers\ControllerWithoutSession;
+use Tests\Tempest\Fixtures\Controllers\ControllerWithSession;
 use Tests\Tempest\Integration\FrameworkIntegrationTestCase;
+use function Tempest\Router\uri;
+use function Tempest\Support\arr;
 
 /**
  * @internal
@@ -16,5 +24,30 @@ final class HttpApplicationTest extends FrameworkIntegrationTestCase
         $this->http
             ->get('/')
             ->assertOk();
+    }
+
+    #[Test]
+    public function session_is_only_cleaned_when_started(): void
+    {
+        $this->appConfig->baseUri = 'http://127.0.0.1:8081';
+        $process = new Process(['./tempest', 'serve', '127.0.0.1:8081'], getcwd());
+        $process->start();
+        usleep(200_000);
+
+        $client = $this->get(HttpClient::class);
+
+        $response = $client->get(uri(ControllerWithoutSession::class));
+        $cookies = arr($response->getHeader('set-cookie')->values ?? [])
+            ->map(fn (string $cookie) => explode('=', $cookie)[0] ?? null);
+
+        $this->assertFalse($cookies->contains('tempest_session_id'));
+
+        $response = $client->get(uri(ControllerWithSession::class));
+        $cookies = arr($response->getHeader('set-cookie')->values ?? [])
+            ->map(fn (string $cookie) => explode('=', $cookie)[0] ?? null);
+
+        $this->assertTrue($cookies->contains('tempest_session_id'));
+
+        $process->stop();
     }
 }
