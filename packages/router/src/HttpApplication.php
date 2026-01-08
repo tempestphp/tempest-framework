ae1d4d96ed89141550dd09763b4042e2aa049f1a
Tim Düsterhus <tim@tideways-gmbh.com>
2026-01-08 12:36:08 +0100

    refactor(router): make `HttpApplication::run()` return `never`
    In practice this was already the case, since `Kernel::shutdown()` is returning
    `never` (by calling `exit()` internally). Since we don’t catch any possible
    exceptions either, this must extend to `ConsoleApplication::run()` to also
    `never` return.
    
    This also means that the `exit()` in `Installers/index.php` is dead code as has
    correctly been pointed out by PHPStan.

--- packages/router/src/HttpApplication.php
+++ packages/router/src/HttpApplication.php
@@ -25,7 +25,7 @@ final readonly class HttpApplication implements Application
         return Tempest::boot($root, $discoveryLocations)->get(HttpApplication::class);
     }
 
-    public function run(): void
+    public function run(): never
     {
         $router = $this->container->get(Router::class);
 
