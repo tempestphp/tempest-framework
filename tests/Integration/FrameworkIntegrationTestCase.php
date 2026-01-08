5f6bfdc1e589f0c0925e4e31bc388e826774f107
Mark <magyarheaven@gmail.com>
2026-01-08 13:04:02 +0100

    refactor(testing): remove migrated methods from FrameworkIntegrationTestCase
    Remove render(), registerViewComponent(), registerRoute(), and
    registerStaticPage() methods now available via ViewTester and HttpRouterTester.

--- tests/Integration/FrameworkIntegrationTestCase.php
+++ tests/Integration/FrameworkIntegrationTestCase.php
@@ -4,27 +4,13 @@ declare(strict_types=1);
 
 namespace Tests\Tempest\Integration;
 
-use InvalidArgumentException;
 use Stringable;
 use Tempest\Database\Builder\ModelInspector;
 use Tempest\Database\DatabaseInitializer;
 use Tempest\Discovery\DiscoveryLocation;
 use Tempest\Framework\Testing\IntegrationTest;
-use Tempest\Reflection\MethodReflector;
-use Tempest\Router\Route;
-use Tempest\Router\RouteConfig;
-use Tempest\Router\RouteDecorator;
-use Tempest\Router\Routing\Construction\DiscoveredRoute;
-use Tempest\Router\Routing\Construction\RouteConfigurator;
-use Tempest\Router\Static\StaticPageConfig;
-use Tempest\Router\StaticPage;
 use Tempest\Support\Filesystem;
 use Tempest\Support\Path;
-use Tempest\View\GenericView;
-use Tempest\View\View;
-use Tempest\View\ViewComponent;
-use Tempest\View\ViewConfig;
-use Tempest\View\ViewRenderer;
 
 use function Tempest\Support\str;
 
@@ -59,79 +45,9 @@ abstract class FrameworkIntegrationTestCase extends IntegrationTest
         ModelInspector::reset();
     }
 
-    protected function render(string|View $view, mixed ...$params): string
-    {
-        if (is_string($view)) {
-            $view = new GenericView($view);
-        }
-
-        $view->data(...$params);
-
-        return $this->container->get(ViewRenderer::class)->render($view);
-    }
-
-    protected function registerViewComponent(string $name, string $html, string $file = '', bool $isVendor = false): void
-    {
-        $viewComponent = new ViewComponent(
-            name: $name,
-            contents: $html,
-            file: $file,
-            isVendorComponent: $isVendor,
-        );
-
-        $this->container->get(ViewConfig::class)->addViewComponent($viewComponent);
-    }
-
     protected function assertStringCount(string $subject, string $search, int $count): void
     {
         $this->assertSame($count, substr_count($subject, $search));
-    }
-
-    protected function registerRoute(array|string|MethodReflector $action): void
-    {
-        $reflector = match (true) {
-            $action instanceof MethodReflector => $action,
-            is_array($action) => MethodReflector::fromParts(...$action),
-            default => MethodReflector::fromParts($action, '__invoke'),
-        };
-
-        if ($reflector->getAttribute(Route::class) === null) {
-            throw new InvalidArgumentException('Missing route attribute');
-        }
-
-        $configurator = $this->container->get(RouteConfigurator::class);
-
-        $configurator->addRoute(
-            DiscoveredRoute::fromRoute(
-                $reflector->getAttribute(Route::class),
-                [
-                    ...$reflector->getDeclaringClass()->getAttributes(RouteDecorator::class),
-                    ...$reflector->getAttributes(RouteDecorator::class),
-                ],
-                $reflector,
-            ),
-        );
-
-        $routeConfig = $this->container->get(RouteConfig::class);
-        $routeConfig->apply($configurator->toRouteConfig());
-    }
-
-    protected function registerStaticPage(array|string|MethodReflector $action): void
-    {
-        $reflector = match (true) {
-            $action instanceof MethodReflector => $action,
-            is_array($action) => MethodReflector::fromParts(...$action),
-            default => MethodReflector::fromParts($action, '__invoke'),
-        };
-
-        if ($reflector->getAttribute(StaticPage::class) === null) {
-            throw new InvalidArgumentException('Missing static page attribute');
-        }
-
-        $this->container->get(StaticPageConfig::class)->addHandler(
-            $reflector->getAttribute(StaticPage::class),
-            $reflector,
-        );
     }
 
     protected function assertSnippetsMatch(string $expected, string $actual): void
