f42e271033a3e6e5efb821ed618f8c02f3d755de
Enzo Innocenzi <enzo@innocenzi.dev>
2026-01-08 17:15:36 +0100

    refactor(reflection)!: move functions in the `Tempest\Reflection` namespace

--- packages/upgrade/config/sets/tempest30.php
+++ packages/upgrade/config/sets/tempest30.php
@@ -6,6 +6,7 @@ use Tempest\Upgrade\Tempest3\UpdateEventFunctionImportsRector;
 use Tempest\Upgrade\Tempest3\UpdateExceptionProcessorRector;
 use Tempest\Upgrade\Tempest3\UpdateHasContextRector;
 use Tempest\Upgrade\Tempest3\UpdateMapperFunctionImportsRector;
+use Tempest\Upgrade\Tempest3\UpdateReflectionFunctionImportsRector;
 use Tempest\Upgrade\Tempest3\UpdateViewFunctionImportsRector;
 
 return static function (RectorConfig $config): void {
@@ -15,6 +16,7 @@ return static function (RectorConfig $config): void {
     $config->rule(UpdateCommandFunctionImportsRector::class);
     $config->rule(UpdateEventFunctionImportsRector::class);
     $config->rule(UpdateMapperFunctionImportsRector::class);
+    $config->rule(UpdateReflectionFunctionImportsRector::class);
     $config->rule(UpdateViewFunctionImportsRector::class);
     $config->rule(UpdateExceptionProcessorRector::class);
     $config->rule(UpdateHasContextRector::class);
