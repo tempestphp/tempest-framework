9c6e54de2a7fa984ca716eecd1f22001620bc4fc
iamdadmin <iamdad_min@outlook.com>
2026-01-19 15:37:55 +0000

    feat(auth): Updated test

--- tests/Integration/Auth/Installer/OAuthInstallerTest.php
+++ tests/Integration/Auth/Installer/OAuthInstallerTest.php
@@ -121,6 +121,11 @@ final class OAuthInstallerTest extends FrameworkIntegrationTestCase
                 'expectedConfigPath' => 'App/Authentication/OAuth/slack.config.php',
                 'expectedControllerPath' => 'App/Authentication/OAuth/SlackController.php',
             ],
+            'twitchhelix' => [
+                'provider' => SupportedOAuthProvider::TWITCHHELIX,
+                'expectedConfigPath' => 'App/Authentication/OAuth/twitchhelix.config.php',
+                'expectedControllerPath' => 'App/Authentication/OAuth/TwitchHelixController.php',
+            ],
         ];
     }
 }
