7306df94ebb83b749a150a384c4bbc42e6fb2cf3
Enzo Innocenzi <enzo@innocenzi.dev>
2026-01-27 11:03:31 +0100

    refactor: update exception messages

--- packages/cryptography/src/Encryption/Exceptions/EncryptionKeyWasInvalid.php
+++ packages/cryptography/src/Encryption/Exceptions/EncryptionKeyWasInvalid.php
@@ -17,16 +17,16 @@ final class EncryptionKeyWasInvalid extends Exception implements EncryptionExcep
     public static function becauseItIsMissing(EncryptionAlgorithm $algorithm): self
     {
         return new self(
-            'The encryption key is missing or empty. Ensure you have a `SIGNING_KEY` environment variable, or generate one with `php tempest key:generate`.',
-            $algorithm,
+            message: 'The encryption key is missing or empty. Generate a valid `SIGNING_KEY` with `php tempest key:generate`.',
+            algorithm: $algorithm,
         );
     }
 
     public static function becauseLengthMismatched(EncryptionAlgorithm $algorithm, int $actualLength): self
     {
         return new self(
-            "The encryption key length ({$actualLength}) does not match the expected length ({$algorithm->getKeyLength()}). Generate a valid key with `php tempest key:generate`.",
-            $algorithm,
+            message: "The encryption key length ({$actualLength}) does not match the expected length ({$algorithm->getKeyLength()}). Generate a valid `SIGNING_KEY` with `php tempest key:generate`.",
+            algorithm: $algorithm,
         );
     }
 }
--- packages/cryptography/src/Signing/Exceptions/SigningKeyWasInvalid.php
+++ packages/cryptography/src/Signing/Exceptions/SigningKeyWasInvalid.php
@@ -8,6 +8,6 @@ final class SigningKeyWasInvalid extends Exception implements SigningException
 {
     public static function becauseItIsMissing(): self
     {
-        return new self('The signing key is missing or empty. Ensure you have a `SIGNING_KEY` environment variable, or generate one with `php tempest key:generate`.');
+        return new self('The signing key is missing or empty. Generate a valid `SIGNING_KEY` with `php tempest key:generate`.');
     }
 }
