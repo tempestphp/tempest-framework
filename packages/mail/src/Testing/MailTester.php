75d54277374185d933a13408cd0f94bac6d32757
Mark <magyarheaven@gmail.com>
2026-02-15 04:03:31 +0100

    fix(mail): remove dead code in MailTester

--- packages/mail/src/Testing/MailTester.php
+++ packages/mail/src/Testing/MailTester.php
@@ -21,7 +21,6 @@ use function Tempest\Support\arr;
 
 final class MailTester
 {
-    private ?Email $sentEmail = null;
     private ?SymfonyEmail $sentSymfonyEmail = null;
 
     public function __construct(
@@ -32,7 +31,6 @@ final class MailTester
     {
         $this->mailer->send($email);
 
-        $this->sentEmail = $email;
         $this->sentSymfonyEmail = map($email)->with(EmailToSymfonyEmailMapper::class)->do();
 
         return $this;
@@ -385,8 +383,6 @@ final class MailTester
             $filename,
             Arr\join(Arr\map($attachments, fn (DataPart $attachment) => $attachment->getName())),
         ));
-
-        return $this;
     }
 
     /**
