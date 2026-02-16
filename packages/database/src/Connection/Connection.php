5864ced8f09fb96fe137c99dbb7a28c872b9c6a3
Mark <magyarheaven@gmail.com>
2026-02-16 16:26:31 +0100

    fix(database): add config property to Connection interface

--- packages/database/src/Connection/Connection.php
+++ packages/database/src/Connection/Connection.php
@@ -5,9 +5,14 @@ declare(strict_types=1);
 namespace Tempest\Database\Connection;
 
 use PDOStatement;
+use Tempest\Database\Config\DatabaseConfig;
 
 interface Connection
 {
+    public DatabaseConfig $config {
+        get;
+    }
+
     public function beginTransaction(): bool;
 
     public function commit(): bool;
