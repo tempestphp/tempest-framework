633a6e0e1486bda7bf86628fb2065228116a053f
Layla Tichy <contact@laylatichy.com>
2026-03-21 22:16:16 +0000

    feat: add HasTableAlias trait for table aliasing

---
+++ packages/database/src/HasTableAlias.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tempest\Database;
+
+use function Tempest\Support\str;
+
+trait HasTableAlias
+{
+    private function getTableAlias(string $tableName): string
+    {
+        if ($this->parent === null || $this->parent === '') {
+            return $tableName;
+        }
+
+        return str(string: $this->parent)->replace('.', '_')->append('_', $this->property->getName())->toString();
+    }
+}
