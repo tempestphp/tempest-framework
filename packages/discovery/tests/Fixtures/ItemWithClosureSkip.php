56da68e90e4e174793c886533bc654eba8e1f0bc
brendt <brent.roose@gmail.com>
2026-03-24 13:54:07 +0100

    wip

--- packages/discovery/tests/Fixtures/ItemWithClosureSkip.php
+++ packages/discovery/tests/Fixtures/ItemWithClosureSkip.php
@@ -4,12 +4,17 @@ namespace Tempest\Discovery\Tests\Fixtures;
 
 use Psr\Container\ContainerInterface;
 use Tempest\Discovery\SkipDiscovery;
+use Throwable;
 
 #[SkipDiscovery(static function (ContainerInterface $container): bool {
     if (! $container->has(DependencyForItemWithClosureSkip::class)) {
-        return false;
+        return true;
     }
 
-    return $container->get(DependencyForItemWithClosureSkip::class)->shouldSkip;
+    try {
+        return $container->get(DependencyForItemWithClosureSkip::class)->shouldSkip;
+    } catch (Throwable) {
+        return true;
+    }
 })]
 final class ItemWithClosureSkip {}
