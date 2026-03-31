e86dae88b23f88b12f3897568d74e8532a9985f5
Jonas Regner <regnerjonas@protonmail.com>
2026-03-31 14:07:24 +0200

    feat(database): set char default size to 1

--- packages/database/src/QueryStatements/CreateTableStatement.php
+++ packages/database/src/QueryStatements/CreateTableStatement.php
@@ -186,7 +186,7 @@ final class CreateTableStatement implements QueryStatement, HasTrailingStatement
     /**
      * Adds a `CHAR` column to the table.
      */
-    public function char(string $name, int $size, bool $nullable = false, ?string $default = null): self
+    public function char(string $name, bool $nullable = false, ?string $default = null, int $size = 1): self
     {
         $this->statements[] = new CharStatement(
             name: $name,
