e0220bc4ad691eceaa964aabbc97df8e562a0131
Jonas Regner <regnerjonas@protonmail.com>
2026-04-01 15:39:07 +0200

    test(database): fix wrong argument order

--- tests/Integration/Database/QueryStatements/CreateTableStatementTest.php
+++ tests/Integration/Database/QueryStatements/CreateTableStatementTest.php
@@ -34,7 +34,7 @@ final class CreateTableStatementTest extends FrameworkIntegrationTestCase
             {
                 return new CreateTableStatement('test_table')
                     ->text('text', default: 'default')
-                    ->char('char', 7, default: 'default')
+                    ->char('char', default: 'default', size: 7)
                     ->varchar('varchar', default: 'default')
                     ->float('float', default: 0.1)
                     ->integer('integer', default: 1)
