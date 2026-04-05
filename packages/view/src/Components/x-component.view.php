bc44fac7429c1336a1ee8bb56aaf6f7e37a8c549
iamdadmin <iamdad_min@outlook.com>
2026-04-05 06:43:36 +0100

    fix(view): handle data attributes in x-component

--- packages/view/src/Components/x-component.view.php
+++ packages/view/src/Components/x-component.view.php
@@ -10,9 +10,20 @@ use Tempest\View\Slot;
 use function Tempest\Container\get;
 use function Tempest\View\view;
 
-$attributeString = $attributes
-    ->map(fn (string $value, string $key) => "{$key}=\"{$value}\"")
-    ->implode(' ');
+$htmlAttributes = [];
+$dataVariables = [];
+
+foreach ($attributes as $key => $value) {
+    if (is_object($value) || is_array($value)) {
+        $dataVariables[$key] = $value;
+    } elseif ($value === true) {
+        $htmlAttributes[] = $key;
+    } elseif ($value !== false && $value !== null) {
+        $htmlAttributes[] = "{$key}=\"{$value}\"";
+    }
+}
+
+$attributeString = implode(' ', $htmlAttributes);
 
 $content = $slots[Slot::DEFAULT]->content ?? '';
 
@@ -24,6 +35,7 @@ HTML, $is, $attributeString, $content, $is);
 
 $data = $scopedVariables ?? $_data ?? [];
 $data = is_array($data) ? $data : [];
+$data = array_merge($data, $dataVariables);
 $html = get(TempestViewRenderer::class)->render(view($template, ...$data));
 ?>
 
