40ecf5100da31a49db107728bd67ebf70ddd2ec6
rexpl <75545403+rexpl@users.noreply.github.com>
2026-05-21 18:15:25 +0200

    config property docblocks

--- packages/http/src/Cookie/CookieConfig.php
+++ packages/http/src/Cookie/CookieConfig.php
@@ -7,7 +7,18 @@ namespace Tempest\Http\Cookie;
 final class CookieConfig
 {
     public function __construct(
+        /**
+         * Whether to discard cookies that cannot be decrypted.
+         * What this means: any cookies not encrypted by your application (or not whitelisted) that
+         * arrive with a request, will prompt tempest to request the browser to forget these cookies.
+         * Cookies sent unencrypted and not whitelisted will also not be available in the request object.
+         */
         public bool $discardUnencryptedCookies = true,
+
+        /**
+         * List of cookies that will not be decrypted by tempest, be available in the request object.
+         * Outgoing whitelisted cookies will be sent to the browser in plaintext.
+         */
         public array $plaintextCookies = [],
     ) {}
 }
