ac08a69e618bd1717c44ad0521add7b1a1502750
brendt <brent.roose@gmail.com>
2026-06-18 11:05:03 +0200

    Add rectors

--- packages/upgrade/src/Set/TempestLevelSetList.php
+++ packages/upgrade/src/Set/TempestLevelSetList.php
@@ -15,4 +15,6 @@ final class TempestLevelSetList
     public const string UP_TO_TEMPEST_34 = __DIR__ . '/../../config/sets/level/up-to-tempest-34.php';
 
     public const string UP_TO_TEMPEST_310 = __DIR__ . '/../../config/sets/level/up-to-tempest-310.php';
+
+    public const string UP_TO_TEMPEST_312 = __DIR__ . '/../../config/sets/level/up-to-tempest-312.php';
 }
