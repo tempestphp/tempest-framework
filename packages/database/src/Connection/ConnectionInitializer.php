d0f23375758e153301fc673ef55c92e37fd4ac44
brendt <brent.roose@gmail.com>
2026-06-19 08:47:31 +0200

    Prevent stale connections

--- packages/database/src/Connection/ConnectionInitializer.php
+++ packages/database/src/Connection/ConnectionInitializer.php
@@ -11,25 +11,37 @@ use Tempest\Database\Config\DatabaseConfig;
 
 final class ConnectionInitializer implements Initializer
 {
-    private static ?Connection $connection = null;
+    /** @var Connection[] */
+    private static array $connections = [];
 
     #[Singleton]
     public function initialize(Container $container): Connection
     {
         $config = $container->get(DatabaseConfig::class);
+        $connectionKey = $this->getConnectionKey($config);
 
         $connection = $config->usePersistentConnection
-            ? self::$connection
+            ? self::$connections[$connectionKey] ?? null
             : null;
 
         if (! $connection instanceof Connection) {
             $connection = new PDOConnection($config);
             $connection->connect();
-            self::$connection = $connection;
+            self::$connections[$connectionKey] = $connection;
         } elseif ($connection->ping() === false) {
             $connection->reconnect();
         }
 
         return $connection;
+    }
+
+    private function getConnectionKey(DatabaseConfig $config): string
+    {
+        return hash('xxh128', serialize([
+            $config->dsn,
+            $config->username,
+            $config->options,
+            $config->password,
+        ]));
     }
 }
