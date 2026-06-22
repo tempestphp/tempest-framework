9892de4b2afcb29be5875d17fdf233bdf7d1e727
brendt <brent.roose@gmail.com>
2026-06-22 12:53:34 +0200

    wip

--- packages/discovery/src/BootDiscovery.php
+++ packages/discovery/src/BootDiscovery.php
@@ -209,7 +209,8 @@ final class BootDiscovery
                 } elseif (class_exists($className)) {
                     $input = new ClassReflector($className);
                 }
-            } catch (AssertionError|InvalidPestCommand|TestAlreadyExist) { // @phpstan-ignore class.notFound
+            // @phpstan-ignore-next-line
+            } catch (AssertionError|InvalidPestCommand|TestAlreadyExist) {
                 // Workaround for Pest test files autoloading.
                 // @mago-expect lint:no-empty-catch-clause
             }
