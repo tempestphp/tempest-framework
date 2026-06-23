f373316f4d01ad51eed28e35f4c9b7606a25916c
brendt <brent.roose@gmail.com>
2026-06-23 09:22:37 +0200

    wip

--- src/Tempest/Framework/Installers/FrameworkInstaller.php
+++ src/Tempest/Framework/Installers/FrameworkInstaller.php
@@ -50,6 +50,7 @@ final class FrameworkInstaller
             callback: function (string $_, string $destination): void {
                 $claude = root_path('CLAUDE.md');
 
+                // @phpstan-ignore-next-line
                 exec("ln -s {$destination} {$claude}");
             },
         );
