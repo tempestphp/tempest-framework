f0bc1fd12d64182a5743460b6ac0930a79e7ad68
brendt <brent.roose@gmail.com>
2026-07-22 10:13:39 +0200

    wip

--- packages/router/src/RouteBindingInitializer.php
+++ packages/router/src/RouteBindingInitializer.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace Tempest\Router;
 
-use Tempest\Reflection\ParameterReflector;
 use Tempest\Container\Container;
 use Tempest\Container\DynamicInitializer;
 use Tempest\Reflection\ClassReflector;
+use Tempest\Reflection\ParameterReflector;
 use Tempest\Router\Exceptions\RouteBindingDidNotSupportRelations;
 use Tempest\Router\Exceptions\RouteBindingFailed;
 use UnitEnum;
@@ -44,7 +44,7 @@ final class RouteBindingInitializer implements DynamicInitializer
         if ($withRelations !== null) {
             $resolve = $class->getMethod('resolve');
 
-            if (!$resolve->getParameter('relations') instanceof ParameterReflector && ! $resolve->getReflection()->isVariadic()) {
+            if (! $resolve->getParameter('relations') instanceof ParameterReflector && ! $resolve->getReflection()->isVariadic()) {
                 throw new RouteBindingDidNotSupportRelations($class->getName());
             }
         }
