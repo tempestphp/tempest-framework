2ef766cd865fe56d837a5cd44b9957cb9f0452e6
Mark <magyarheaven@gmail.com>
2026-08-21 20:46:10 +0200

    test: add failing tests

--- tests/Integration/Mapper/Serializers/ArrayOfObjectsSerializerTest.php
+++ tests/Integration/Mapper/Serializers/ArrayOfObjectsSerializerTest.php
@@ -3,9 +3,16 @@
 namespace Tests\Tempest\Integration\Mapper\Serializers;
 
 use PHPUnit\Framework\Attributes\Test;
+use Tempest\Mapper\DynamicSerializer;
 use Tempest\Mapper\Exceptions\ValueCouldNotBeSerialized;
+use Tempest\Mapper\MappingContext;
+use Tempest\Mapper\Serializer;
+use Tempest\Mapper\SerializerFactory;
 use Tempest\Mapper\Serializers\ArrayOfObjectsSerializer;
 use Tempest\Mapper\Serializers\EnumSerializer;
+use Tempest\Reflection\PropertyReflector;
+use Tempest\Reflection\TypeReflector;
+use Tempest\Support\Priority;
 use Tests\Tempest\Integration\FrameworkIntegrationTestCase;
 use Tests\Tempest\Integration\Mapper\Fixtures\ObjectWithSerializerProperties;
 
@@ -50,5 +57,53 @@ final class ArrayOfObjectsSerializerTest extends FrameworkIntegrationTestCase
         $this->expectException(ValueCouldNotBeSerialized::class);
 
         new EnumSerializer()->serialize('foo');
+    }
+
+    #[Test]
+    public function uses_mapping_context_for_nested_objects(): void
+    {
+        $context = MappingContext::from('api');
+
+        $this->container
+            ->get(SerializerFactory::class)
+            ->addSerializer(
+                ContextStringSerializer::class,
+                priority: Priority::HIGHEST,
+                context: $context,
+            );
+
+        $serializer = $this->container->get(
+            ArrayOfObjectsSerializer::class,
+            context: $context,
+        );
+
+        $this->assertSame(
+            [['name' => 'api:a']],
+            $serializer->serialize([new ContextObject('a')]),
+        );
+    }
+}
+
+final readonly class ContextObject
+{
+    public function __construct(
+        public string $name,
+    ) {}
+}
+
+final class ContextStringSerializer implements Serializer, DynamicSerializer
+{
+    public static function accepts(PropertyReflector|TypeReflector $input): bool
+    {
+        $type = $input instanceof PropertyReflector
+            ? $input->getType()
+            : $input;
+
+        return $type->getName() === 'string';
+    }
+
+    public function serialize(mixed $input): string
+    {
+        return 'api:' . $input;
     }
 }
