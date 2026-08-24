e3dccac63c874b43d4ca15dc059ae7b3c034e277
Radoslav Grenčík <radoslav.grencik@gmail.com>
2026-08-24 12:37:25 +0200

    fix(mapper): improve serialization of nested objects and handle context in ArrayOfObjectsSerializer

--- packages/mapper/src/Serializers/ArrayOfObjectsSerializer.php
+++ packages/mapper/src/Serializers/ArrayOfObjectsSerializer.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace Tempest\Mapper\Serializers;
 
+use Tempest\Mapper\ConfigurableSerializer;
+use Tempest\Mapper\Context;
 use Tempest\Mapper\DynamicSerializer;
 use Tempest\Mapper\Exceptions\ValueCouldNotBeSerialized;
 use Tempest\Mapper\Mappers\ObjectToArrayMapper;
+use Tempest\Mapper\MappingContext;
 use Tempest\Mapper\Serializer;
 use Tempest\Reflection\PropertyReflector;
 use Tempest\Reflection\TypeReflector;
@@ -15,8 +18,16 @@ use Tempest\Support\Priority;
 use function Tempest\Mapper\map;
 
 #[Priority(Priority::HIGHEST)]
-final class ArrayOfObjectsSerializer implements Serializer, DynamicSerializer
+final class ArrayOfObjectsSerializer implements Serializer, DynamicSerializer, ConfigurableSerializer
 {
+    private readonly Context $context;
+
+    public function __construct(
+        ?Context $context = null,
+    ) {
+        $this->context = $context ?? MappingContext::default();
+    }
+
     public static function accepts(PropertyReflector|TypeReflector $input): bool
     {
         if ($input instanceof TypeReflector) {
@@ -26,16 +37,24 @@ final class ArrayOfObjectsSerializer implements Serializer, DynamicSerializer
         return $input->getIterableType() instanceof TypeReflector;
     }
 
+    public static function configure(PropertyReflector|TypeReflector|string $input, Context $context): Serializer
+    {
+        return new self($context);
+    }
+
     public function serialize(mixed $input): array
     {
-        if (! is_array($input)) {
+        if (!is_array($input)) {
             throw new ValueCouldNotBeSerialized('array');
         }
 
         $values = [];
 
         foreach ($input as $key => $object) {
-            $values[$key] = map($object)->with(ObjectToArrayMapper::class)->do();
+            $values[$key] = map($object)
+                ->in($this->context)
+                ->with(ObjectToArrayMapper::class)
+                ->do();
         }
 
         return $values;
